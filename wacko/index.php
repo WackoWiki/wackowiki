903746df918e5dc2d9ca35e23628d495844383ee
Tann San <16375124+TannSan@users.noreply.github.com>
2008-06-08 23:21:03 +0000

    Installer Fix: No longer shows mod_rewrite JS warning when rewrite mode is not enabled. Installer Fix: Now detects HTTPS address and uses it in base url when detected.

--- wacko/index.php
+++ wacko/index.php
@@ -57,7 +57,7 @@ $wakkaDefaultConfig = array(
 
   "root_page" => "HomePage",
   "wakka_name" => "MyWackoSite",
-  "base_url" => "http://".$_SERVER["SERVER_NAME"].
+  "base_url" => ($_SERVER['SERVER_PORT'] == 443 ? 'https' : 'http').'://'.$_SERVER["SERVER_NAME"].
                        ($_SERVER["SERVER_PORT"] != 80 ? ":".$_SERVER["SERVER_PORT"] : "").
                        preg_replace("/(\?|&)installAction=site-config/","",$_SERVER["REQUEST_URI"]).
                        ($found_rewrite_extension ? "" : "?page="),
