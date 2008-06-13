ead089f82e4908f7608e913df9842e0262fd587f
the0bone <the0bone@localhost>
2008-06-13 20:53:27 +0000

    change table design to tableless design that is css designable; remove spacer gif;

--- wacko/handlers/page/settings.php
+++ wacko/handlers/page/settings.php
@@ -1,7 +1,6 @@
-<div class="pageBefore"><img
-	src="<?php echo $this->GetConfigValue("root_url"); ?>images/z.gif"
-	width="1" height="1" alt="" style="border-width:0px; display: block; vertical-align:top" /></div>
-<div class="page"><?php
+<div class="pageBefore">&nbsp;</div>
+<div class="page">
+  <?php
 
 if ($this->UserIsOwner() || $this->HasAccess("write",$page["tag"]))
 {
@@ -20,25 +19,21 @@ if ($this->UserIsOwner() || $this->HasAccess("write",$page["tag"]))
 
 		// show form
 		?>
-<h3><?php echo str_replace("%1",$this->Link("/".$this->GetPageTag()),$this->GetResourceValue("SettingsFor")); ?></h3>
-<br />
-
-		<?php echo $this->FormOpen("settings") ?>
-<table border="0" cellspacing="0" cellpadding="0">
-	<tr>
-		<td valign="top" style="padding-right: 20px"><?php echo $this->GetResourceValue("meta1"); ?>:<br />
-		<textarea name="keywords" rows="4" cols="51"><?php echo $this->page["keywords"] ?></textarea>
-		</td>
-		<td valign="top" style="padding-right: 20px"><?php echo $this->GetResourceValue("meta2"); ?>:<br />
-		<textarea name="description" rows="4" cols="51"><?php echo $this->page["description"] ?></textarea>
-		</td>
-	</tr>
-</table>
-<table border="0" cellspacing="0" cellpadding="0">
-	<tr>
-		<td valign="middle" nowrap="nowrap"><br />
-		<?php echo $this->GetResourceValue("SetLang"); ?>: <select name="lang">
-		<?php
+  <h3><?php echo str_replace("%1",$this->Link("/".$this->GetPageTag()),$this->GetResourceValue("SettingsFor")); ?></h3>
+  <?php echo $this->FormOpen("settings") ?>
+  <div class="cssform">
+    <p>
+      <label><?php echo $this->GetResourceValue("meta1"); ?></label>
+      <textarea name="keywords" rows="4" cols="51"><?php echo $this->page["keywords"] ?></textarea>
+    </p>
+    <p>
+      <label><?php echo $this->GetResourceValue("meta2"); ?></label>
+      <textarea name="description" rows="4" cols="51"><?php echo $this->page["description"] ?></textarea>
+    </p>
+    <p>
+      <label> <?php echo $this->GetResourceValue("SetLang"); ?></label>
+      <select name="lang">
+        <?php
 		if (!($clang = $this->page["lang"]))
 		$clang = $this->GetConfigValue("language");
 
@@ -50,58 +45,46 @@ if ($this->UserIsOwner() || $this->HasAccess("write",$page["tag"]))
 			}
 		}
 		?>
-		</select></td>
-		<td valign="bottom">
-		<div
-			style="border: 1px solid red; background-color: #DDDDDD; padding: 3px 7px; margin-left: 10px;">
-			<?php echo $this->GetResourceValue("BewareChangeLang"); ?></div>
-		</td>
-	</tr>
-	<tr>
-		<td colspan="3"><br />
-		<input class="OkBtn" onmouseover='this.className="OkBtn_";'
-			onmouseout='this.className="OkBtn";' type="submit" align="top"
-			value="<?php echo $this->GetResourceValue("MetaStoreButton"); ?>"
-			style="width: 120px" accesskey="s" /> <img
-			src="<?php echo $this->GetConfigValue("root_url");?>images/z.gif"
-			width="100" height="1" alt="" border="0" /> <input class="CancelBtn"
-			onmouseover='this.className="CancelBtn_";'
-			onmouseout='this.className="CancelBtn";' type="button" align="top"
-			value="<?php echo $this->GetResourceValue("MetaCancelButton"); ?>"
-			onclick="history.back();" style="width: 120px" /></td>
-	</tr>
-</table>
-			<?php
+      </select>
+    </p>
+    <div class="BewareChangeLang"> <?php echo $this->GetResourceValue("BewareChangeLang"); ?></div>
+    <p>
+      <input class="OkBtn" onmouseover='this.className=&quot;OkBtn_&quot;;' onmouseout='this.className=&quot;OkBtn&quot;;' type="submit" align="top" value="<?php echo $this->GetResourceValue("MetaStoreButton"); ?>" style="width: 120px" accesskey="s" />
+      &nbsp;
+      <input class="CancelBtn" onmouseover='this.className=&quot;CancelBtn_&quot;;' onmouseout='this.className=&quot;CancelBtn&quot;;' type="button" align="top" value="<?php echo $this->GetResourceValue("MetaCancelButton"); ?>" onclick="history.back();" style="width: 120px" />
+    </p>
+  </div>
+  <?php
 			print($this->FormClose());
 }
 ?>
-<table border="0" cellspacing="0" cellpadding="0">
-	<tr>
-		<td colspan="3"><br />
-		<?php echo $this->GetResourceValue("SettingsPortal");?>
-		<ul>
-			<li><a
-				href="<?php echo $this->href("edit"     )."\">".$this->GetResourceValue("SettingsEdit"     ); ?></a></li>
-<li><a href="<?php echo $this->href("revisions")."\">".$this->GetResourceValue("SettingsRevisions"); ?></a></li>
-<li><a href="<?php echo $this->href("rename"   )."\">".$this->GetResourceValue("SettingsRename"   ); ?></a>
-<ul><li><a href="<?php echo $this->href("massrename" )."\">".$this->GetResourceValue("SettingsMassRename" ); ?></a></li></ul></li>
-<li><a href="<?php echo $this->href("remove"   )."\">".$this->GetResourceValue("SettingsRemove"   ); ?></a></li>
-<li><a href="<?php echo $this->href("acls"     )."\">".$this->GetResourceValue("SettingsAcls"     ); ?></a>
-<ul><li><a href="<?php echo $this->href("massacls" )."\">".$this->GetResourceValue("SettingsMassAcls" ); ?></a></li></ul></li>
-<li><a href="<?php echo $this->href("upload"   )."\">".$this->GetResourceValue("SettingsUpload"   ); ?></a></li>
-<li><a href="<?php echo $this->href("referrers")."\">".$this->GetResourceValue("SettingsReferrers"); ?></a></li>
-<li><a href="<?php echo $this->href("watch"    )."\">".$this->GetResourceValue("SettingsWatch"    ); ?></a></li>
-<li><a href="<?php echo $this->href("print"    )."\">".$this->GetResourceValue("SettingsPrint"    ); ?></a></li>
-<li><a href="<?php echo $this->href("msword"   )."\">".$this->GetResourceValue("SettingsMsword"   ); ?></a></li>
-</ul>
-        </td>
-      </tr>
-    </table>
-<?php
+  <p><?php echo $this->GetResourceValue("SettingsPortal");?>
+  <ul>
+    <li><a href="<?php echo $this->href("edit");?>"><?php echo $this->GetResourceValue("SettingsEdit"); ?></a></li>
+    <li><a href="<?php echo $this->href("revisions");?>"><?php echo $this->GetResourceValue("SettingsRevisions"); ?></a></li>
+    <li><a href="<?php echo $this->href("rename");?>"><?php echo $this->GetResourceValue("SettingsRename"); ?></a>
+      <ul>
+        <li><a href="<?php echo $this->href("massrename");?>"><?php echo $this->GetResourceValue("SettingsMassRename"); ?></a></li>
+      </ul>
+    </li>
+    <li><a href="<?php echo $this->href("remove");?>"><?php echo $this->GetResourceValue("SettingsRemove"); ?></a></li>
+    <li><a href="<?php echo $this->href("acls");?>"><?php echo $this->GetResourceValue("SettingsAcls"); ?></a>
+      <ul>
+        <li><a href="<?php echo $this->href("massacls" );?>"><?php echo $this->GetResourceValue("SettingsMassAcls" ); ?></a></li>
+      </ul>
+    </li>
+    <li><a href="<?php echo $this->href("upload");?>"><?php echo $this->GetResourceValue("SettingsUpload"); ?></a></li>
+    <li><a href="<?php echo $this->href("referrers");?>"><?php echo $this->GetResourceValue("SettingsReferrers"); ?></a></li>
+    <li><a href="<?php echo $this->href("watch");?>"><?php echo $this->GetResourceValue("SettingsWatch"); ?></a></li>
+    <li><a href="<?php echo $this->href("print");?>"><?php echo $this->GetResourceValue("SettingsPrint"); ?></a></li>
+    <li><a href="<?php echo $this->href("msword");?>"><?php echo $this->GetResourceValue("SettingsMsword"); ?></a></li>
+  </ul>
+  <?php
 }
 else
 {
    print($this->GetResourceValue("ReadAccessDenied"));
 }
 ?>
+  </li>
 </div>
