044108ddfeb6fc27472cd47080b6147d8304239a
the0bone <24297832+the0bone@users.noreply.github.com>
2008-06-14 09:23:46 +0000

    Bugfixing from previous version. P Tag was set wrong. Now each revision is in a new line and own p Tag

--- wacko/handlers/page/revisions.php
+++ wacko/handlers/page/revisions.php
@@ -1,6 +1,5 @@
 <div class="pageBefore">&nbsp;</div>
-<div class="page">
-  <?php
+<div class="page"><?php
 if ($this->HasAccess("read")) {
 	// load revisions for this page
 	if ($pages = $this->LoadRevisions($this->tag))
@@ -12,7 +11,6 @@ if ($this->HasAccess("read")) {
 		$output .= "&nbsp;&nbsp;&nbsp;<input type=\"checkbox\" name=\"fastdiff\" />\n".$this->GetResourceValue("SimpleDiff");
 		$output .= "&nbsp;&nbsp;&nbsp;<a href=\"".$this->href("revisions.xml")."\"><img src=\"".$this->GetConfigValue("theme_url")."icons/xml.gif"."\" title=\"".$this->GetResourceValue("RevisionXMLTip")."\" alt=\"XML\" /></a>";
 		$output .= "</p>\n";
-		$output .= "<p>\n";
 		if ($user = $this->GetUser())
 		{
 			$max = $user["revisioncount"];
@@ -28,13 +26,13 @@ if ($this->HasAccess("read")) {
 			$c++;
 			if (($c <= $max) || !$max)
 			{
-				$output .= "<input type=\"radio\" name=\"a\" value=\"".($c==1?"-1":$page["id"])."\" ".($c == 1 ? "checked=\"checked\"" : "")." />";
+				$output .= "<p><input type=\"radio\" name=\"a\" value=\"".($c==1?"-1":$page["id"])."\" ".($c == 1 ? "checked=\"checked\"" : "")." />";
 				$output .= "&nbsp;&nbsp;&nbsp;<input type=\"radio\" name=\"b\" value=\"".($c==1?"-1":$page["id"])."\" ".($c == 2 ? "checked=\"checked\"" : "")." />";
 				$output .= "&nbsp;&nbsp;&nbsp;&nbsp;<a href=\"".$this->href("show").($this->GetConfigValue("rewrite_mode")?"?":"&amp;")."time=".urlencode($page["time"])."\">".$page["time"]."</a>";
-				$output .= "&nbsp;&nbsp;&nbsp;&nbsp;".$this->GetResourceValue("By")." ".($this->IsWikiName($page["user"])?$this->Link($page["user"]):$page["user"])."";
+				$output .= "&nbsp;&nbsp;&nbsp;&nbsp;".$this->GetResourceValue("By")." ".($this->IsWikiName($page["user"])?$this->Link($page["user"]):$page["user"])."</p>\n";
 			}
 		}
-		$output .= "</p><br />\n";
+		$output .= "<br />\n";
 		if (!$this->GetConfigValue("revisions_hide_cancel")) $output .= "<input type=\"button\" value=\"".$this->GetResourceValue("CancelDifferencesButton")."\" onclick=\"document.location='".addslashes($this->href(""))."';\" />\n";
 		$output .= $this->FormClose()."\n";
 	}
@@ -43,5 +41,4 @@ if ($this->HasAccess("read")) {
 } else {
 	print($this->GetResourceValue("ReadAccessDenied"));
 }
-?>
-</div>
+?></div>
