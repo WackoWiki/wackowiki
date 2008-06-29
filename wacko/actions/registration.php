bc8d27b8e211e60153c19c89705010d5ab621e73
tann_san <tann_san@localhost>
2008-06-29 19:15:12 +0000

    Added: Registration captcha

--- wacko/actions/registration.php
+++ wacko/actions/registration.php
@@ -25,54 +25,106 @@ else if ($_REQUEST["action"] == "login")
    // otherwise, create new account
    else if ($this->GetConfigValue("allow_registration") || $this->IsAdmin())
    {
+
       $name = trim($_POST["name"]);
       $email = trim($_POST["email"]);
       $password = $_POST["password"];
       $confpassword = $_POST["confpassword"];
       $lang = $_POST["lang"];
 
-      // check if name is WikiName style
-      if (!$this->IsWikiName($name)) $error = $this->GetResourceValue("MustBeWikiName");
-      else if (!$email) $error = $this->GetResourceValue("SpecifyEmail");
-      else if (!preg_match("/^.+?\@.+$/", $email)) $error = $this->GetResourceValue("NotAEmail");
-      else if ($confpassword != $password) $error = $this->GetResourceValue("PasswordsDidntMatch");
-      else if (preg_match("/ /", $password)) $error = $this->GetResourceValue("SpacesArentAllowed");
-      else if (strlen($password) < 5) $error = $this->GetResourceValue("PasswordTooShort");
-      else
+   // Start Comment Captcha
+
+   // Only show captcha if the admin enabled it in the config file
+   if(!$this->IsAdmin() && $this->GetConfigValue("captcha_registration"))
       {
-         $confirm = md5(rand().$email.rand());
-         $more = $this->ComposeOptions(array("send_watchmail"=>"Y",));
+         // Don't load the captcha at all if the GD extension isn't enabled
+         if(extension_loaded('gd'))
+            {
+               //check whether anonymous user
+               //anonymous user has the IP or host name as name
+               //if name contains '.', we assume it's anonymous
+               if(strpos($this->GetUserName(), '.'))
+                  {
+                     //anonymous user, check the captcha
+                     if(!empty($_SESSION['freecap_word_hash']) && !empty($_POST['word']))
+                        {
+                           if($_SESSION['hash_func'](strtolower($_POST['word']))==$_SESSION['freecap_word_hash'])
+                              {
+                                 // reset freecap session vars
+                                 // cannot stress enough how important it is to do this
+                                 // defeats re-use of known image with spoofed session id
+                                 $_SESSION['freecap_attempts'] = 0;
+                                 $_SESSION['freecap_word_hash'] = false;
 
-         $this->Query("INSERT INTO ".$this->config["user_table"]." SET ".
-          "signuptime = now(), ".
-          "name = '".quote($this->dblink, $name)."', ".
-          "email = '".quote($this->dblink, $email)."', ".
-          "email_confirm = '".quote($this->dblink, $confirm)."', ".
-          "bookmarks = '".quote($this->dblink, $this->GetDefaultBookmarks($lang))."', ".
-          "typografica = '".(($this->config["default_typografica"]==1)?"Y":"N")."', ".
-          "showdatetime = '".(($this->config["default_showdatetime"]==1)?"Y":"N")."', ".
-          "more = '".quote($this->dblink, $more)."', ".
-         ($lang?"lang = '".quote($this->dblink, $lang)."', ":"").
-          "password = md5('".quote($this->dblink, $_POST["password"])."')");
+                                 // now process form
+                                 $word_ok = true;
+                              }
+                           else
+                              {
+                                 $word_ok = false;
+                              }
+                        }
+                     else
+                        {
+                           $word_ok = false;
+                        }
 
-         $subject = $this->GetResourceValue("Mail.Welcome").$this->GetConfigValue("wakka_name");
-         $message = $this->GetResourceValue("MailHello"). $name.".<br /> <br /> ";
-         $message.= str_replace('%1', $this->GetConfigValue("wakka_name"),
-         str_replace('%2', $name,
-         str_replace('%3', $this->Href().($this->config["rewrite_mode"] ? "?" : "&amp;")."confirm=".$confirm,
-         $this->GetResourceValue("Mail.Registered"))))."<br />  ";
-         $message.= "<br />".$this->GetResourceValue("MailGoodbye")." ".$this->GetConfigValue("wakka_name");
-         $this->SendMail($email, $subject, $message);
+                     if(!$word_ok)
+                        {
+                           //not the right word
+                           $error = $this->GetResourceValue("SpamAlert");
+                           $this->SetMessage($this->GetResourceValue("SpamAlert"));
+                        }
+                  }
+            }
+         }
+      // End Comment Captcha
 
-         // log in
-         $this->SetUser($this->LoadUser($name));
-         $this->LogUserIn($this->GetUser());
+      if($word_ok)
+         {
+            // check if name is WikiName style
+            if (!$this->IsWikiName($name)) $error = $this->GetResourceValue("MustBeWikiName");
+            else if (!$email) $error = $this->GetResourceValue("SpecifyEmail");
+            else if (!preg_match("/^.+?\@.+$/", $email)) $error = $this->GetResourceValue("NotAEmail");
+            else if ($confpassword != $password) $error = $this->GetResourceValue("PasswordsDidntMatch");
+            else if (preg_match("/ /", $password)) $error = $this->GetResourceValue("SpacesArentAllowed");
+            else if (strlen($password) < 5) $error = $this->GetResourceValue("PasswordTooShort");
+            else
+            {
+               $confirm = md5(rand().$email.rand());
+               $more = $this->ComposeOptions(array("send_watchmail"=>"Y",));
 
-         // forward
-         $this->context[++$this->current_context] = "";
-         $this->Redirect($this->Href("",$name,"",1));
+               $this->Query("INSERT INTO ".$this->config["user_table"]." SET ".
+                "signuptime = now(), ".
+                "name = '".quote($this->dblink, $name)."', ".
+                "email = '".quote($this->dblink, $email)."', ".
+                "email_confirm = '".quote($this->dblink, $confirm)."', ".
+                "bookmarks = '".quote($this->dblink, $this->GetDefaultBookmarks($lang))."', ".
+                "typografica = '".(($this->config["default_typografica"]==1)?"Y":"N")."', ".
+                "showdatetime = '".(($this->config["default_showdatetime"]==1)?"Y":"N")."', ".
+                "more = '".quote($this->dblink, $more)."', ".
+               ($lang?"lang = '".quote($this->dblink, $lang)."', ":"").
+                "password = md5('".quote($this->dblink, $_POST["password"])."')");
+
+               $subject = $this->GetResourceValue("Mail.Welcome").$this->GetConfigValue("wakka_name");
+               $message = $this->GetResourceValue("MailHello"). $name.".<br /> <br /> ";
+               $message.= str_replace('%1', $this->GetConfigValue("wakka_name"),
+               str_replace('%2', $name,
+               str_replace('%3', $this->Href().($this->config["rewrite_mode"] ? "?" : "&amp;")."confirm=".$confirm,
+               $this->GetResourceValue("Mail.Registered"))))."<br />  ";
+               $message.= "<br />".$this->GetResourceValue("MailGoodbye")." ".$this->GetConfigValue("wakka_name");
+               $this->SendMail($email, $subject, $message);
+
+               // log in
+               $this->SetUser($this->LoadUser($name));
+               $this->LogUserIn($this->GetUser());
+
+               // forward
+               $this->context[++$this->current_context] = "";
+               $this->Redirect($this->Href("",$name,"",1));
+            }
+         }
       }
-   }
 }
 //  else
 if (!$_REQUEST["confirm"])
@@ -100,7 +152,7 @@ if ($this->GetConfigValue("allow_registration") || $this->IsAdmin())
       <?php
          $langs = $this->AvailableLanguages();
          for ($i=0;$i<count($langs);$i++)
-         echo '<option value="'.$langs[$i].'">'.$langs[$i].'</option>';
+         echo '<option value="'.$langs[$i].'"'.($lang == $langs[$i] ? 'selected="selected"' : '').'>'.$langs[$i].'</option>';
          ?>
     </select>
   </p>
@@ -114,17 +166,41 @@ if ($this->GetConfigValue("allow_registration") || $this->IsAdmin())
   </p>
   <p>
     <label><?php echo $this->GetResourceValue("RegistrationPassword");?>:</label>
-    <input type="password" name="password" size="24" />
+    <input type="password" name="password" size="24" value="<?php echo $password ?>" />
   </p>
   <p>
     <label><?php echo $this->GetResourceValue("ConfirmPassword");?>:</label>
-    <input type="password" name="confpassword" size="24" />
+    <input type="password" name="confpassword" size="24" value="<?php echo $confpassword ?>" />
   </p>
   <p>
     <label><?php echo $this->GetResourceValue("Email");?>:</label>
     <input name="email" size="30"
          value="<?php echo htmlspecialchars($email); ?>" />
   </p>
+<?php
+            // captcha code starts
+
+            // Only show captcha if the admin enabled it in the config file
+            if($this->GetConfigValue("captcha_registration"))
+               {
+                  // Don't load the captcha at all if the GD extension isn't enabled
+                  if(extension_loaded('gd'))
+                     {
+                        if(strpos($this->GetUserName(), '.'))
+                           {
+          ?>
+<p><?php echo $this->GetResourceValue("Captcha");?>:</p>
+<img src="<?php echo $this->GetConfigValue("root_url");?>lib/captcha/freecap.php" id="freecap" alt="<?php echo $this->GetResourceValue("Captcha");?>" /> <a href="" onClick="this.blur(); new_freecap(); return false;" title="<?php echo $this->GetResourceValue("CaptchaReload"); ?>"><img src="<?php echo $this->GetConfigValue("theme_url");?>icons/reload.png" width="18" height="17" alt="<?php echo $this->GetResourceValue("CaptchaReload"); ?>" /></a>
+<br />
+<input type="text" name="word" maxlength="6" style="width: 273px;" />
+<br />
+<br />
+<?php
+                           }
+                     }
+               }
+            // end captcha
+?>
   <p>
     <input class="OkBtn" onmouseover='this.className="OkBtn_";'
          onmouseout='this.className="OkBtn";' type="submit" align="top"
