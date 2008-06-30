ebb728c455b0a812dfd15114953d52a501839b58
Tann San <16375124+TannSan@users.noreply.github.com>
2008-06-30 10:23:18 +0000

    Fixed: Bug 176 - Using $this inside functions for recentcomments.php and recentlycommented.php when should of been using $wacko reference.

--- wacko/actions/recentcomments.php
+++ wacko/actions/recentcomments.php
@@ -1,13 +1,13 @@
 <?php
 
 if (!function_exists('LoadRecentComments')){
-	function LoadRecentComments(&$wacko, $for = "", $limit = 50)
-	{
-		return
-		$wacko->LoadAll("select ".$wacko->pages_meta." from ".$wacko->config["table_prefix"]."pages where ".
-		($for?"super_comment_on like '".quote($this->dblink, $wacko->NpjTranslit($for))."/%' ":"comment_on != '' ").
+   function LoadRecentComments(&$wacko, $for = "", $limit = 50)
+   {
+      return
+      $wacko->LoadAll("select ".$wacko->pages_meta." from ".$wacko->config["table_prefix"]."pages where ".
+      ($for?"super_comment_on like '".quote($wacko->dblink, $wacko->NpjTranslit($for))."/%' ":"comment_on != '' ").
                     "AND latest = 'Y' order by time desc LIMIT ".(int)$limit);
-	}
+   }
 }
 
 if (!isset($root)) $root = $this->UnwrapLink($vars[0]);
@@ -16,31 +16,31 @@ if (!$max) $max = 50;
 
 if ($comments = LoadRecentComments($this, $root, (int)$max))
 {
-	foreach ($comments as $comment)
-	{
-		if ($this->config["hide_locked"]) $access = $this->HasAccess("read",$comment["comment_on"]);
-		else $access = true;
+   foreach ($comments as $comment)
+   {
+      if ($this->config["hide_locked"]) $access = $this->HasAccess("read",$comment["comment_on"]);
+      else $access = true;
 
-		if ($access && $this->UserAllowedComments())
-		{
-			// day header
-			list($day, $time) = explode(" ", $comment["time"]);
-			if ($day != $curday)
-			{
-				if ($curday) print("<br />\n");
-				print("<strong>$day:</strong><br />\n");
-				$curday = $day;
-			}
+      if ($access && $this->UserAllowedComments())
+      {
+         // day header
+         list($day, $time) = explode(" ", $comment["time"]);
+         if ($day != $curday)
+         {
+            if ($curday) print("<br />\n");
+            print("<strong>$day:</strong><br />\n");
+            $curday = $day;
+         }
 
-			// print entry
-			print("&nbsp;&nbsp;&nbsp;(".$comment["time"].") <a href=\"".$this->href("", $comment["comment_on"], "show_comments=1")."#".$comment["tag"]."\">".$comment["comment_on"]."</a> . . . . <small>".
-			($this->IsWikiName($comment["user"])?$this->Link("/".$comment["user"],"",$comment["user"]):$comment["user"])."</small><br />\n");
-		}
-	}
+         // print entry
+         print("&nbsp;&nbsp;&nbsp;(".$comment["time"].") <a href=\"".$this->href("", $comment["comment_on"], "show_comments=1")."#".$comment["tag"]."\">".$comment["comment_on"]."</a> . . . . <small>".
+         ($this->IsWikiName($comment["user"])?$this->Link("/".$comment["user"],"",$comment["user"]):$comment["user"])."</small><br />\n");
+      }
+   }
 }
 else
 {
-	echo $this->GetResourceValue("NoRecentComments");
+   echo $this->GetResourceValue("NoRecentComments");
 }
 
 ?>
