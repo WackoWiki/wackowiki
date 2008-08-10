9242f5373ce1c2986846394b5388862a07b3f211
emanelhs <emanelhs@localhost>
2008-08-10 10:49:29 +0000

    lang.el.php final version by Evaggelos Balaskas

--- wacko/lang/lang.el.php
+++ wacko/lang/lang.el.php
@@ -5,9 +5,10 @@ $wackoLanguage = array(
   "code" => "el",
   "charset" => "iso-8859-7",
   "utfdecode" => array(),
-  "UPPER_P"  => "A-Z\xc0-\xd6\xd8-\xdd",
-  "LOWER_P"  => "a-z\xdf-\xf6\xf8-\xfd\xff\/'",
-  "ALPHA_P"  => "A-Za-z\xc0-\xd6\xd8-\xdd\xdf-\xf6\xf8-\xfd\xff\_\-\/'",
+//http://en.wikipedia.org/wiki/ISO-8859-7
+  "UPPER_P"  => "A-Z\xc1\xb6\xc2-\xc5\xb8\xc6\xc7\xb9\xc8\xc9\xba\xda\xca-\xcf\xbc\xd0-\xd5\xbe\xdb\xd6-\xd9\xbf",
+  "LOWER_P"  => "a-z\xe1\xdc\xe2-\xe5\xdd\xe6\xe7\xde\xe8\xe9\xdf\xfa\xc0\xea-\xef\xfc\xf0-\xf5\xfd\xfb\xe0\xf6-\xf9\xfe\/'",
+  "ALPHA_P"  => "A-Za-z\xb6\xb8-\xba\xbc\xbe-\xfe\/'",
   "locale" => "el_GR",
   "NpjLettersFrom" => "¶¸¹º¼¾¿ÀÚÛÜÝÞßàúûüýþáâãäåæçéêëìíïðñóòôõö÷ùÁÂÃÄÅÆÇÉÊËÌÍÏÐÑÓÔÕÖ×Ù",
   "NpjLettersTo"   => "aehioywiiyaehiyiyoywavgdezhiklmnoprsstyfxwavgdezhiklmnoprstyfxw",
