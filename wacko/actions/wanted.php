98826bee5a21c40b35c998e8a26e22c0468bcba8
Tann San <16375124+TannSan@users.noreply.github.com>
2008-09-08 16:39:09 +0000

    Fixed Bug#60: WantedPages don't obey ACLs - Had to change the SQL statement and the way the returned data was handled.  Also identified a bug that must of been there from the start to do with totally the referrer counts for WantedPages.

--- wacko/actions/wanted.php
+++ wacko/actions/wanted.php
@@ -10,7 +10,10 @@ if ($linking_to = $_REQUEST["linking_to"])
       echo $this->GetResourceValue("PagesLinkingTo")." ".$this->Link($linking_to).":<br />\n";
       foreach ($pages as $page)
       {
-         print($this->Link("/".$page["tag"],"","/".$page["tag"])."<br />\n");
+         if(!$this->config["hide_locked"] || $this->HasAccess("read", $page["tag"]))
+            {
+               print($this->Link("/".$page["tag"],"","/".$page["tag"])."<br />\n");
+            }
       }
    }
    else
@@ -22,9 +25,32 @@ else
 {
    if ($pages = $this->LoadWantedPages($root))
    {
-      foreach ($pages as $page)
+      foreach($pages as $page)
       {
-         print($this->Link("/".$page["wanted_tag"])." (<a href=\"".$this->href().($this->config["rewrite_mode"] ? "?" : "&amp;")."linking_to=".$page["wanted_tag"]."\">".$page["count"]."</a>)<br />\n");
+         $page_parent = substr($page["wanted_tag"], 0, strrpos($page["wanted_tag"], "/"));
+
+         if(!$this->config["hide_locked"] || $this->HasAccess("read", $page_parent))
+            {
+               // Update the referrer count for the WantedPage, we need to take pages the user is not allowed to view out of the total
+               $count = 0;
+
+               if($referring_pages = $this->LoadPagesLinkingTo($page["wanted_tag"], $root))
+                  {
+                     foreach ($referring_pages as $referrer_page)
+                     {
+                        if(!$this->config["hide_locked"] || $this->HasAccess("read", $referrer_page["tag"]))
+                           {
+                              $count++;
+                           }
+                     }
+                  }
+
+               // If no pages are referring to the WantedPage it means the referrers are all locked so don't show the link at all
+               if($count > 0)
+                  {
+                     print($this->Link("/".$page["wanted_tag"])." (<a href=\"".$this->href().($this->config["rewrite_mode"] ? "?" : "&amp;")."linking_to=".$page["wanted_tag"]."\">".$count."</a>)<br />\n");
+                  }
+            }
       }
    }
    else
