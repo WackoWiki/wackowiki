52b9c8ec1ed1b3ad2eafc2820e549d9fc80b89bf
vendeeglobe <vendeeglobe@localhost>
2008-09-10 01:09:16 +0000

    minor changes

--- wacko/themes/tabs/lang/wakka.ru.php
+++ wacko/themes/tabs/lang/wakka.ru.php
@@ -1,6 +1,6 @@
 <?php
 $themeResource = array(
-  "EditIcon" => "<img src=\"".$this->GetConfigValue("theme_url")."icons/edit.gif\" align=\"absmiddle\" hspace=\"2\" alt=\"Редактировать вложенную страницу\" border=\"0\" />",
+  "EditIcon" => "<img src=\"".$this->GetConfigValue("theme_url")."icons/edit.gif\" alt=\"Редактировать вложенную страницу\" border=\"0\" />",
   "ACLStoreButton"  => "Сохранить",
   "ACLCancelButton" => "Отменить\nи вернуться",
   "ACLAccessDenied" => "<em>Вы не владелец этой страницы.</em>",
