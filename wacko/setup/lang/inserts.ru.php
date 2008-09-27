b182adabc6c447cfa760a4a792299104175832fd
vladakafreeman <vladakafreeman@localhost>
2008-09-27 19:57:49 +0000

    Russian translation for R4.3

--- wacko/setup/lang/inserts.ru.php
+++ wacko/setup/lang/inserts.ru.php
@@ -3,7 +3,7 @@ $lng = "ru";
 
 if ($config["language"]==$lng)
 {
-	InsertPage($config["root_page"], "((file:wacko4.gif WackoWiki))\n**Добро пожаловать в волшебный мир ((WackoWiki:Doc/English/WackoWiki WackoWiki)).**\n\nНажмите \"Правка\" внизу страницы, чтобы изменить её (между прочим, вы можете просто совершить двойной щелчок мышкой - это приведёт к тому же результату).\n\nДокументация по Ваке доступна на WackoWiki:Wacko/Документация.\n\nПолезные страницы: ((WackoWiki:Doc/Russian/Sintaksis Форматирование)), ПропущенныеСтраницы, ЗабытыеСтраницы, TextSearch, МоиСтраницы, МоиИзменения.\n", $lng, "Admins", true);
+	InsertPage($config["root_page"], "((file:wacko4.gif WackoWiki))\n**Добро пожаловать в волшебный мир ((WackoWiki:Doc/English/WackoWiki WackoWiki)).**\n\nНажмите \"Правка\" внизу страницы, чтобы изменить её (между прочим, вы можете просто совершить двойной щелчок мышкой -- это приведёт к тому же результату).\n\nДокументация по WackoWiki доступна на WackoWiki:Wacko/Документация.\n\nПолезные страницы: ((WackoWiki:Doc/Russian/Sintaksis Форматирование)), ПропущенныеСтраницы, ЗабытыеСтраницы, TextSearch, МоиСтраницы, МоиИзменения.\n", $lng, "Admins", true);
 	InsertPage('ПропущенныеСтраницы', '{{WantedPages}}', $lng, "Admins", true);
 	InsertPage('ЗабытыеСтраницы', '{{OrphanedPages}}', $lng, "Admins", true);
 	InsertPage('МоиСтраницы', '{{MyPages}}', $lng, "Admins", true);
@@ -16,9 +16,9 @@ InsertPage('Пользователи', '{{LastUsers}}', $lng);
 InsertPage('НовыеКомментарии', '{{RecentlyCommented}}', $lng);
 InsertPage('Регистрация', '{{Registration}}', $lng);
 
-InsertPage('Password', '{{ChangePassword}}', $lng);
-InsertPage('TextSearch', '{{Search}}', $lng);
-InsertPage('Login', '{{Login}}', $lng);
+InsertPage('Пароль', '{{ChangePassword}}', $lng);
+InsertPage('Поиск', '{{Search}}', $lng);
+InsertPage('Вход', '{{Login}}', $lng);
 InsertPage('InterWiki', '{{InterWikiList}}', $lng);
 InsertPage('Настройки', '{{UserSettings}}', $lng);
 ?>
