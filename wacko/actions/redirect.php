ac7b2313c9c731f73601bfe9d44fdd977a5c1b0f
vendeeglobe <54716082+vendeeglobe@users.noreply.github.com>
2008-10-08 16:25:15 +0000

    update CSS class .BewareChangeLang

--- wacko/actions/redirect.php
+++ wacko/actions/redirect.php
@@ -3,7 +3,7 @@ $page = $this->UnwrapLink($vars[0]);
 if ($page) {
 	if ($this->LoadPage($page, "", LOAD_CACHE, LOAD_META)) {
 		$user = $this->GetUser();
-		if ($user["options"]["dont_redirect"]=="Y" || $_REQUEST["redirect"]=='no')
+		if ($user["options"]["dont_redirect"]=="Y" || $_REQUEST["redirect"]=="no")
 		{
 			print ("<br /><br /><br /><center>".$this->GetResourceValue("PageMoved")." ".$this->Link("/".$page)."</center><br /><br /><br />");
 		}
