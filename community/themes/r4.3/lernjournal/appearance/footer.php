075ed75311b0d153e262238d7b5c9f5eaf93fb25
vendeeglobe <vendeeglobe@localhost>
2008-10-22 08:51:23 +0000

    added new lernjournal theme to community folder

---
+++ community/themes/r4.3/lernjournal/appearance/footer.php
@@ -0,0 +1,32 @@
+<?php 
+/*
+lernjournal theme.
+
+*/
+?>
+</div>
+
+  <div id="footer"><?php 
+if ($this->GetUser()){
+	echo $this->GetResourceValue("PoweredBy")." ".$this->Link("WackoWiki:WackoWiki", "", "WackoWiki ".$this->GetWackoVersion());
+}
+?></div>
+  </div>
+
+<?php
+
+//Debug Querylog.
+if ($this->GetConfigValue("debug")>=2)
+{
+ print("<span class=\"debug\">");
+ print("<strong>Query log:</strong><br />\n");
+ foreach ($this->queryLog as $query)
+ {
+  print($query["query"]." (".$query["time"].")<br />\n");
+  $zz++;
+ }
+ print("<b>total: $zz</b>");
+ print("</span>");
+}
+// Don't place final </body></html> here. Wacko closes HTML automatically.
+?>
