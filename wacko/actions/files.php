b292802b3a28a52f7cd04af6d042528768fa1278
vendeeglobe <vendeeglobe@localhost>
2009-01-05 21:27:33 +0000

    FIX bug 250: incorrect use of $this in linkstree.php - many thanks to pascalvmf

--- wacko/actions/files.php
+++ wacko/actions/files.php
@@ -13,6 +13,7 @@
 if (!isset($order)) $order = "";
 if (!isset($global)) $global = "";
 if (!isset($tag)) $tag = "";
+if (!isset($owner)) $owner = "";
 if (!isset($pictures)) $pictures = "";
 
 $orderby = "filename ASC";
@@ -22,12 +23,10 @@ if ($order == "size") $orderby = "filesize ASC";
 if ($order == "size_desc") $orderby = "filesize DESC";
 if ($order == "ext") $orderby = "file_ext ASC";
 
-if (!isset($owner)) $owner = "";
 if ($owner) $user_add = "and user='".quote($this->dblink, $owner)."' ";
 else        $user_add = "";
 
 // do we allowed to see?
-
 
 if (!$global)
 {
@@ -41,7 +40,8 @@ if (!$global)
 	}
 	$can_view   = $this->HasAccess("read",$vars[0]) || $this->IsAdmin() || $this->UserIsOwner($vars[0]);
 	$can_delete = $this->IsAdmin() || $this->UserIsOwner($vars[0]);
-} else
+}
+else
 {
 	$can_view = 1;
 	$vars[0] = $this->tag;
@@ -54,13 +54,16 @@ if ($can_view)
 	if (!$global && !$filepage["id"]) return;
 
 	// load files list
-	$files = $this->LoadAll( "SELECT id, page_id, filesize, picture_w, picture_h, filename, description, uploaded_dt, user FROM ".
+	$files = $this->LoadAll(
+		"SELECT id, page_id, filesize, picture_w, picture_h, filename, description, uploaded_dt, user FROM ".
 	$this->config["table_prefix"]."upload WHERE ".
-                             " page_id = '". ($global?0:$filepage["id"])."' ".$user_add.
-                             " ORDER BY ".$orderby );
+		" page_id = '". ($global?0:$filepage["id"])."' ".$user_add.
+		" ORDER BY ".$orderby );
+
 	if (!is_array($files)) $files = array();
 
-	if (!$nomark){
+	if (!$nomark)
+	{
 		$title = $this->GetResourceValue("UploadTitle".($global?"Global":""));
 		print("<fieldset><legend>".$title.": ".$showpageandpath."</legend>\n");
 	}
@@ -79,62 +82,72 @@ if ($can_view)
 
 	if (count($files))
 	{
-		?>
+?>
 <table class="upload" cellspacing="0" cellpadding="0" border="0">
 <?php
-}
+	}
 
-foreach( $files as $file )
-{
-	$this->filesCache[$file["page_id"]][$file["filename"]] = &$file;
+	foreach( $files as $file )
+	{
+		$this->filesCache[$file["page_id"]][$file["filename"]] = &$file;
 
-	$dt = $file["uploaded_dt"];
-	$desc = $this->Format( $file["description"], "typografica" );
+		$dt = $file["uploaded_dt"];
+		$desc = $this->Format($file["description"], "typografica" );
 
-	if ($desc == "") $desc = "&nbsp;";
+		if ($desc == "") $desc = "&nbsp;";
 
-	$filename = $file["filename"];
-	$filesize = ceil($file["filesize"]/1024);
-	$link = $this->Link($path2.$filename, "", $filename );
+		$filename = $file["filename"];
+		$filesize = ceil($file["filesize"] / 1024);
+		$link = $this->Link($path2.$filename, "", $filename );
 
-	if ($this->IsAdmin() || (!$is_global && ($this->GetPageOwner($vars[0]) == $this->GetUserName()))
-	|| ($file["user"]                                 == $this->GetUserName()))
-	$remove_mode=1;
-	else $remove_mode=0;
+		if ($this->IsAdmin() || (!$is_global && ($this->GetPageOwner($vars[0]) == $this->GetUserName()))
+		|| ($file["user"] == $this->GetUserName()))
+		{
+			$remove_mode = 1;
+		}
+		else
+		{
+			$remove_mode = 0;
+		}
 
-	$remove_href = $this->Href("upload", $vars[0], "remove=".($global?"global":"local")."&amp;file=".$filename);
-	?>
+		$remove_href = $this->Href("upload", $vars[0], "remove=".($global?"global":"local")."&amp;file=".$filename);
+?>
 	<tr>
 		<td class="dt-" nowrap="nowrap"><span class="dt2-"><?php echo $dt ?></span>&nbsp;</td>
-		<?php
+<?php
 		if ($remove_mode)
 		{
-			?>
+?>
 		<td class="remove-"><a href="<?php echo $remove_href; ?>"
 			class="remove2-"><?php echo $del; ?></a>&nbsp;</td>
-			<?php
-} else {
-	?>
+<?php
+		}
+		else
+		{
+?>
 		<td class="remove-">&nbsp;</td>
-		<?php
-}
+<?php
+		}
 ?>
 		<td class="size-"><span class="size2-">(<?php echo $filesize; ?>&nbsp;<?php
 		echo $kb; ?>)</span>&nbsp;</td>
 		<td class="file-" nowrap="nowrap"><?php echo $link; ?></td>
 		<td class="desc-"><?php echo $desc ?></td>
 	</tr>
-	<?php
-}
-if (count($files))
-{
-	?>
+<?php
+	}
+
+	if (count($files))
+	{
+?>
 </table>
-	<?php
-}
-if (!$nomark) echo "</fieldset>\n";
+<?php
+	}
+	if (!$nomark) echo "</fieldset>\n";
 }
 else
-echo "<em>".$this->GetResourceValue("ActionDenied")."</em> ";
+{
+	echo "<em>".$this->GetResourceValue("ActionDenied")."</em> ";
+}
 
 ?>
--- wacko/actions/linkstree.php
+++ wacko/actions/linkstree.php
@@ -1,20 +1,18 @@
 <?php
 
-if (!function_exists('links_tree_view')){
-
+if (!function_exists('links_tree_view'))
+{
 	function links_tree_view(&$wacko, $node,$level,$indent=0)
 	{
 		if ($level > 0)
 		{
-
 			if ($indent)
 			print((str_repeat("&nbsp;",$indent*7)).$wacko->Link("/".$node, "", $node)."<br/>\n");
 
-			$pages = $wacko->LoadAll("SELECT to_tag FROM ".$wacko->config["table_prefix"]."links, ".$wacko->config["table_prefix"]."pages WHERE from_tag='".quote($this->dblink, $node)."' AND ".$wacko->config["table_prefix"]."links.to_tag = ".$wacko->config["table_prefix"]."pages.tag ORDER BY to_tag ASC");
+			$pages = $wacko->LoadAll("SELECT to_tag FROM ".$wacko->config["table_prefix"]."links, ".$wacko->config["table_prefix"]."pages WHERE from_tag='".quote($wacko->dblink, $node)."' AND ".$wacko->config["table_prefix"]."links.to_tag = ".$wacko->config["table_prefix"]."pages.tag ORDER BY to_tag ASC");
 
 			if (is_array($pages))
 			{
-
 				$head=split(" :: ",str_replace("]]", "", str_replace("[[", "", $wacko->GetConfigValue("navigation_links"))));
 
 				foreach ($pages as $page)
@@ -38,22 +36,25 @@ if ($root == "/") $root = "";
 if (!($root)) $root = $this->page["tag"];
 $root = $this->UnwrapLink($root);
 
-if (!$nomark){
+if (!$nomark)
+{
 	print("<fieldset><legend>".$this->GetResourceValue("LinksTreeTitle")."</legend>\n");
 }
 
 if (!$levels) $levels = 3;
 else $levels = (int)$levels;
-if ($levels > 4){
+if ($levels > 4)
+{
 	$levels = 4;
 	print("<em>".$this->GetResourceValue("LinksTreeLevel4Warning")."</em><br />");
 }
 
-print($this->Link($root)."<br />\n");//<br/>
+print($this->Link("/".$root, "", $root)."<br />\n");//<br/>
 
 links_tree_view($this,$root,$levels,0);
 
-if (!$nomark){
+if (!$nomark)
+{
 	print("</fieldset>\n");
 }
 
