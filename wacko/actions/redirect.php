59c2b35a852003d26d969c2638ef7c522daf0b78
vendeeglobe <54716082+vendeeglobe@users.noreply.github.com>
2009-01-21 17:41:44 +0000

    add $permanent parameter to Redirect function, default value is false

--- wacko/actions/redirect.php
+++ wacko/actions/redirect.php
@@ -1,14 +1,22 @@
 <?php
+
+// {{Redirect to="!/NewPage" $permanent="[true]"}}
+
 $page = $this->UnwrapLink($vars[0]);
-if ($page) {
-	if ($this->LoadPage($page, "", LOAD_CACHE, LOAD_META)) {
+if ($page) 
+{
+	if ($this->LoadPage($page, "", LOAD_CACHE, LOAD_META)) 
+	{
 		$user = $this->GetUser();
 		if ($user["options"]["dont_redirect"]=="Y" || $_REQUEST["redirect"]=="no")
 		{
 			print ("<br /><br /><br /><center>".$this->GetResourceValue("PageMoved")." ".$this->Link("/".$page)."</center><br /><br /><br />");
 		}
-		else $this->Redirect($this->href("", $page));
-	} else {
+		else 
+		$this->Redirect($this->href("", $page));
+	} 
+	else 
+	{
 		print ("<i>".$this->GetResourceValue("WrongPage4Redirect")."</i>");
 	};
 };
