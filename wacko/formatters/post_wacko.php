715a2e4dfa7f6eb25c49c0597a13580ca524cc90
vendeeglobe <vendeeglobe@localhost>
2009-03-11 03:04:54 +0000

    minor changes

--- wacko/formatters/post_wacko.php
+++ wacko/formatters/post_wacko.php
@@ -7,9 +7,10 @@ $parser = &new post_wacko( $this, $options );
 $text = preg_replace_callback("/(\¢\¢(\S+?)([^\n]*?)==([^\n]*?)\¯\¯|\¡\¡[^\n]+?\¡\¡)/sm",
 array( &$parser, "postcallback"), $text);
 
-if ( !isset( $options["stripnotypo"] ) ) $options["stripnotypo"] = "";
+if (!isset($options["stripnotypo"])) $options["stripnotypo"] = "";
 
-if ( $options["stripnotypo"] ) {
+if ($options["stripnotypo"])
+{
 	$text = str_replace("<!--notypo-->", "", $text);
 	$text = str_replace("<!--/notypo-->", "", $text);
 }
--- wacko/formatters/wacko.php
+++ wacko/formatters/wacko.php
@@ -8,6 +8,7 @@ $text = "\177\n".$text."\n";
 $parser = &new WackoFormatter( $this );
 
 $this->headerCount = 0;
+
 $text = preg_replace_callback($parser->NOTLONGREGEXP, array( &$parser, "WackoPreprocess"), $text);
 $texts = explode("\xa5\xa5", $text);
 $wtext = $texts[0];
@@ -32,7 +33,7 @@ $text = str_replace("\177"."<br />\n","",$text);
 $text = str_replace("\177"."","",$text);
 
 // we're cutting the last <br />
-$text = preg_replace("/<br \/>$/", "", $text);//trim($text));
+$text = preg_replace("/<br \/>$/", "", $text); //trim($text));
 
 // close all open tables
 $opens = preg_match_all("/<table/", $text, $matches);
@@ -40,7 +41,7 @@ $closes = preg_match_all("/<\/table/", $text, $matches);
 
 if (1 * $closes < 1 * $opens)
 {
-	for ($i = 0; $i < (1 * $opens - 1 * $closes); $i++) 
+	for ($i = 0; $i < (1 * $opens - 1 * $closes); $i++)
 	{
 		$text .= "</table>";
 	}
