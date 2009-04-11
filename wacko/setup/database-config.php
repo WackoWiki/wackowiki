3d870c575735df6a27211728ee4e966492db88fd
vendeeglobe <54716082+vendeeglobe@users.noreply.github.com>
2009-04-11 11:18:03 +0000

    minor fix for label

--- wacko/setup/database-config.php
+++ wacko/setup/database-config.php
@@ -83,7 +83,7 @@ for($count = 0; $count < count($drivers); $count++)
    {
       if(extension_loaded($drivers[$count][0]))
          {
-            echo "      <li><input type=\"radio\" id=\"db_driver_".$drivers[$count][0]."\" name=\"config[database_driver]\" value=\"".$drivers[$count][1]."\"".($detected == 0 ? "checked=\"checked\"" : "")."><label for=\"db_driver_".$drivers[$count][1]."\">".$drivers[$count][2]."</label></li>\n";
+            echo "      <li><input type=\"radio\" id=\"db_driver_".$drivers[$count][0]."\" name=\"config[database_driver]\" value=\"".$drivers[$count][1]."\"".($detected == 0 ? "checked=\"checked\"" : "")."><label for=\"db_driver_".$drivers[$count][0]."\">".$drivers[$count][2]."</label></li>\n";
             $detected++;
          }
    }
