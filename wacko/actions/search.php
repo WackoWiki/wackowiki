fd6e8b6980dc6ebd63d66fdd1723286325362a20
vendeeglobe <54716082+vendeeglobe@users.noreply.github.com>
2009-04-15 10:12:19 +0000

    Update Bug 173: Extended Search

--- wacko/actions/search.php
+++ wacko/actions/search.php
@@ -1,11 +1,18 @@
 <?php
 
-if (($topic == 1) || ($title == 1)) $mode = "topic"; else $mode = "full";
+if (($topic == 1) || 
+	($title == 1)) $mode = "topic"; 
+else $mode = "full";
+
 if ($_GET["topic"] == "on") $mode = "topic";
+
 //if (!$delim) $delim="---";
 if (!in_array($style,array("br","ul","ol","comma"))) $style="ol";
+
 $i = 0;
+
 if ($filter!="pages") $filter = "all";
+if (!isset($clean)) $clean = false;
 
 if ($vars[0] != "") $phrase = $vars[0]; else { $phrase=""; $form=1; }
 
@@ -32,46 +39,136 @@ if ($phrase)
 {
 	if ($form) print "<br />";
 
-	if (strlen($phrase)>2)
+	if (strlen($phrase) >= 3)
 	{
 
 		if ($mode == "topic") $results = $this->TagSearch($phrase);
-		else $results = $this->FullTextSearch($phrase, ($filter=="all"?0:1));
+		else $results = $this->FullTextSearch($phrase, ($filter == "all" ? 0 : 1));
 		$phrase = htmlspecialchars($phrase);
+
+		// make and display results
 		if ($results)
 		{
 			if (!$nomark) print(  "<div class=\"layout-box\"><p class=\"layout-box\"><span>".
-			$this->GetTranslation(($mode=="topic"?"Topic":"")."SearchResults").
+			$this->GetTranslation(($mode == "topic" ? "Topic" : "")."SearchResults").
               " \"$phrase\":</span></p>");
-			if ($style=="ul") print "<ul class=\"SearchResults\">\n";
-			if ($style=="ol") print "<ol class=\"SearchResults\">\n";
+			// open list
+			if ($style == "ul") print "<ul id=\"search_results\">\n";
+			if ($style == "ol") print "<ol id=\"search_results\">\n";
 
 			foreach ($results as $page)
-			if (!$this->config["hide_locked"] || $this->HasAccess("read",$page["tag"]) )
 			{
-				// Don't show it if it's a comment and we're hiding comments from this user
-				if($page["comment_on"] == '' || ($page["comment_on"] != '' && $this->UserAllowedComments()))
+				if (!$this->config["hide_locked"] || $this->HasAccess("read",$page["tag"]) )
 				{
-					if ($style=="ul" || $style=="ol") print "<li>";
-					if ($style=="comma" && $i>0) print ",\n";
+					// Don't show it if it's a comment and we're hiding comments from this user
+					if($page["comment_on"] == '' || ($page["comment_on"] != '' && $this->UserAllowedComments()))
+					{
+						// open item
+						if ($style == "ul" || $style == "ol") print "<li>";
+						if ($style == "comma" && $i > 0) print ",\n";
 
-					print($this->Link("/".$page["tag"],"",$page["tag"]));
+					print("<h3>".$this->Link("/".$page["tag"],"",$page["tag"])."</h3>");
+					$context = getLineWithPhrase($phrase, $page["body"], $clean);
+					$context = preview_text($TEXT=$context, $LIMIT=500, $TAGS = 0);
+					$context = highlight_this($text=$context, $words=$phrase, $the_place=0);
+					print("<div>".str_replace("\n", '<br />', $context)."</div>");
 
-					if ($style=="br") print "<br />\n";
-					if ($style=="ul" || $style=="ol") print "</li>\n";
+					// close item
+					if ($style == "br") print "<br />\n";
+					if ($style == "ul" || $style == "ol") print "</li>\n";
 					$i++;
+					}
 				}
 			}
 
+			// close list
 			if ($style=="ul") print "</ul>";
 			if ($style=="ol") print "</ol>";
 			if (!$nomark) print("</div>");
 		}
 		else
-		if (!$nomark) echo $this->GetTranslation("NoResultsFor")."\"$phrase\".";
+			if (!$nomark) echo $this->GetTranslation("NoResultsFor")."\"$phrase\".";
 	}
 	else
-	if (!$nomark) echo $this->GetTranslation("NoResultsFor")."\"$phrase\".";
+	{
+		if (!$nomark) echo $this->GetTranslation("NoResultsFor")."\"$phrase\".";
+	}
 }
+
+function getLineWithPhrase($phrase, $string, $cleanup)
+{
+    $lines = split("\n", $string);
+    $result = "";
+    foreach ($lines as $line)
+    {
+        if (strpos($line, $phrase))
+        {
+            if ($result) $result .= "<br/>\n";
+            $result .= $cleanup ? str_replace("$phrase", "", $line) : $line;
+        }
+    }
+    return $result;
+}
+
+function preview_text($TEXT, $LIMIT, $TAGS = 0) {
+
+
+    // TRIM TEXT
+    $TEXT = trim($TEXT);
+
+    // STRIP TAGS IF PREVIEW IS WITHOUT HTML
+    if ($TAGS == 0) $TEXT = preg_replace('/\s\s+/', ' ', strip_tags($TEXT));
+
+    // IF STRLEN IS SMALLER THAN LIMIT RETURN
+    if (strlen($TEXT) < $LIMIT) return $TEXT;
+
+    if ($TAGS == 0) return substr($TEXT, 0, $LIMIT) . " ...";
+    else {
+
+        $COUNTER = 0;
+        for ($i = 0; $i<= strlen($TEXT); $i++) {
+
+            if ($TEXT{$i} == "<") $STOP = 1;
+
+            if ($STOP != 1) {
+
+                $COUNTER++;
+            }
+
+            if ($TEXT{$i} == ">") $STOP = 0;
+            $RETURN .= $TEXT{$i};
+
+            if ($COUNTER >= $LIMIT && $TEXT{$i} == " ") break;
+
+        }
+
+        return $RETURN . "...";
+    }
+
+}
+
+function highlight_this($text, $words, $the_place) {
+    $words = trim($words);
+    $the_count = 0;
+    $wordsArray = explode(' ', $words);
+        foreach($wordsArray as $word) {
+         if(strlen(trim($word)) != 0)
+        
+         //exclude these words from being replaced
+         $exclude_list = array("word1", "word2", "word3");
+        // Check if it's excluded
+        if ( in_array( strtolower($word), $exclude_list ) ) {
+   
+        } else {
+            $text = str_ireplace($word, "<span class=\"highlight\">".$word."</span>", $text, $count);
+            $the_count = $count + $the_count;
+            }
+           
+    }
+    //added to show how many keywords were found
+    #echo "<br><div class=\"emphasis\">A search for <strong>" . $words. "</strong> found <strong>" . $the_count . "</strong> matches within the " . $the_place. ".</div><br>";
+   
+    return $text;
+} 
 
 ?>
