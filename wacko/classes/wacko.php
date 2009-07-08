5630cb53af0c35f20097564874ac9bdda91cc0bc
vendeeglobe <54716082+vendeeglobe@users.noreply.github.com>
2009-07-08 11:26:00 +0000

    minor fix

--- wacko/classes/wacko.php
+++ wacko/classes/wacko.php
@@ -3461,8 +3461,8 @@ class Wacko
 				"SELECT title ".
 				"FROM {$this->config['table_prefix']}pages ".
 				"WHERE ".( $id != 0
-					? "id	= '".quote((int)$id)."' "
-					: "tag	= '".quote($tag)."' " ).
+					? "id	= '".quote($this->dblink, (int)$id)."' "
+					: "tag	= '".quote($this->dblink, $tag)."' " ).
 				"LIMIT 1");
 
 			if ($page['title'] == true)
@@ -3878,10 +3878,10 @@ class Wacko
 		// current timestamp set automatically
 		return $this->Query(
 			"INSERT INTO {$this->config['table_prefix']}log SET ".
-				"level		= '".quote($level)."', ".
-				"user		= '".quote( $user ? $user : GUEST )."', ".
-				"ip			= '".quote($this->GetUserIP())."', ".
-				"message	= '".quote($message)."'");
+				"level		= '".quote($this->dblink, $level)."', ".
+				"user		= '".quote($this->dblink, $user ? $user : GUEST )."', ".
+				"ip			= '".quote($this->dblink, $this->GetUserIP())."', ".
+				"message	= '".quote($this->dblink, $message)."'");
 	}
 }
 
