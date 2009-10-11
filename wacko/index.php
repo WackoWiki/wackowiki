cdccbd2f0be1dc7867fcb6a79185aeea7a7eea21
vendeeglobe <vendeeglobe@localhost>
2009-10-11 15:08:15 +0000

    revert changes from r362: reintroduced 'root_url', wee need root_url for the case if mode_rewrite is disabled or not available - part 1

--- wacko/index.php
+++ wacko/index.php
@@ -8,7 +8,8 @@ $init = new Init();
 $init->Settings();	// populate from config.php
 // $init->Settings();	// initialize DBAL and populate from config table (TODO: required for config table, not yet implemented)
 $init->DBAL();
-$init->Settings('theme_url',	$init->config['base_url'].'themes/'.$init->config['theme'].'/');
+$init->Settings('root_url',		(preg_replace("#/[^/]*$#","/",$init->config['base_url'])));
+$init->Settings('theme_url',	$init->config['root_url'].'themes/'.$init->config['theme'].'/');
 $init->Settings('user_table',	$init->config['table_prefix'].'users');
 
 if ($init->IsLocked() === true)
