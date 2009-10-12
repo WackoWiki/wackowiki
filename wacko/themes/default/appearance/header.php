9045979c56483a5679013ea22cd0fece90523903
vendeeglobe <vendeeglobe@localhost>
2009-10-12 19:14:35 +0000

    minor fix: default theme: Undefined offset: 1 in \wacko\formatters\wacko.php on line 28

--- wacko/themes/default/appearance/header.php
+++ wacko/themes/default/appearance/header.php
@@ -10,28 +10,28 @@ header("Content-Type: text/html; charset=".$this->GetCharset());
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->page["lang"] ?>" lang="<?php echo $this->page["lang"] ?>">
 <head>
-  <title><?php echo $this->GetWackoName()." : ".$this->AddSpaces($this->GetPageTag()).($this->method != "show" ? " (".$this->method.")" : "");?></title>
+	<title><?php echo $this->GetWackoName()." : ".$this->AddSpaces($this->GetPageTag()).($this->method != "show" ? " (".$this->method.")" : "");?></title>
 <?php
 // We don't need search robots to index subordinate pages
 if ($this->GetMethod() != 'show' || $this->page["latest"] == "0")
 	echo "   <meta name=\"robots\" content=\"noindex, nofollow\" />\n";
 ?>
-  <meta name="keywords" content="<?php echo $this->GetKeywords(); ?>" />
-  <meta name="description" content="<?php echo $this->GetDescription(); ?>" />
-  <meta name="language" content="<?php echo $this->page["lang"] ?>" />
-  <meta http-equiv="content-type" content="text/html; charset=<?php echo $this->GetCharset(); ?>" />
-  <link rel="stylesheet" type="text/css" href="<?php echo $this->GetConfigValue("theme_url") ?>css/default.css" />
-  <link media="print" rel="stylesheet" type="text/css" href="<?php echo $this->GetConfigValue("theme_url") ?>css/print.css" />
-  <link rel="shortcut icon" href="<?php echo $this->GetConfigValue("theme_url") ?>icons/favicon.ico" type="image/x-icon" />
-  <link title="<?php echo $this->GetConfigValue("root_page");?>" href="<?php echo $this->GetConfigValue("base_url");?>" rel="start"/>
-  <link rel="alternate" type="application/rss+xml" title="<?php echo $this->GetTranslation("RecentChangesRSS");?>" href="<?php echo $this->GetConfigValue("root_url");?>xml/recentchanges_<?php echo preg_replace("/[^a-zA-Z0-9]/", "", strtolower($this->GetConfigValue("wacko_name")));?>.xml" />
-  <link rel="alternate" type="application/rss+xml" title="<?php echo $this->GetTranslation("RecentCommentsRSS");?>" href="<?php echo $this->GetConfigValue("root_url");?>xml/recentcomment_<?php echo preg_replace("/[^a-zA-Z0-9]/", "", strtolower($this->GetConfigValue("wacko_name")));?>.xml" />
-  <link rel="alternate" type="application/rss+xml" title="<?php echo $this->GetTranslation("HistoryRevisionsRSS");?><?php echo $this->tag; ?>" href="<?php echo $this->href("revisions.xml");?>" />
+	<meta name="keywords" content="<?php echo $this->GetKeywords(); ?>" />
+	<meta name="description" content="<?php echo $this->GetDescription(); ?>" />
+	<meta name="language" content="<?php echo $this->page["lang"] ?>" />
+	<meta http-equiv="content-type" content="text/html; charset=<?php echo $this->GetCharset(); ?>" />
+	<link rel="stylesheet" type="text/css" href="<?php echo $this->GetConfigValue("theme_url") ?>css/default.css" />
+	<link media="print" rel="stylesheet" type="text/css" href="<?php echo $this->GetConfigValue("theme_url") ?>css/print.css" />
+	<link rel="shortcut icon" href="<?php echo $this->GetConfigValue("theme_url") ?>icons/favicon.ico" type="image/x-icon" />
+	<link title="<?php echo $this->GetConfigValue("root_page");?>" href="<?php echo $this->GetConfigValue("base_url");?>" rel="start"/>
+	<link rel="alternate" type="application/rss+xml" title="<?php echo $this->GetTranslation("RecentChangesRSS");?>" href="<?php echo $this->GetConfigValue("root_url");?>xml/recentchanges_<?php echo preg_replace("/[^a-zA-Z0-9]/", "", strtolower($this->GetConfigValue("wacko_name")));?>.xml" />
+	<link rel="alternate" type="application/rss+xml" title="<?php echo $this->GetTranslation("RecentCommentsRSS");?>" href="<?php echo $this->GetConfigValue("root_url");?>xml/recentcomment_<?php echo preg_replace("/[^a-zA-Z0-9]/", "", strtolower($this->GetConfigValue("wacko_name")));?>.xml" />
+	<link rel="alternate" type="application/rss+xml" title="<?php echo $this->GetTranslation("HistoryRevisionsRSS");?><?php echo $this->tag; ?>" href="<?php echo $this->href("revisions.xml");?>" />
 <?php
 // JS files.
 // default.js contains common procedures and should be included everywhere
 ?>
-  <script type="text/javascript" src="<?php echo $this->GetConfigValue("root_url");?>js/default.js"></script>
+	<script type="text/javascript" src="<?php echo $this->GetConfigValue("root_url");?>js/default.js"></script>
 <?php
 // autocomplete.js, protoedit & wikiedit2.js contain classes for WikiEdit editor. We may include them only on method==edit pages.
 if ($this->method == 'edit')
@@ -41,37 +41,37 @@ if ($this->method == 'edit')
 	echo "  <script type=\"text/javascript\" src=\"".$this->GetConfigValue("root_url")."js/autocomplete.js\"></script>\n";
 }
 ?>
-  <script type="text/javascript" src="<?php echo $this->GetConfigValue("root_url");?>js/captcha.js"></script>
+	<script type="text/javascript" src="<?php echo $this->GetConfigValue("root_url");?>js/captcha.js"></script>
 <?php
 // Doubleclick edit feature.
 // Enabled only for registered users who don't swith it off (requires class=page in show handler).
 if ($user = $this->GetUser())
-   {
-      if ($user["doubleclickedit"] == "1")
-         {
+{
+	if ($user["doubleclickedit"] == "1")
+	{
 ?>
-  <script type="text/javascript">
-   var edit = "<?php echo $this->href("edit");?>";
-  </script>
+	<script type="text/javascript">
+	var edit = "<?php echo $this->href("edit");?>";
+	</script>
 <?php
-         }
-   }
+	}
+}
 else if($this->HasAccess("write"))
-   {
+{
 ?>
 
-      <script type="text/javascript">
-      var edit = "<?php echo $this->href("edit");?>";
-     </script>
+	<script type="text/javascript">
+	var edit = "<?php echo $this->href("edit");?>";
+	</script>
 <?php
-   }
+}
 ?>
 </head>
 <?php
 // all_init() initializes all js features:
-//   * WikiEdit
-//   * Doubleclick editing
-//   * Smooth scrolling
+//	* WikiEdit
+//	* Doubleclick editing
+//	* Smooth scrolling
 
 ?>
 <body onload="all_init();">
@@ -85,19 +85,19 @@ else if($this->HasAccess("write"))
 <?php
 // If user are logged, Wacko shows "You are UserName"
 if ($this->GetUser())
-   { ?> <span class="nobr"><?php echo $this->GetTranslation("YouAre")." ".$this->Link($this->GetUserName()) ?></span><small> ( <span class="nobr Tune"><?php
+{ ?> <span class="nobr"><?php echo $this->GetTranslation("YouAre")." ".$this->Link($this->GetUserName()) ?></span><small> ( <span class="nobr Tune"><?php
 echo $this->ComposeLinkToPage($this->GetTranslation("YouArePanelLink"), "", $this->GetTranslation("YouArePanelName"), 0); ?>
  | <a onclick="return confirm('<?php echo $this->GetTranslation("LogoutAreYouSure");?>');" href="<?php echo $this->Href("",$this->GetTranslation("LoginPage")).($this->config["rewrite_mode"] ? "?" : "&amp;");?>action=logout&amp;goback=<?php echo $this->SlimUrl($this->tag);?>"><?php echo $this->GetTranslation("LogoutLink"); ?></a></span>
 )</small>
 <?php
 // Else Wacko shows login's controls
-   }
+}
 else
-   {
-   // Show Register / Login link
+{
+	// Show Register / Login link
 	echo "<ul>\n<li>".$this->ComposeLinkToPage($this->GetTranslation("LoginPage").($this->config["rewrite_mode"] ? "?" : "&amp;")."goback=".$this->SlimUrl($this->tag), "", $this->GetTranslation("LoginPage"), 0)."</li>\n";
 	echo "<li>".$this->ComposeLinkToPage($this->GetTranslation("RegistrationPage"), "", $this->GetTranslation("RegistrationPage"), 0)."</li>\n</ul>";
-   }
+}
 
 // End if
 ?></div>
@@ -111,10 +111,8 @@ else
 		echo "<li>".$this->ComposeLinkToPage($this->config["root_page"])."</li>\n";
 		echo "<li>";
 		// Bookmarks
-		$BMs = $this->GetBookmarks();
-		$formatedBMs =  $this->Format($this->Format(implode("| ", $BMs), "wacko"), "post_wacko");
-		$formatedBMs = str_replace ( "| ", "</li>\n<li>", $formatedBMs );
-		//echo "<ol><li>".$formatedBMs."</li></ol>";
+		$formatedBMs = $this->Format($this->GetBookmarksFormatted(), "post_wacko");
+		$formatedBMs = str_replace (" | ", "</li>\n<li>", $formatedBMs);
 		echo $formatedBMs;
 
 		echo "</li>\n";
