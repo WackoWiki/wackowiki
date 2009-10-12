e526dd6028e690d9127509b29bd60ebe311a9151
vendeeglobe <vendeeglobe@localhost>
2009-10-12 19:54:13 +0000

    minor fix

--- wacko/classes/init.php
+++ wacko/classes/init.php
@@ -29,34 +29,34 @@
 
 */
 
-define("BM_AUTO", 						0);
-define("BM_USER", 						1);
-define("BM_DEFAULT", 					2);
+define("BM_AUTO",						0);
+define("BM_USER",						1);
+define("BM_DEFAULT",					2);
 define("CACHE_FEED_DIR",				"feeds/");
 define("CACHE_PAGE_DIR",				"pages/");
 define("CACHE_SQL_DIR",					"queries/");
 define("GUEST",							"guest@wacko");
 // define("INTERCOM_MAX_SIZE",				262144);
-define("LOAD_NOCACHE", 					0);
-define("LOAD_CACHE", 					1);
-define("LOAD_ALL", 						0);
-define("LOAD_META", 					1);
+define("LOAD_NOCACHE",					0);
+define("LOAD_CACHE",					1);
+define("LOAD_ALL",						0);
+define("LOAD_META",						1);
 // define("SESSION_HANDLER_ID",			"sid");
 // define("SESSION_HANDLER_PATH",			NULL);	// if you are using specific path (instead of system default /tmp) for session variables storing, define it here
 // define("SQL_NULLDATE",					"0000-00-00 00:00:00");
 // define("SQL_DATE_FORMAT",				"Y-m-d H:i:s");
-define("TRAN_DONTCHANGE", 				0);
-define("TRAN_LOWERCASE", 				1);
-define("TRAN_LOAD", 					0);
-define("TRAN_DONTLOAD", 				1);
+define("TRAN_DONTCHANGE",				0);
+define("TRAN_LOWERCASE",				1);
+define("TRAN_LOAD",						0);
+define("TRAN_DONTLOAD",					1);
 
 // do not change this two lines, PLEASE-PLEASE. In fact, don't change anything! Ever!
 define("WAKKA_VERSION",					"0.1.2");
 define("WACKO_VERSION",					"R4.3.rc2");
 define("XML_HTMLSAX3",					"lib/HTMLSax3/");
-#define('XML_HTMLSAX3', 					dirname(__FILE__)."/lib/HTMLSax3/");
-define("ACTIONS4DIFF", "a, anchor, toc"); //allowed actions in DIFF
-define("PHP_MIN_VERSION", "5.1.0"); //minimum required PHP version
+#define('XML_HTMLSAX3',					dirname(__FILE__)."/lib/HTMLSax3/");
+define("ACTIONS4DIFF",					"a, anchor, toc"); //allowed actions in DIFF
+define("PHP_MIN_VERSION",				"5.1.0"); //minimum required PHP version
 
 class Init
 {
@@ -548,7 +548,7 @@ class Init
 		}
 		else if ($this->cache == true && $op == "log")
 		{
-			return $this->cache->Log("Before Run WackoWiki=".$this->engine->config["WackoWiki_version"]);
+			return $this->cache->Log("Before Run WackoWiki=".$this->engine->config["wacko_version"]);
 		}
 		else
 		{
