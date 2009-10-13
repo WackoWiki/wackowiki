436752401306556d04a16b56313325ee248b4176
vendeeglobe <54716082+vendeeglobe@users.noreply.github.com>
2009-10-13 02:14:41 +0000

    moved XML feeds in own class -> 'rss'

--- wacko/classes/wacko.php
+++ wacko/classes/wacko.php
@@ -1384,13 +1384,27 @@ class Wacko
 		}
 
 		// writing xmls
-		$this->WriteRecentChangesXML();
-		$this->WriteRecentCommentsXML();
 
-		if($this->config["xml_sitemap"])
-		{
-			$this->WriteSiteMapXML();
-		}
+		#if ($mute === false)
+		#{
+			#if (!$oldPage['comment_on'] || !$comment_on)
+			#{
+				$this->UseClass('rss', 'classes/');
+				$xml = new RSS($this);
+				$xml->Changes();
+				$xml->Comments();
+
+				#if (substr($this->tag, 0, strlen($this->config['news_cluster'].'/')) == $this->config['news_cluster'].'/')
+				#	$xml->News();
+
+				if($this->config["xml_sitemap"])
+				{
+					$this->SiteMap();
+				}
+
+				unset($xml);
+			#}
+		#}
 
 		return $body_r;
 	}
@@ -2317,7 +2331,7 @@ class Wacko
 	{
 		if (!class_exists($class_name))
 		{
-			if ($file_name == "") $file_name = $class_name;
+			if ($file_name == "") $file_name = strtolower($class_name);
 			if ($class_dir == "") $class_dir = $this->classes_dir;
 
 			$class_file = $class_dir.$file_name.".php";
@@ -2839,165 +2853,6 @@ class Wacko
 		while ($replaced > 0);
 
 		return $acl;
-	}
-
-	// XML
-	function WriteFile($name, $body)
-	{
-		$filename = "xml/".$name."_".preg_replace("/[^a-zA-Z0-9]/", "", strtolower($this->config["wacko_name"])).".xml";
-
-		$fp = fopen($filename, "w");
-		if ($fp)
-		{
-			fwrite($fp, $body);
-			fclose($fp);
-		}
-
-		@chmod($filename, 0644);
-	}
-
-	function WriteRecentChangesXML()
-	{
-		$name = "recentchanges";
-
-		$xml = "<?xml version=\"1.0\" encoding=\"".$this->GetCharset()."\"?>\n";
-		$xml .= "<rss version=\"2.0\" xmlns:dc=\"http://purl.org/dc/elements/1.1/\">\n";
-		$xml .= "<channel>\n";
-		$xml .= "<title>".$this->config["wacko_name"].$this->GetTranslation("RecentChangesTitleXML")."</title>\n";
-		$xml .= "<link>".$this->config["root_url"]."</link>\n";
-		$xml .= "<description>".$this->GetTranslation("RecentChangesXML").$this->config["wacko_name"]." </description>\n";
-		$xml .= "<lastBuildDate>".date('r')."</lastBuildDate>\n";
-		$xml .= "<image>\n";
-		$xml .= "<title>".$this->config["wacko_name"].$this->GetTranslation("RecentCommentsTitleXML")."</title>\n";
-		$xml .= "<link>".$this->config["root_url"]."</link>\n";
-		$xml .= "<url>".$this->config["root_url"]."files/wacko4.gif"."</url>\n";
-		$xml .= "<width>108</width>\n";
-		$xml .= "<height>50</height>\n";
-		$xml .= "</image>\n";
-		$xml .= "<language>en-us</language>\n";
-		$xml .= "<docs>http://blogs.law.harvard.edu/tech/rss</docs>\n";
-		$xml .= "<generator>WackoWiki ".WACKO_VERSION."</generator>\n";//!!!
-
-		if ($pages = $this->LoadRecentlyChanged())
-		{
-			foreach ($pages as $i => $page)
-			{
-				if ($this->config["hide_locked"]) $access =$this->HasAccess("read",$page["tag"],"guest@wacko");
-				if ($access && ($count < 30))
-				{
-					$count++;
-					$xml .= "<item>\n";
-					$xml .= "<title>".$page["tag"]."</title>\n";
-					$xml .= "<link>".$this->href("show", $page["tag"], "time=".urlencode($page["time"]))."</link>\n";
-					$xml .= "<guid>".$this->href("show", $page["tag"], "time=".urlencode($page["time"]))."</guid>\n";
-					$xml .= "<pubDate>".date('r', strtotime($page['time']))."</pubDate>\n";
-					$xml .= "<description>".$page["time"]." ".$this->GetTranslation("By")." ".$page["user"].($page["edit_note"] ? " [".$page["edit_note"]."]" : "")."</description>\n";
-					$xml .= "</item>\n";
-				}
-			}
-		}
-
-		$xml .= "</channel>\n";
-		$xml .= "</rss>\n";
-
-		$this->WriteFile($name, $xml);
-	}
-
-	function WriteRecentCommentsXML()
-	{
-		$name = "recentcomment";
-
-		$xml = "<?xml version=\"1.0\" encoding=\"".$this->GetCharset()."\"?>\n";
-		$xml .= "<?xml-stylesheet type=\"text/css\" href=\"".$this->config["theme_url"]."css/wacko.css\" media=\"screen\"?>\n";
-		$xml .= "<rss version=\"2.0\" xmlns:content=\"http://purl.org/rss/1.0/modules/content/\" xmlns:dc=\"http://purl.org/dc/elements/1.1/\">\n";
-		$xml .= "<channel>\n";
-		$xml .= "<title>".$this->config["wacko_name"].$this->GetTranslation("RecentCommentsTitleXML")."</title>\n";
-		$xml .= "<link>".$this->config["root_url"]."</link>\n";
-		$xml .= "<description>".$this->GetTranslation("RecentCommentsXML").$this->config["wacko_name"]." </description>\n";
-		$xml .= "<lastBuildDate>".date('r')."</lastBuildDate>\n";
-		$xml .= "<image>\n";
-		$xml .= "<title>".$this->config["wacko_name"].$this->GetTranslation("RecentCommentsTitleXML")."</title>\n";
-		$xml .= "<link>".$this->config["root_url"]."</link>\n";
-		$xml .= "<url>".$this->config["root_url"]."files/wacko4.gif"."</url>\n";
-		$xml .= "<width>108</width>\n";
-		$xml .= "<height>50</height>\n";
-		$xml .= "</image>\n";
-		$xml .= "<language>en-us</language>\n";
-		$xml .= "<docs>http://blogs.law.harvard.edu/tech/rss</docs>\n";
-		$xml .= "<generator>WackoWiki ".WACKO_VERSION."</generator>\n";//!!!
-
-		if ( $pages = $this->LoadRecentlyComment() ) {
-			foreach ($pages as $i => $page) {
-				if ($this->config["hide_locked"]) $access =$this->HasAccess("read",$page["tag"],"guest@wacko");
-				if ( $access && ($count < 30) ) {
-					$count++;
-					$xml .= "<item>\n";
-					$xml .= "<title>".$page["tag"]." ".$this->GetTranslation("To")." ".$this->GetCommentOnTag($page["comment_on_id"])." ".$this->GetTranslation("From")." ".$page["user"]."</title>\n";
-					$xml .= "<link>".$this->href("show", $page["tag"], "time=".urlencode($page["time"]))."</link>\n";
-					$xml .= "<guid>".$this->href("show", $page["tag"], "time=".urlencode($page["time"]))."</guid>\n";
-					$xml .= "<pubDate>".date('r', strtotime($page['time']))."</pubDate>\n";
-					$xml .= "<dc:creator>".$page["user"]."</dc:creator>\n";
-					$text = $this->Format($page["body_r"], "post_wacko");
-					$xml .= "<description><![CDATA[".str_replace("]]>", "]]&gt;", $text)."]]></description>\n";
-					#$xml .= "<content:encoded><![CDATA[".str_replace("]]>", "]]&gt;", $text)."]]></content:encoded>\n";
-					$xml .= "</item>\n";
-				}
-			}
-		}
-
-		$xml .= "</channel>\n";
-		$xml .= "</rss>\n";
-
-		$this->WriteFile($name, $xml);
-	}
-
-	function WriteSiteMapXML()
-	{
-		$xml = "<?xml version=\"1.0\" encoding=\"windows-1251\"?>\n";
-		$xml .= "<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\">\n";
-
-		if ($pages = $this->LoadAllPagesByTime())
-		{
-			foreach ($pages as $i => $page)
-			{
-				if ($this->config["hide_locked"] ? $this->HasAccess("read",$page["tag"],"guest@wacko") : true)
-				{
-					$xml .= "<url>\n";
-					$xml .= "<loc>".$this->href("", $page["tag"])."</loc>\n";
-					$xml .= "<lastmod>". substr($page["time"], 0, 10) ."</lastmod>\n";
-
-					$daysSinceLastChanged = floor((time() - strtotime(substr($page["time"], 0, 10)))/86400);
-
-					if($daysSinceLastChanged < 30)
-					{
-						$xml .= "<changefreq>daily</changefreq>\n";
-					}
-					else if($daysSinceLastChanged < 60)
-					{
-						$xml .= "<changefreq>monthly</changefreq>\n";
-					}
-					else
-					{
-						$xml .= "<changefreq>yearly</changefreq>\n";
-					}
-
-					// The only thing I'm not sure about how to handle dynamically...
-					$xml .= "<priority>0.8</priority>\n";
-					$xml .= "</url>\n";
-				}
-			}
-		}
-
-		$xml .= "</urlset>\n";
-
-		$filename = "sitemap.xml";
-
-		$fp = @fopen($filename, "w");
-		if ($fp)
-		{
-			fwrite($fp, $xml);
-			fclose($fp);
-		}
 	}
 
 	// WATCHES
