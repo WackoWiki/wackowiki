6e8c83dae6f2bd1e11a4c02d9b4e1c57db9aeb1d
vendeeglobe <54716082+vendeeglobe@users.noreply.github.com>
2009-10-15 20:53:02 +0000

    update mychangeswatches action

--- wacko/actions/mychangeswatches.php
+++ wacko/actions/mychangeswatches.php
@@ -1,24 +1,50 @@
 <?php
 
-if (!$max) $max = 50;
-
-$str = "SELECT p.tag, w.user ".
-		"FROM ".$this->config["table_prefix"]."pages AS p, ".$this->config["table_prefix"]."pagewatches AS w ".
-		"WHERE p.tag = w.tag ".
-			"AND p.time > w.time ".
-			"AND w.user='".quote($this->dblink, $this->GetUserName())."'".
-			"AND p.user<>'".quote($this->dblink, $this->GetUserName())."' ".
-		"LIMIT ".(int)$max;
-
-if ($pages = $this->LoadAll($str))
+if ($user_id = $this->GetUserId())
 {
-	$s = '';
-	foreach ($pages as $page)
-	if (!$this->config["hide_locked"] || $this->HasAccess("read",$page["tag"]))
-	{
-		$s = $this->Link("/".$page["tag"],"",$page["tag"])."<br />\n";
+	$limit	= 100;
+	$pref	= $this->config['table_prefix'];
+	
+	echo $this->GetTranslation('MyChangesWatches').
+		' (<a href="'.$this->href('', '', 'mode=mychangeswatches&amp;reset=1').'#list">'.
+		$this->GetTranslation('ResetChangesWatches').'</a>).<br /><br />';
 
-		if ($s != '') echo $s;
+	$pages = $this->LoadAll(
+			"SELECT p.id, p.tag, p.time, w.user_id ".
+			"FROM {$pref}pages AS p, {$pref}pagewatches AS w ".
+			"WHERE p.id = w.page_id ".
+				"AND p.time  > w.time ".
+				"AND w.user_id  = '".quote($this->dblink, $user_id)."' ".
+				"AND p.user_id <> '".quote($this->dblink, $user_id)."' ".
+			"GROUP BY p.tag ".
+			"ORDER BY p.time DESC, p.tag ASC ".
+			"LIMIT $limit");
+	
+	if ($_GET['reset'] == 1 && $pages == true)
+	{
+		foreach ($pages as $page)
+			$this->Query(
+				"UPDATE {$this->config['table_prefix']}pagewatches ".
+				"SET time = NOW() ".
+				"WHERE page_id = '".quote($this->dblink, $page['id'])."' ".
+					"AND user_id = '".quote($this->dblink, $user_id)."'");
+		$this->Redirect($this->href('', '', 'mode=mychangeswatches').'#list');
 	}
+
+	if ($pages == true)
+	{
+		foreach ($pages as $page)
+			if (!$this->config['hide_locked'] || $this->HasAccess('read', $page['tag']))
+				echo '<small>('.$this->ComposeLinkToPage($page['tag'], 'revisions', $page['time'], 0, $this->GetTranslation("History")).
+					')</small> '.$this->ComposeLinkToPage($page['tag'], '', '', 0)."<br />\n";
+	}
+	else
+	{
+		echo '<em>'.$this->GetTranslation('NoChangesWatches').'</em>';
+	}
+}
+else
+{
+	echo '<em>'.$this->GetTranslation('NotLoggedInWatches').'</em>';
 }
 ?>
