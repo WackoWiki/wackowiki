42394e7b2db1ce2937c74d1070afdfa298aafce5
vendeeglobe <vendeeglobe@localhost>
2009-10-16 04:34:29 +0000

    fixed broken SQL query in watchers action

--- wacko/actions/watchers.php
+++ wacko/actions/watchers.php
@@ -1,34 +1,44 @@
 <?php
 
 if ($vars[0] && $vars[0]!=$vars["nomark"])
-$tag = $this->UnwrapLink( $vars[0] );
+	$tag = $this->UnwrapLink( $vars[0] );
 else
-$tag = $this->getPageTag();
-if ($this->UserIsOwner($tag)){
+	$tag = $this->getPageTag();
+	$page_id = $this->getPageId();
+if ($this->UserIsOwner($tag))
+{
 
-	$watchers = $this->LoadAll("SELECT * FROM ".$this->config["table_prefix"]."pagewatches WHERE tag = '".quote($this->dblink, $tag)."' GROUP BY tag");
-	if ($watchers){
+	$watchers = $this->LoadAll(
+		"SELECT * ".
+		"FROM ".$this->config["table_prefix"]."pagewatches ".
+		"WHERE page_id = '".quote($this->dblink, $page_id)."' ".
+		"");
 
+	if ($watchers)
+	{
 		$title = $this->GetTranslation("Watchers");
 		$title = str_replace("%1",  $this->Link("/".$tag,"",$tag),  $title);
 		if (!$nomark)
-		echo "<div class=\"layout-box\"><p class=\"layout-box\"><span>".$title.":</span></p>\n";
+			echo "<div class=\"layout-box\"><p class=\"layout-box\"><span>".$title.":</span></p>\n";
 
 		foreach ($watchers as $watcher)
 		{
-			$user = $watcher["user"];
+			$user = $this->GetUserNameById($watcher["user_id"]);
 			echo $this->Link("/".$user, "", $user)."<br />";
 		}
 		if (!$nomark)
-		echo "</div>\n";
+			echo "</div>\n";
 	}
 	else
 	{
 		if (!$nomark)
-		echo str_replace("%1",  $this->Link("/".$tag,"",$tag), $this->GetTranslation("NoWatchers"));
+			echo str_replace("%1",  $this->Link("/".$tag,"",$tag), $this->GetTranslation("NoWatchers"));
 	}
-}else{
-	if (!$nomark)
-	echo str_replace("%1",  $this->Link("/".$tag,"",$tag), $this->GetTranslation("NotOwnerAndViewWatchers"));
 }
+else
+{
+	if (!$nomark)
+		echo str_replace("%1",  $this->Link("/".$tag,"",$tag), $this->GetTranslation("NotOwnerAndViewWatchers"));
+}
+
 ?>
