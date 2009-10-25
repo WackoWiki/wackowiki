b87f9b198fd814a63fde8b50757cb03b0b4c36b5
vendeeglobe <vendeeglobe@localhost>
2009-10-25 07:43:29 +0000

    added RSS 'news' channel

--- wacko/themes/wakka/appearance/header.php
+++ wacko/themes/wakka/appearance/header.php
@@ -1,8 +1,8 @@
 <?php
 /*
- Default theme.
- Common header file.
- */
+	Default theme.
+	Common header file.
+*/
 
 // HTTP header with right Charset settings
 header("Content-Type: text/html; charset=".$this->GetCharset());
@@ -16,15 +16,16 @@ header("Content-Type: text/html; charset=".$this->GetCharset());
 if ($this->GetMethod() != 'show' || $this->page["latest"] == "0")
 echo "  <meta name=\"robots\" content=\"noindex, nofollow\" />\n";
 ?>
-  <meta name="keywords" content="<?php echo $this->GetKeywords(); ?>" />
-  <meta name="description" content="<?php echo $this->GetDescription(); ?>" />
-  <meta name="language" content="<?php echo $this->page["lang"] ?>" />
-  <meta http-equiv="content-type" content="text/html; charset=<?php echo $this->GetCharset(); ?>" />
-  <link rel="stylesheet" type="text/css" href="<?php echo $this->GetConfigValue("theme_url") ?>css/default.css" />
-  <link rel="shortcut icon" href="<?php echo $this->GetConfigValue("theme_url") ?>icons/favicon.ico" type="image/x-icon" />
-  <link rel="alternate" type="application/rss+xml" title="<?php echo $this->GetTranslation("RecentChangesRSS");?>" href="<?php echo $this->GetConfigValue("root_url");?>xml/changes_<?php echo preg_replace("/[^a-zA-Z0-9]/", "", strtolower($this->GetConfigValue("wacko_name")));?>.xml" />
-  <link rel="alternate" type="application/rss+xml" title="<?php echo $this->GetTranslation("RecentCommentsRSS");?>" href="<?php echo $this->GetConfigValue("root_url");?>xml/comments_<?php echo preg_replace("/[^a-zA-Z0-9]/", "", strtolower($this->GetConfigValue("wacko_name")));?>.xml" />
-  <link rel="alternate" type="application/rss+xml" title="<?php echo $this->GetTranslation("HistoryRevisionsRSS");?><?php echo $this->tag; ?>" href="<?php echo $this->href("revisions.xml");?>" />
+	<meta name="keywords" content="<?php echo $this->GetKeywords(); ?>" />
+	<meta name="description" content="<?php echo $this->GetDescription(); ?>" />
+	<meta name="language" content="<?php echo $this->page["lang"] ?>" />
+	<meta http-equiv="content-type" content="text/html; charset=<?php echo $this->GetCharset(); ?>" />
+	<link rel="stylesheet" type="text/css" href="<?php echo $this->GetConfigValue("theme_url") ?>css/default.css" />
+	<link rel="shortcut icon" href="<?php echo $this->GetConfigValue("theme_url") ?>icons/favicon.ico" type="image/x-icon" />
+	<link rel="alternate" type="application/rss+xml" title="<?php echo $this->GetTranslation("RecentChangesRSS");?>" href="<?php echo $this->GetConfigValue("root_url");?>xml/changes_<?php echo preg_replace("/[^a-zA-Z0-9]/", "", strtolower($this->GetConfigValue("wacko_name")));?>.xml" />
+	<link rel="alternate" type="application/rss+xml" title="<?php echo $this->GetTranslation("RecentCommentsRSS");?>" href="<?php echo $this->GetConfigValue("root_url");?>xml/comments_<?php echo preg_replace("/[^a-zA-Z0-9]/", "", strtolower($this->GetConfigValue("wacko_name")));?>.xml" />
+	<link rel="alternate" type="application/rss+xml" title="<?php echo $this->GetTranslation("RecentNewsRSS");?>" href="<?php echo $this->GetConfigValue("root_url");?>xml/news_<?php echo preg_replace("/[^a-zA-Z0-9]/", "", strtolower($this->GetConfigValue("wacko_name")));?>.xml" />
+<link rel="alternate" type="application/rss+xml" title="<?php echo $this->GetTranslation("HistoryRevisionsRSS");?><?php echo $this->tag; ?>" href="<?php echo $this->href("revisions.xml");?>" />
 <?php
 // JS files.
 // default.js contains common procedures and should be included everywhere
@@ -39,43 +40,43 @@ if ($this->method == 'edit')
 	echo "  <script type=\"text/javascript\" src=\"".$this->GetConfigValue("root_url")."js/autocomplete.js\"></script>\n";
 }
 ?>
-  <script type="text/javascript" src="<?php echo $this->GetConfigValue("root_url");?>js/swfobject.js"></script>
-  <script type="text/javascript" src="<?php echo $this->GetConfigValue("root_url");?>js/captcha.js"></script>
-  <script type="text/javascript">
+	<script type="text/javascript" src="<?php echo $this->GetConfigValue("root_url");?>js/swfobject.js"></script>
+	<script type="text/javascript" src="<?php echo $this->GetConfigValue("root_url");?>js/captcha.js"></script>
+	<script type="text/javascript">
 <?php
 // Doubleclick edit feature.
 // Enabled only for registered users who don't swith it off.
 if ($user = $this->GetUser())
-   {
-      if($user["doubleclickedit"] == "1")
-         {
+{
+	if($user["doubleclickedit"] == "1")
+	{
 ?>
-         var edit = "<?php echo $this->href("edit");?>";
+		var edit = "<?php echo $this->href("edit");?>";
 <?php
-         }
-   }
+	}
+}
 else if($this->HasAccess("write"))
-   {
+{
 ?>
-         var edit = "<?php echo $this->href("edit");?>";
+		var edit = "<?php echo $this->href("edit");?>";
 <?php
-   }
+}
 ?>
 	</script>
 </head>
 <?php
 // all_init() initializes all js features:
-//   * WikiEdit
-//   * Doubleclick editing
-//   * Smooth scrolling
+//	* WikiEdit
+//	* Doubleclick editing
+//	* Smooth scrolling
 ?>
 <body onload="all_init();">
 <?php
 // Begin Login form
 echo $this->FormOpen("", $this->GetTranslation("LoginPage"), "post"); ?>
-      <input type="hidden" name="action" value="login" />
-      <div class="header">
-         <h1><span class="main"><?php echo $this->config["wacko_name"] ?>: </span><?php echo $this->GetPagePath(); ?> <a class="Search" title="<?php echo $this->GetTranslation("SearchTitleTip")?>" href="<?php echo $this->config["base_url"].$this->GetTranslation("TextSearchPage").($this->config["rewrite_mode"] ? "?" : "&amp;");?>phrase=<?php echo urlencode($this->GetPageTag()); ?>">...</a></h1>
+	<input type="hidden" name="action" value="login" />
+	<div class="header">
+		<h1><span class="main"><?php echo $this->config["wacko_name"] ?>: </span><?php echo $this->GetPagePath(); ?> <a class="Search" title="<?php echo $this->GetTranslation("SearchTitleTip")?>" href="<?php echo $this->config["base_url"].$this->GetTranslation("TextSearchPage").($this->config["rewrite_mode"] ? "?" : "&amp;");?>phrase=<?php echo urlencode($this->GetPageTag()); ?>">...</a></h1>
 <?php
 // Outputs Bookmarks AKA QuickLinks
 // Main page
@@ -84,30 +85,30 @@ echo $this->ComposeLinkToPage($this->config["root_page"]); ?> | <?php
 echo $this->Format($this->GetBookmarksFormatted(), "post_wacko"); ?> | <?php
 // Here Wacko determines what it should show: "add to Bookmarks" or "remove from Bookmarks" icon
 if($this->GetUser())
-   {
-      if(!in_array($this->GetPageSuperTag(),$this->GetBookmarkLinks()))
-         {?> <a href="<?php echo $this->Href('', '', "addbookmark=yes")?>"><img src="<?php echo $this->GetConfigValue("theme_url") ?>icons/bookmark1.gif" alt="+" title="<?php echo $this->GetTranslation("AddToBookmarks") ?>" /></a>
-| <?php
-         }
-      else
-         { ?> <a href="<?php echo $this->Href('', '', "removebookmark=yes")?>"><img src="<?php echo $this->GetConfigValue("theme_url") ?>icons/bookmark2.gif" alt="-" title="<?php echo $this->GetTranslation("RemoveFromBookmarks") ?>" /></a>
-| <?php
-         }
-   }
+	{
+		if(!in_array($this->GetPageSuperTag(),$this->GetBookmarkLinks()))
+		{?> <a href="<?php echo $this->Href('', '', "addbookmark=yes")?>"><img src="<?php echo $this->GetConfigValue("theme_url") ?>icons/bookmark1.gif" alt="+" title="<?php echo $this->GetTranslation("AddToBookmarks") ?>" /></a>
+			| <?php
+		}
+		else
+		{ ?> <a href="<?php echo $this->Href('', '', "removebookmark=yes")?>"><img src="<?php echo $this->GetConfigValue("theme_url") ?>icons/bookmark2.gif" alt="-" title="<?php echo $this->GetTranslation("RemoveFromBookmarks") ?>" /></a>
+	| <?php
+		}
+}
 
 // If user are logged, Wacko shows "You are UserName"
 if ($this->GetUser())
-   { ?> <span class="nobr"><?php echo $this->GetTranslation("YouAre")." ".$this->Link($this->GetUserName()) ?></span><small> ( <span class="nobr Tune"><?php
-echo $this->ComposeLinkToPage($this->GetTranslation("YouArePanelLink"), "", $this->GetTranslation("YouArePanelName"), 0); ?>
- | <a onclick="return confirm('<?php echo $this->GetTranslation("LogoutAreYouSure");?>');" href="<?php echo $this->Href("",$this->GetTranslation("LoginPage")).($this->config["rewrite_mode"] ? "?" : "&amp;");?>action=logout&amp;goback=<?php echo $this->SlimUrl($this->tag);?>"><?php echo $this->GetTranslation("LogoutLink"); ?></a></span>
+{ ?> <span class="nobr"><?php echo $this->GetTranslation("YouAre")." ".$this->Link($this->GetUserName()) ?></span><small> ( <span class="nobr Tune"><?php
+	echo $this->ComposeLinkToPage($this->GetTranslation("YouArePanelLink"), "", $this->GetTranslation("YouArePanelName"), 0); ?>
+	 | <a onclick="return confirm('<?php echo $this->GetTranslation("LogoutAreYouSure");?>');" href="<?php echo $this->Href("",$this->GetTranslation("LoginPage")).($this->config["rewrite_mode"] ? "?" : "&amp;");?>action=logout&amp;goback=<?php echo $this->SlimUrl($this->tag);?>"><?php echo $this->GetTranslation("LogoutLink"); ?></a></span>
 )</small> <?php
 // Else Wacko shows login's controls
-   }
+}
 else
-   {
-   ?> <span class="nobr"><input type="hidden" name="goback" value="<?php echo $this->SlimUrl($this->tag);?>" /><strong><strong><?php echo $this->GetTranslation("LoginWelcome") ?></strong>:&nbsp;</strong>
-   <input type="text" name="name" size="18" class="login" />&nbsp;<?php echo $this->GetTranslation("LoginPassword") ?>:&nbsp;<input type="password" name="password" class="login" size="8" />&nbsp;<input type="image" src="<?php echo $this->GetConfigValue("theme_url") ?>icons/login.gif" alt=">>>" style="vertical-align:top" /></span> <?php
-   }
+{
+	?> <span class="nobr"><input type="hidden" name="goback" value="<?php echo $this->SlimUrl($this->tag);?>" /><strong><strong><?php echo $this->GetTranslation("LoginWelcome") ?></strong>:&nbsp;</strong>
+	<input type="text" name="name" size="18" class="login" />&nbsp;<?php echo $this->GetTranslation("LoginPassword") ?>:&nbsp;<input type="password" name="password" class="login" size="8" />&nbsp;<input type="image" src="<?php echo $this->GetConfigValue("theme_url") ?>icons/login.gif" alt=">>>" style="vertical-align:top" /></span> <?php
+}
 // End if
 ?></div>
 <?php
