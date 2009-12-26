591e20c4b24c0e7488c8974237065d4d589d89b4
vendeeglobe <54716082+vendeeglobe@users.noreply.github.com>
2009-12-26 12:10:22 +0000

    only styling

--- wacko/actions/usersettings.php
+++ wacko/actions/usersettings.php
@@ -171,7 +171,7 @@ else if ($user = $this->GetUser())
 ?>
 <input type="hidden" name="action" value="update" />
 <div id="cssformX">
-<h3><?php echo $this->GetTranslation("UserSettings"); ?></h3>
+<h3><?php echo $this->GetTranslation("UsersProfile"); ?></h3>
 <table class="form_tbl">
 <tbody>
   <tr>
