b4743c451819db84f565e13bbdbfbef828d9acde
iliagi <60047205+Elar9000@users.noreply.github.com>
2010-01-10 14:51:20 +0000

    Fixed: Adding Logo Image Adding Config Values

--- wacko/setup/database-install.php
+++ wacko/setup/database-install.php
@@ -33,10 +33,8 @@ print("         <h2>".$lang["TestingConfiguration"]."</h2>\n");
 // Generic Default Inserts
 $insert_admin = "INSERT INTO ".$config["table_prefix"]."users (name, password, email, signuptime, lang) VALUES ('".$config["admin_name"]."', md5('".$_POST["password"]."'), '".$config["admin_email"]."', NOW(), '".$config["language"]."')";
 
-$user_id = "SELECT user_id FROM ".$config_global["table_prefix"]."users WHERE name = '".$config["admin_name"]."' LIMIT 1";
-
-$insert_logo_image = "INSERT INTO ".$config["table_prefix"]."upload (page_id, user_id, filename, description, uploaded_dt, filesize, picture_w, picture_h, file_ext) VALUES ('0', '".$user_id."','wacko4.gif', 'WackoWiki', NOW(), '1580', '108', '50', 'gif')";
-$insert_config = "INSERT INTO ".$config["table_prefix"]."config (id, name, value) VALUES
+$insert_logo_image = "INSERT INTO ".$config["table_prefix"]."upload (page_id, user_id, filename, description, uploaded_dt, filesize, picture_w, picture_h, file_ext) VALUES ('0', (SELECT user_id FROM ".$config["table_prefix"]."users WHERE name = '".$config["admin_name"]."' LIMIT 1),'wacko4.gif', 'WackoWiki', NOW(), '1580', '108', '50', 'gif')";
+$insert_config = "INSERT INTO ".$config["table_prefix"]."config (config_id, name, value) VALUES
 	(0, 'maint_last_cache', NULL),
 	(0, 'maint_last_log', NULL),
 	(0, 'maint_last_refs', NULL),
