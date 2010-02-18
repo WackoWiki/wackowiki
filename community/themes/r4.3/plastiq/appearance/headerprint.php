ed806bbb1a8ea86d9777831ec7068b02d28812f5
vendeeglobe <vendeeglobe@localhost>
2010-02-18 10:29:28 +0000

    added theme plastic

---
+++ community/themes/r4.3/plastiq/appearance/headerprint.php
@@ -0,0 +1,31 @@
+<?php
+	header("Content-Type: text/html; charset=".$this->GetCharset());
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title><?php echo htmlspecialchars($this->config['wacko_name']); ?> / <?php echo $this->GetPagePath(true) ?> (<?php echo $this->GetTranslation("PrintVersion");?>)</title>
+<meta name="robots" content="noindex, nofollow" />
+<meta http-equiv="content-type" content="text/html; charset=<?php echo $this->GetCharset(); ?>" />
+<meta http-equiv="imagetoolbar" content="no" />
+<!-- style sheets attachment -->
+<link href="<?php echo $this->GetConfigValue("theme_url") ?>css/atom.css" rel="stylesheet" type="text/css" media="screen, print" />
+<link href="<?php echo $this->GetConfigValue("theme_url") ?>css/wacko.css" rel="stylesheet" type="text/css" media="screen, print" />
+<link href="<?php echo $this->GetConfigValue("theme_url") ?>css/print.css" rel="stylesheet" type="text/css" media="screen, print" />
+<!-- nav destinations -->
+<link rel="start" href="/" />
+<link rel="copyright" href="<?php echo htmlspecialchars($this->href('', $this->config['policy_page'])); ?>" />
+<link rel="prev" href="<?php echo $this->href() ?>" />
+</head>
+<body id="print">
+<div id="header">
+	<div class="container">
+		<h1><?php echo $this->config['wacko_name']; ?></h1>
+		<a href="/"><?php echo rtrim($this->config['base_url'], '/'); ?></a><?php if ($this->page) { ?>&nbsp;&nbsp;&nbsp;&nbsp;modified: <?php echo $this->GetPageTimeFormatted(); } ?><br />
+		<?php echo $this->GetPagePath() ?><br />
+		<a href="<?php echo $this->href() ?>">&laquo back</a>
+	</div>
+</div>
+<div id="content" style="letter-spacing: normal;">
+<!-- begin page output -->
+
---
+++ community/themes/r4.3/plastiq/lang/wacko.all.php
@@ -0,0 +1,10 @@
+<?php
+// WackoWiki theme options =========
+  $this->config['revisions_hide_cancel'] = 0;
+  $this->config['footer_comments'] = 0;
+  $this->config['footer_files'] = 0;
+// ============================
+$themeResource = array(
+  "" => "",
+);
+?>
