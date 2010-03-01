f160fe924f06e60f507d9979ee120c43a3421c73
mb <mb@localhost>
2010-03-01 01:59:24 +0100

    only code styling

--- wacko/db/mysqli.php
+++ wacko/db/mysqli.php
@@ -9,11 +9,11 @@ function quote($dblink, $string)
 function query($dblink, $query, $debug)
 {
 	$result = mysqli_query($dblink, $query);
-	
+
 	if (mysqli_connect_errno())
 	{
 		ob_end_clean();
-		
+
 		if ($debug > 2)
 		{
 			die("Query failed: ".$query." (".mysqli_connect_errno().": ".mysqli_connect_error().")");
@@ -39,7 +39,7 @@ function free_result($rs)
 function connect($host, $user, $passw, $db, $collation = false, $driver, $port = "")
 {
 	$dblink = mysqli_connect($host, $user, $passw, $db, $port);
-	
+
 	if ($collation)
 	{
 		mysqli_query($dblink, "SET NAMES '".$collation."'");
@@ -50,6 +50,6 @@ function connect($host, $user, $passw, $db, $collation = false, $driver, $port =
 
 function last_insert_id($dblink)
 {
-   return mysqli_insert_id($dblink);
+	return mysqli_insert_id($dblink);
 }
 ?>
