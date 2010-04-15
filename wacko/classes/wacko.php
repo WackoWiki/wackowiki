e72b74645ecd195c673cc79afbe52b3fc52e5d06
vendeeglobe <54716082+vendeeglobe@users.noreply.github.com>
2010-04-15 17:06:31 +0200

    removed obsolete function LoadPageTitles()

--- wacko/classes/wacko.php
+++ wacko/classes/wacko.php
@@ -1043,7 +1043,6 @@ class Wacko
 		}
 	}
 
-	function LoadPageTitles() { return $this->LoadAll("SELECT DISTINCT tag FROM ".$this->config["table_prefix"]."pages ORDER BY tag"); }
 	function LoadAllPagesByTime() { return $this->LoadAll("SELECT page_id, tag, modified FROM ".$this->config["table_prefix"]."pages WHERE comment_on_id = '0' ORDER BY modified DESC, BINARY tag"); }
 
 	function LoadRecentlyDeleted($limit = 1000, $cache = 1)
