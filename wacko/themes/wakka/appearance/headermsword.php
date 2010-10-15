d97bf96175c9c199e3225641d0c54df89260e1f6
vendeeglobe <vendeeglobe@localhost>
2010-10-15 11:10:34 +0200

    code cleanup

--- wacko/themes/wakka/appearance/headermsword.php
+++ wacko/themes/wakka/appearance/headermsword.php
@@ -6,16 +6,12 @@ header("Content-Type: text/html; charset=".$this->GetCharset());
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->page["lang"] ?>" lang="<?php echo $this->page["lang"] ?>">
 <head>
 <title><?php echo $this->GetWakkaName()." : ".$this->GetPageTag(); ?></title>
-<?php if ($this->GetMethod() != 'show')
+<?php // do not index alternative print pages
 echo "<meta name=\"robots\" content=\"noindex, nofollow\" />\n";?>
-<meta http-equiv="content-type"
-	content="text/html; charset=<?php echo $this->GetCharset(); ?>" />
-<meta name="keywords"
-	content="<?php echo $this->GetConfigValue("meta_keywords") ?>" />
-<meta name="description"
-	content="<?php echo $this->GetConfigValue("meta_description") ?>" />
-<link rel="stylesheet" type="text/css"
-	href="<?php echo $this->GetConfigValue("theme_url") ?>css/msword.css" />
+<meta http-equiv="content-type" content="text/html; charset=<?php echo $this->GetCharset(); ?>" />
+<meta name="keywords" content="<?php echo $this->GetConfigValue("meta_keywords") ?>" />
+<meta name="description" content="<?php echo $this->GetConfigValue("meta_description") ?>" />
+<link rel="stylesheet" type="text/css" href="<?php echo $this->GetConfigValue("theme_url") ?>css/msword.css" />
 </head>
 
 <body>
