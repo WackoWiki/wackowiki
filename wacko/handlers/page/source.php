764da8b84c70c2bbf6aebc131fb9007bcaf0e3a7
vendeeglobe <vendeeglobe@localhost>
2010-10-17 17:23:12 +0200

    added backported 'source' handler

---
+++ wacko/handlers/page/source.php
@@ -0,0 +1,49 @@
+<div id="page">
+<?php
+
+/*
+	the source text will be shown with exception of those fragments which are hidden by formatters %%(comments)..%%
+
+	TODO: add config option to set an treshhold or to disable the source handler
+*/
+
+// redirect to show method if page don't exists
+if (!$this->page) $this->redirect($this->href('show'));
+
+// deny for comment
+if ($this->page['comment_on_id']) $this->redirect($this->href('', $this->page['tag']));
+
+if ($this->has_access('read'))
+{
+	if (!$this->page)
+	{
+		print(str_replace('%1', $this->href('edit'), $this->get_translation('DoesNotExists')));
+	}
+	else
+	{
+		/* obsolete code - or do we need an ability to print old revisions?
+		if ($this->page['latest'] == 0)
+		{
+			print("<div class=\"revisioninfo\">".
+			str_replace('%1', $this->href(),
+			str_replace('%2', $this->tag,
+			str_replace('%3', $this->page['modified'],
+			$this->get_translation('Revision')))).".</div>");
+		}*/
+
+		// build html body
+		$data = $this->page['body'];
+
+		// display page
+		$data = $this->format($data, 'source', array('bad' => 'good'));
+		echo $data;
+
+	}
+}
+else
+{
+	print($this->get_translation('ReadAccessDenied'));
+}
+
+?>
+</div>
