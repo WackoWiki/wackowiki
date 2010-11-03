1e30c34d2ef4b818e5a51f46c805efd4a6d7ce6b
vendeeglobe <vendeeglobe@localhost>
2010-11-03 17:17:17 +0100

    fixed typo

--- wacko/actions/adminupdate.php
+++ wacko/actions/adminupdate.php
@@ -1,21 +1,24 @@
 <?php
 
-// status: alpha - just hacked together
+// status: alpha
 //
 // for testing and improvement - thought for upgrade routine of the installer
+
 echo "<h2>Upgrade Utilities -> Migration Routines for R4.3.rc1 to R4.4.rc1 Upgrade</h2>";
+echo 'Recent Wacko version '.$this->format('**!!(green)'.$this->config['wacko_version'].'!!**', 'wacko');
+
+########################################################
+##            RENAME files to @page_id@file_name      ##
+########################################################
 
 if ($this->is_admin())
 {
 	if (!isset($_POST['rename']))
 	{
 		echo "<h3>1. Renames files in \\files\perpage folder to @page_id@file_name:</h3>";
-
 		echo $this->form_open();
 		?>
-		<input
-		type="submit" name="rename"
-		value="<?php echo $this->get_translation('KeywordsSaveButton');?>" />
+		<input type="submit" name="rename"  value="<?php echo $this->get_translation('CategoriesSaveButton');?>" />
 		<?php
 		echo $this->form_close();
 	}
@@ -56,16 +59,17 @@ if ($this->is_admin())
 				closedir($handle);
 
 				echo "<tr><td>".$old_name."".$file_name."</td><td> </td><td>".$new_name."".$file_name."</td></tr>";
-
 			}
-
 		}
 		echo "</table>";
 		echo "<br />Files renamed";
 	}
 }
 
-// Migrate User settings
+########################################################
+##            MIGRATE user settings                   ##
+########################################################
+
 if (!function_exists('DecomposeOptions'))
 {
 	function DecomposeOptions($more)
@@ -85,17 +89,14 @@ if (!function_exists('DecomposeOptions'))
 	}
 }
 
-
 if ($this->is_admin())
 {
 	if (!isset($_POST['migrate_user_otions']))
 	{
-		echo "<h3>2. Migrates user otions to user_setting table:</h3>";
+		echo "<h3>2. Migrates user options to user_setting table:</h3>";
 		echo $this->form_open();
 		?>
-		<input
-		type="submit" name="migrate_user_otions"
-		value="<?php echo $this->get_translation('KeywordsSaveButton');?>" />
+		<input type="submit" name="migrate_user_otions" value="<?php echo $this->get_translation('CategoriesSaveButton');?>" />
 		<?php
 		echo $this->form_close();
 	}
@@ -124,6 +125,200 @@ if ($this->is_admin())
 		}
 
 		echo "<br />".$count." user settings inserted.";
+	}
+}
+
+########################################################
+##            MIGRATE ACLs to new scheme              ##
+########################################################
+
+if ($this->is_admin())
+{
+	if (!isset($_POST['migrate_acls']))
+	{
+		echo "<h3>2. Migrates acls to new scheme:</h3>";
+		echo $this->form_open();
+		?>
+		<input type="submit" name="migrate_acls" value="<?php echo $this->get_translation('CategoriesSaveButton');?>" />
+		<?php
+		echo $this->form_close();
+	}
+	// migrate acls to new acl and acl_privilege table
+	else if (isset($_POST['migrate_acls']))
+	{
+		// load old ACLs
+		$_acls = $this->load_all(
+			"SELECT page_id, privilege, list ".
+			"FROM {$this->config['table_prefix']}acl_old ");
+
+		$old_acl_count = count($_acls);
+
+		foreach ($_acls as $_acl)
+		{
+			echo $_acl['privilege'].'<br />';
+			// get object_right_id (e.g. 'write' -> 1, 'read' -> 2)
+			$_object_right_id = $this->load_single(
+				"SELECT acl_right_id ".
+				"FROM {$this->config['table_prefix']}acl_right ".
+				"WHERE object_right = '{$_acl['privilege']}'
+				");
+			$object_right_id = $_object_right_id['acl_right_id'];
+
+			// get object_type_id (e.g. 'page' -> 1) / there is only 'page' so far
+			$_object_type_id = $this->load_single(
+				"SELECT acl_type_id ".
+				"FROM {$this->config['table_prefix']}acl_type ".
+				"WHERE object_type = 'page'
+				");
+			$object_type_id = $_object_type_id['acl_type_id'];
+
+			// INSERT rights in 'acl' table
+			$sql =	"INSERT INTO {$this->config['table_prefix']}acl
+					(object_id, object_type_id, object_right_id)
+					VALUES ('{$_acl['page_id']}', '{$object_type_id}', '{$object_right_id}')";
+
+			$this->query($sql);
+
+			// get new created $acl_id
+			$acl_id = $this->load_single(
+				"SELECT acl_id ".
+				"FROM {$this->config['table_prefix']}acl ".
+				"WHERE object_id = '{$_acl['page_id']}' ".
+					"AND object_type_id = '{$object_type_id}' ".
+					"AND object_right_id = '{$object_right_id}'
+				");
+			$acl_id = $acl_id['acl_id'];
+
+			// get user and group privileges
+			$privileges	= explode("\n", $_acl['list']);
+			$this->debug_print_r($privileges);
+
+			foreach ($privileges as $privilege)
+			{
+				if (!empty($privilege))
+				{
+					$grant_id		= '';
+					$grant_type_id	= '';
+					$deny			= '';
+
+					#$privilege = (string)$privilege;
+					// look for '!' prefix, if true set $deny to true and remove it
+					if ($privilege[0] == '!')
+					{
+						$deny = 1;
+						$privilege = substr($privilege, 1);
+					}
+					else
+					{
+						$deny = 0;
+					}
+
+					echo $privilege.'<br />';
+					// is group?
+					// 1. default groups
+					// 1.1 Everybody
+					if ($privilege == '*')
+					{
+						$_grant_id = $this->load_single(
+							"SELECT group_id ".
+							"FROM {$this->config['table_prefix']}group ".
+							"WHERE group_name = 'Everybody'
+							");
+						$grant_id = $_grant_id['group_id'];
+						$grant_type_id = 1;
+					}
+					// 1.2 Registered
+					else if  ($privilege == '$')
+					{
+						$_grant_id = $this->load_single(
+							"SELECT group_id ".
+							"FROM {$this->config['table_prefix']}group ".
+							"WHERE group_name = 'Registered'
+							");
+						$grant_id = $_grant_id['group_id'];
+						$grant_type_id = 1;
+					}
+					// 1.3 Admins
+					else if  ($privilege == 'Admins')
+					{
+						$_grant_id = $this->load_single(
+							"SELECT group_id ".
+							"FROM {$this->config['table_prefix']}group ".
+							"WHERE group_name = 'Admins'
+							");
+						$grant_id = $_grant_id['group_id'];
+						$grant_type_id = 1;
+					}
+					else
+					{
+						// 2. non default groups
+						if (!isset($this->groups))
+						{
+							$_groups = $this->load_all(
+								"SELECT group_name ".
+								"FROM {$this->config['table_prefix']}group ");
+
+							foreach ($_groups as $_group)
+							{
+								$groups[] = $_group['group_name'];
+							}
+							$this->groups = $groups;
+						}
+
+						$this->debug_print_r($groups);
+
+						if (in_array($privilege, $this->groups))
+						{
+							$grant_id = $this->load_single(
+								"SELECT group_id ".
+								"FROM {$this->config['table_prefix']}group ".
+								"WHERE group_name = '{$privilege}'
+								");
+							$grant_id		= $grant_id['group_id'];
+							$grant_type_id	= 1;
+						}
+						else
+						{
+							// 3. users
+							if (!isset($this->users))
+							{
+								$_users = $this->load_all(
+								"SELECT user_name ".
+								"FROM {$this->config['table_prefix']}user ");
+
+								foreach ($_users as $_user)
+								{
+									$users[] = $_user['user_name'];
+								}
+								$this->users = $users;
+							}
+
+							$this->debug_print_r($users);
+
+							if (in_array($privilege, $this->users))
+							{
+								$_grant_id = $this->load_single(
+									"SELECT user_id ".
+									"FROM {$this->config['table_prefix']}user ".
+									"WHERE user_name = '{$privilege}'
+									");
+								$grant_id		= $_grant_id['user_id'];
+								$grant_type_id	= 2;
+							}
+						}
+					}
+
+					// INSERT privileges in 'acl_privilege' table
+					$sql =	"INSERT INTO {$this->config['table_prefix']}acl_privilege
+							(acl_id, grant_type_id, grant_id, deny)
+							VALUES ('{$acl_id}', '{$grant_type_id}', '{$grant_id}', '{$deny}')";
+
+					$this->query($sql);
+				}
+			}
+		}
+
+		echo '<br />'.$old_acl_count.' acl and '.$privilege_count.' privilege settings inserted.';
 	}
 }
 
