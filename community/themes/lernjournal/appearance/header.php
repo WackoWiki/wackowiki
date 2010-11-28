2bfb2d561196e24cae0217c461c4a6eaf577929d
vendeeglobe <vendeeglobe@localhost>
2010-11-28 06:30:17 +0100

    code cleanup

--- community/themes/lernjournal/appearance/header.php
+++ community/themes/lernjournal/appearance/header.php
@@ -196,7 +196,7 @@ if ($this->is_admin()){
 ?>
 <?php
 // Print icon
-echo"<a href=\"".$this->href('print')."\" target=\"_new\"><img src=\"".$this->config['theme_url']."icons/print.gif\" title=\"".$this->get_translation('PrintVersion')."\" alt=\"".$this->get_translation('PrintVersion')."\"  align=\"middle\" border=\"0\" /></a>";
+echo"<a href=\"".$this->href('print')."\" target=\"_blank\"><img src=\"".$this->config['theme_url']."icons/print.gif\" title=\"".$this->get_translation('PrintVersion')."\" alt=\"".$this->get_translation('PrintVersion')."\"  align=\"middle\" border=\"0\" /></a>";
 
 ?>
 
