c00102ec44f92fa53a6b60758275a3e23c2596d8
vendeeglobe <54716082+vendeeglobe@users.noreply.github.com>
2010-12-23 16:35:39 +0100

    code cleanup

--- wacko/actions/recentlycommented.php
+++ wacko/actions/recentlycommented.php
@@ -4,10 +4,10 @@ if (!function_exists('load_recently_commented'))
 {
 	function load_recently_commented(&$wacko, $for = '', $limit = 50)
 	{
-		$_ids = '';
-		$limit = (int) $limit;
-		$comments = '';
-		$pagination = '';
+		$_ids		= '';
+		$limit		= (int) $limit;
+		$comments	= '';
+		$pagination	= '';
 
 		// NOTE: this is really stupid. Maybe my SQL-Fu is too weak, but apparently there is no easier way
 		if ($ids = $wacko->load_all(
@@ -31,7 +31,9 @@ if (!function_exists('load_recently_commented'))
 					foreach ($ids as $key => $id)
 					{
 						if ($key > 0)
+						{
 							$_ids .= ", ";
+						}
 						$_ids .= "'".$id['page_id']."'";
 					}
 
@@ -41,11 +43,12 @@ if (!function_exists('load_recently_commented'))
 							"FROM ".$wacko->config['table_prefix']."page a ".
 								"INNER JOIN ".$wacko->config['table_prefix']."page b ON (a.comment_on_id = b.page_id) ".
 								"LEFT OUTER JOIN ".$wacko->config['table_prefix']."user u ON (a.user_id = u.user_id) ".
-							" WHERE a.page_id IN ( ".$_ids." ) ".
+							"WHERE a.page_id IN ( ".$_ids." ) ".
 							"ORDER BY comment_time DESC ".
 							"LIMIT {$pagination['offset']}, {$limit}");
 				}
 		}
+
 		return array($comments, $pagination);
 	}
 }
@@ -57,14 +60,25 @@ if (!isset($noxml)) $noxml	= 0;
 if ($user = $this->get_user())
 {
 	$usermax = $user['changes_count'];
-	if ($usermax == 0) $usermax = 10;
+
+	if ($usermax == 0)
+	{
+		$usermax = 10;
+	}
 }
 else
+{
 	$usermax = 50;
+}
 if (!isset($max) || $usermax < $max)
+{
 	$max = $usermax;
+}
 
-if ($max > 100)		$max	= 100;
+if ($max > 100)
+{
+	$max	= 100;
+}
 
 // process 'mark read' - reset session time
 if (isset($_GET['markread']) && $user == true)
@@ -80,13 +94,18 @@ if (list ($pages, $pagination) = load_recently_commented($this, $root, (int)$max
 	{
 		echo '<small><a href="'.$this->href('', '', 'markread=yes').'">'.$this->get_translation('MarkRead').'</a></small>';
 	}
+
 	if ($root == '' && !(int)$noxml)
 	{
 		echo "<span class=\"desc_rss_feed\"><a href=\"".$this->config['base_url']."xml/comments_".preg_replace('/[^a-zA-Z0-9]/', '', strtolower($this->config['wacko_name'])).".xml\"><img src=\"".$this->config['theme_url']."icons/xml.gif"."\" title=\"".$this->get_translation('RecentCommentsXMLTip')."\" alt=\"XML\" /></a></span><br /><br />\n";
 	}
+
 	// pagination
 	if (isset($pagination['text']))
+	{
 		echo "<span class=\"pagination\">{$pagination['text']}</span>\n";
+	}
+
 	echo "<ul class=\"ul_list\">\n";
 
 	if ($pages)
@@ -94,22 +113,31 @@ if (list ($pages, $pagination) = load_recently_commented($this, $root, (int)$max
 		foreach ($pages as $page)
 		{
 			if ($this->config['hide_locked'])
+			{
 				$access = $this->has_access('read', $page['comment_on_id']);
+			}
 			else
+			{
 				$access = true;
+			}
 
 			if ($access && $this->user_allowed_comments())
 			{
 				// day header
 				list($day, $time) = explode(' ', $page['comment_time']);
 
-				if (!isset($curday)) $curday = '';
+				if (!isset($curday))
+				{
+					$curday = '';
+				}
+
 				if ($day != $curday)
 				{
 					if ($curday)
 					{
 						echo "</ul>\n<br /></li>\n";
 					}
+
 					echo "<li><b>".date($this->config['date_format'], strtotime($day)).":</b>\n<ul>\n";
 					$curday = $day;
 				}
@@ -132,7 +160,9 @@ if (list ($pages, $pagination) = load_recently_commented($this, $root, (int)$max
 
 		// pagination
 		if (isset($pagination['text']))
+		{
 			echo "<br /><span class=\"pagination\">{$pagination['text']}</span>\n";
+		}
 	}
 	else
 	{
