499575fc34c92706d81fae422e4e7e57702b647d
vendeeglobe <54716082+vendeeglobe@users.noreply.github.com>
2011-01-25 14:41:01 +0100

    moved <header> in common file to include it

--- community/themes/lernjournal/appearance/header.php
+++ community/themes/lernjournal/appearance/header.php
@@ -3,83 +3,9 @@
 lernjournal theme.
 */
 
-// HTTP header with right Charset settings
-  header("Content-Type: text/html; charset=".$this->get_charset());
-?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+require ('themes/_common/_header.php');
 
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->page['lang'] ?>" lang="<?php echo $this->page['lang'] ?>">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-<title>
-<?php
-// Echoes Title of the page.
-  echo htmlspecialchars($this->config['wacko_name'])." : ".(isset($this->page['title']) ? $this->page['title'] : $this->add_spaces($this->tag)).($this->method != 'show' ? ' ('.$this->method.')' : '');
 ?>
-</title>
-<?php
-// We don't need search robots to index subordinate pages, if indexing is disabled globally or per page
-if ($this->method != 'show' || $this->page['latest'] == 0 || $this->config['noindex'] == 1 || $this->page['noindex'] == 1)
-	echo "	<meta name=\"robots\" content=\"noindex, nofollow\" />\n";
-?>
-<meta name="Keywords" content="<?php echo $this->get_keywords(); ?>" />
-<meta name="Description" content="<?php echo $this->get_description(); ?>" />
-<meta name="language" content="<?php echo $this->page['lang'] ?>" />
-<meta http-equiv="Content-Type" content="text/html; charset=<?php echo $this->get_charset(); ?>" />
-<link rel="stylesheet" type="text/css" href="<?php echo $this->config['theme_url'] ?>css/default.css" />
-<?php if ($this->config['allow_x11colors']) {?><link rel="stylesheet" type="text/css" href="<?php echo $this->config['base_url'] ?>themes/_common/X11colors.css" /><?php } ?>
-<link href="<?php echo $this->config['theme_url'] ?>css/layout.css" rel="stylesheet" type="text/css" />
-<link href="<?php echo $this->config['theme_url'] ?>css/fontdesign.css" rel="stylesheet" type="text/css" />
-<link href="<?php echo $this->config['theme_url'] ?>css/sidenote.css" rel="stylesheet" type="text/css" />
-<link rel="shortcut icon" href="<?php echo $this->config['theme_url'] ?>icons/wacko.ico" type="image/x-icon" />
-<link rel="alternate" type="application/rss+xml" title="<?php echo $this->get_translation('RecentChangesRSS');?>" href="<?php echo $this->config['base_url'];?>xml/changes_<?php echo preg_replace('/[^a-zA-Z0-9]/', '', strtolower($this->config['wacko_name']));?>.xml" />
-<link rel="alternate" type="application/rss+xml" title="<?php echo $this->get_translation('RecentCommentsRSS');?>" href="<?php echo $this->config['base_url'];?>xml/comments_<?php echo preg_replace('/[^a-zA-Z0-9]/', '', strtolower($this->config['wacko_name']));?>.xml" />
-<link rel="alternate" type="application/rss+xml" title="<?php echo $this->get_translation('HistoryRevisionsRSS');?><?php echo $this->tag; ?>" href="<?php echo $this->href('revisions.xml');?>" />
-<?php
-// JS files.
-// default.js contains common procedures and should be included everywhere
-?>
-  <script type="text/javascript" src="<?php echo $this->config['base_url'];?>js/default.js"></script>
-<?php
-// load swfobject with flash action (e.g. $this->config['allow_swfobject'] = 1), by default it is set off
-if ($this->config['allow_swfobject'])
-{
-	echo "  <script type=\"text/javascript\" src=\"".$this->config['base_url']."js/swfobject.js\"></script>\n";
-}
-// autocomplete.js, protoedit & wikiedit2.js contain classes for WikiEdit editor. We may include them only on method==edit pages.
-if ($this->method == 'edit')
-{
-	echo "  <script type=\"text/javascript\" src=\"".$this->config['base_url']."js/protoedit.js\"></script>\n";
-	echo "  <script type=\"text/javascript\" src=\"".$this->config['base_url']."js/wikiedit2.js\"></script>\n";
-	echo "  <script type=\"text/javascript\" src=\"".$this->config['base_url']."js/autocomplete.js\"></script>\n";
-}
-?>
-  <script type="text/javascript" src="<?php echo $this->config['base_url'];?>js/captcha.js"></script>
-<?php
-// Doubleclick edit feature.
-// Enabled only for registered users who don't swith it off (requires class=page in show handler).
-if ($user = $this->get_user())
-   {
-      if ($user['doubleclick_edit'] == 1)
-         {
-?>
-  <script type="text/javascript">
-   var edit = "<?php echo $this->href('edit');?>";
-  </script>
-<?php
-         }
-   }
-else if($this->has_access('write'))
-   {
-?>
-
-      <script type="text/javascript">
-      var edit = "<?php echo $this->href('edit');?>";
-     </script>
-<?php
-   }
-?>
-</head>
 <body onload="all_init();">
 <div id="head">
   <div id="wikititle"><?php echo $this->config['wacko_name'] ?></div>
