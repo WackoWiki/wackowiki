d08c1f330aed0b50469753240318487147dde0ae
vendeeglobe <vendeeglobe@localhost>
2011-02-11 17:07:26 +0100

    minor fixes

--- wacko/themes/default/appearance/headerprint.php
+++ wacko/themes/default/appearance/headerprint.php
@@ -4,7 +4,7 @@ header("Content-Type: text/html; charset=".$this->get_charset());
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->page['lang'] ?>" lang="<?php echo $this->page['lang'] ?>">
 <head>
-<title><?php echo htmlspecialchars($this->config['wacko_name'])." : ".(isset($this->page['title']) ? $this->page['title'] : $this->tag); ?></title>
+<title><?php echo htmlspecialchars($this->config['site_name'])." : ".(isset($this->page['title']) ? $this->page['title'] : $this->tag); ?></title>
 <meta name="robots" content="noindex, nofollow" />
 <meta http-equiv="content-type" content="text/html; charset=<?php echo $this->get_charset(); ?>" />
 <link rel="stylesheet" type="text/css" href="<?php echo $this->config['theme_url'] ?>css/print.css" />
@@ -17,7 +17,7 @@ header("Content-Type: text/html; charset=".$this->get_charset());
 <div id="header">
 	<div class="container">
 		<?php // Print wackoname and wackopath ?>
-		<h1><?php echo $this->config['wacko_name']; ?>: <?php echo (isset($this->page['title']) ? $this->page['title'] : $this->get_page_path()); ?></h1>
+		<h1><?php echo $this->config['site_name']; ?>: <?php echo (isset($this->page['title']) ? $this->page['title'] : $this->get_page_path()); ?></h1>
 		<a href="<?php echo $this->config['base_url'] ?>"><?php echo rtrim($this->config['base_url'], "/"); ?></a><?php if ($this->page) { ?>&nbsp;&nbsp;&nbsp;&nbsp;<?php echo $this->get_translation('Version').": ".$this->get_page_time_formatted(); } ?><br />
 		<?php echo $this->get_page_path() ?><br />
 	</div>
