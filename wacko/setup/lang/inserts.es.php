347927e6ac6fad76ae0f61ee5a67197becd1ffec
vendeeglobe <54716082+vendeeglobe@users.noreply.github.com>
2011-02-28 19:17:11 +0100

    minor fixes

--- wacko/setup/lang/inserts.es.php
+++ wacko/setup/lang/inserts.es.php
@@ -5,7 +5,7 @@ $lng = "es";
 // insert these pages only for default language
 if ($config['language'] == $lng)
 {
-	if ($config['is_update'] = false)
+	if ($config['is_update'] == false)
 	{
 		insert_page($config['root_page'], '', "((file:wacko4.png WackoWiki))\n**Bienvenida a tu ((WackoWiki:Doc/English/WackoWiki WackoWiki))!**\n\nDa click en el enlace \"Editar esta pagina\" abajo en la pagina para empezar.\n\nLa documentación se puede encontrar en WackoWiki:Doc/English.\n\nPaginas útiles: ((WackoWiki:Doc/English/Formatting Formatting)), ((Buscar)).\n\n", $lng, 'Admins', true, false);
 		insert_page($config['users_page'].'/'.$config['admin_name'], $config['admin_name'], "{{adminupdate}}\n\n", $lng, $config['admin_name'], true, false);
--- wacko/setup/lang/inserts.pl.php
+++ wacko/setup/lang/inserts.pl.php
@@ -5,7 +5,7 @@ $lng = "pl";
 // insert these pages only for default language
 if ($config['language'] == $lng)
 {
-	if ($config['is_update'] = false)
+	if ($config['is_update'] == false)
 	{
 		insert_page($config['root_page'], '', "((file:wacko4.png WackoWiki))\n**Witaj na swojej stronie ((WackoWiki:Doc/English/WackoWiki WackoWiki))!**\n\nKlinkij na link \"Edytuj stronê\" na dole by rozpocz±æ.\n\nDokumentacja dostêpna jest w WackoWiki:Doc/English.\n\nPrzydatne strony: ((WackoWiki:Doc/English/Formatting Formatting)), TextSearch.\n\n", $lng, 'Admins', true, false);
 		insert_page($config['users_page'].'/'.$config['admin_name'], $config['admin_name'], "{{adminupdate}}\n\n", $lng, $config['admin_name'], true, false);
