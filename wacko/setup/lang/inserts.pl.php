8ae5a14ae5e59ddf63768dbaaab17664811f19b7
vendeeglobe <vendeeglobe@localhost>
2011-03-01 15:48:35 +0100

    minor fixes

--- wacko/setup/lang/inserts.pl.php
+++ wacko/setup/lang/inserts.pl.php
@@ -8,7 +8,11 @@ if ($config['language'] == $lng)
 	if ($config['is_update'] == false)
 	{
 		insert_page($config['root_page'], '', "((file:wacko4.png WackoWiki))\n**Witaj na swojej stronie ((WackoWiki:Doc/English/WackoWiki WackoWiki))!**\n\nKlinkij na link \"Edytuj stronê\" na dole by rozpocz±æ.\n\nDokumentacja dostêpna jest w WackoWiki:Doc/English.\n\nPrzydatne strony: ((WackoWiki:Doc/English/Formatting Formatting)), TextSearch.\n\n", $lng, 'Admins', true, false);
-		insert_page($config['users_page'].'/'.$config['admin_name'], $config['admin_name'], "{{adminupdate}}\n\n", $lng, $config['admin_name'], true, false);
+		insert_page($config['users_page'].'/'.$config['admin_name'], $config['admin_name'], "::+::\n\n", $lng, $config['admin_name'], true, false);
+	}
+	else
+	{
+		insert_page($config['users_page'].'/'.$config['admin_name'].'/MigrateDataToR44', $config['admin_name'], "{{adminupdate}}\n\n", $lng, $config['admin_name'], true, false);
 	}
 
 	#insert_page('PotrzebneStrony', 'Potrzebne Strony', '{{wanted}}', $lng, 'Admins', true, false);
