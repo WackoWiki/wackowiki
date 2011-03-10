cf50bc02c770a756f2d78db2f8e6ad9f458b26e9
vendeeglobe <vendeeglobe@localhost>
2011-03-10 13:50:19 +0100

    only code styling

--- community/themes/redfort/appearance/headerprint.php
+++ community/themes/redfort/appearance/headerprint.php
@@ -1,5 +1,5 @@
 <?php
-  header("Content-Type: text/html; charset=".$this->get_charset());
+  header('Content-Type: text/html; charset='.$this->get_charset());
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->page['lang'] ?>" lang="<?php echo $this->page['lang'] ?>">
