8b14df68f2ec835dfddf8434c1edbb004769b0ab
vendeeglobe <54716082+vendeeglobe@users.noreply.github.com>
2011-04-01 23:44:51 +0200

    minor fixes

--- wacko/actions/toc.php
+++ wacko/actions/toc.php
@@ -26,7 +26,7 @@ if ($page)
 	$context	= $page;
 	$_page		= $this->load_page($page);
 	if (!$legend) $legend = $page;
-	$link		= $this->href('', $_page['tag']);
+	if ($_page) $link		= $this->href('', $_page['tag']);
 }
 else
 {
@@ -48,77 +48,85 @@ if (!$nomark)
 	echo "<div class=\"layout-box\"><p class=\"layout-box\"><span> ".$this->get_translation('TOCTitle')." ".$this->link($ppage, '', $legend)." </span></p>";
 }
 
-if (!$this->has_access('read', $_page['page_id']))
+if ($_page)
 {
-	echo $this->get_translation('ReadAccessDenied');
+	if (!$this->has_access('read', $_page['page_id']))
+	{
+		echo $this->get_translation('ReadAccessDenied');
+	}
+	else
+	{
+		$toc = $this->build_toc($context, $start_depth, $end_depth, $numerate, $link);
+		{
+			// ---------------------- toc numeration ------------------------
+			// identify what size where faces
+			$toc_len	= sizeof($toc);
+			$numbers	= array();
+			$depth		= 0;
+
+			for($i = 0; $i < $toc_len; $i++)
+			{
+				if ($toc[$i][2] < 66666)
+				{
+					// normalized depth immersion
+					$toc[$i][4] = $toc[$i][2] - $start_depth + 1;
+
+					if ($numerate)
+					{
+						// if dive deeper, reset the meter for new depths
+						if ($toc[$i][2] > $depth)
+						{
+							$numbers[ $toc[$i][2] ] = 0;
+						}
+
+						// if left lower level, nothing else to do.
+						// store and increase the depth meter item
+						$depth = $toc[$i][2];
+						$numbers[ $depth ]++;
+						// collect numbering on the array of $ numbers from start to the current depth, allowing zero
+						$num = '';
+
+						for($j = 1; $j <= $depth; $j++)
+						{
+							if (isset($numbers[$j]) && $numbers[$j] > 0)
+							{
+								$num .= $numbers[$j].'.';
+							}
+						}
+
+						// Human content TOC
+						$toc[$i][1] = $num.' '.$toc[$i][1];
+					}
+				}
+			}
+
+			// not bad in a cache write similar version
+			$this->tocs[ $context ] = &$toc;
+			// it is now necessary to place flag about the fact that good to [iskurochit] in Post-[vake]
+			// the source page, adding there [tsyfirki]
+			if (!$ppage)
+			{
+				$this->post_wacko_toc = &$toc; $this->post_wacko_action['toc'] = 1;
+			}
+		} // --------------------------------------------------------------
+		// display!
+		foreach( $toc as $v )
+		{
+			if (isset($v[4]) && $v[4])
+			{
+				echo '<div class="toc'.$v[4].'">';
+				echo '<a href="'.$v[3].'#'.$v[0].'">'.strip_tags($v[1]).'</a>';
+				echo '</div>';
+			}
+		}
+		//$this->tocRecursion( ($ppage ? $this->href('', $ppage) : ''), $toc_body, 2 );
+	}
 }
 else
 {
-	$toc = $this->build_toc($context, $start_depth, $end_depth, $numerate, $link);
-	{
-		// ---------------------- toc numeration ------------------------
-		// identify what size where faces
-		$toc_len	= sizeof($toc);
-		$numbers	= array();
-		$depth		= 0;
-
-		for($i = 0; $i < $toc_len; $i++)
-		{
-			if ($toc[$i][2] < 66666)
-			{
-				// normalized depth immersion
-				$toc[$i][4] = $toc[$i][2] - $start_depth + 1;
-
-				if ($numerate)
-				{
-					// if dive deeper, reset the meter for new depths
-					if ($toc[$i][2] > $depth)
-					{
-						$numbers[ $toc[$i][2] ] = 0;
-					}
-
-					// if left lower level, nothing else to do.
-					// store and increase the depth meter item
-					$depth = $toc[$i][2];
-					$numbers[ $depth ]++;
-					// collect numbering on the array of $ numbers from start to the current depth, allowing zero
-					$num = '';
-
-					for($j = 1; $j <= $depth; $j++)
-					{
-						if (isset($numbers[$j]) && $numbers[$j] > 0)
-						{
-							$num .= $numbers[$j].".";
-						}
-					}
-
-					// Human content TOC
-					$toc[$i][1] = $num." ".$toc[$i][1];
-				}
-			}
-		}
-
-		// not bad in a cache write similar version
-		$this->tocs[ $context ] = &$toc;
-		// it is now necessary to place flag about the fact that good to [iskurochit] in Post-[vake]
-		// the source page, adding there [tsyfirki]
-		if (!$ppage)
-		{
-			$this->post_wacko_toc = &$toc; $this->post_wacko_action['toc'] = 1;
-		}
-	} // --------------------------------------------------------------
-	// display!
-	foreach( $toc as $v )
-	{
-		if (isset($v[4]) && $v[4])
-		{
-			echo '<div class="toc'.$v[4].'">';
-			echo '<a href="'.$v[3].'#'.$v[0].'">'.strip_tags($v[1]).'</a>';
-			echo '</div>';
-		}
-	}
-	//$this->tocRecursion( ($ppage ? $this->href('', $ppage) : ''), $toc_body, 2 );
+	echo $this->get_translation('DoesNotExists');
 }
+
 if (!$nomark)
 {
 	echo "</div>";
