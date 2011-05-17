b4a4f0e943dd06200d81248cce5373db51200247
vendeeglobe <54716082+vendeeglobe@users.noreply.github.com>
2011-05-17 13:35:27 +0200

    minor fixes

--- wacko/setup/lang/inserts.de.php
+++ wacko/setup/lang/inserts.de.php
@@ -33,7 +33,7 @@ insert_page('SeitenIndex', 'Seiten Index', '{{pageindex}}', $lng, 'Admins', fals
 insert_page('Registrierung', 'Registrierung', '{{registration}}', $lng, 'Admins', false, false);
 
 insert_page('Passwort', 'Passwort', '{{changepassword}}', $lng, 'Admins', false, false);
-insert_page('TextSuche', 'TextSuche', '{{search}}\n\n\n  * ++4 Buchstaben Minimum für die Suche im Text der Seiten. Das ist eine ~MySQL Beschränkung.++\n  * ++3 Buchstaben Minimum für die Suche in Seitennamen.++\n  * ++Bei der Suche im Text verwenden wir die Volltextsuche Funktion von ~MySQL. Es wird  nur nach ganzen Worten gesucht.++\n\n', $lng, 'Admins', false, false);
+insert_page('TextSuche', 'TextSuche', '{{search}}\n\n\n  * ++4 Buchstaben Minimum für die Suche im Text der Seiten. Das ist eine ~MySQL Beschränkung.++\n  * ++3 Buchstaben Minimum für die Suche in Seitennamen.++\n  * ++Bei der Suche im Text wird die Volltextsuche Funktion von ~MySQL verwendet. Es wird nur nach ganzen Worten gesucht.++\n\n', $lng, 'Admins', false, false);
 insert_page('Anmeldung', 'Anmeldung', '{{login}}', $lng, 'Admins', false, false);
 insert_page('Einstellungen', 'Einstellungen', '{{usersettings}}', $lng, 'Admins', false, false);
 
