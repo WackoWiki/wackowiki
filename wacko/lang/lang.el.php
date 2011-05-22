720a0a3a6fe3a051049531cf887465dd3fb54f5c
vendeeglobe <54716082+vendeeglobe@users.noreply.github.com>
2011-05-22 19:33:12 +0200

    only code styling

--- wacko/lang/lang.el.php
+++ wacko/lang/lang.el.php
@@ -6,21 +6,21 @@ if (!defined('IN_WACKO'))
 }
 
 $wacko_language = array(
-	"name"				=> "Greek",
-	"code"				=> "el",
-	"charset"			=> "iso-8859-7",
-	"locale"			=> "el_GR",
-	"utfdecode"			=> array(),
+	"name"					=> "Greek",
+	"code"					=> "el",
+	"charset"				=> "iso-8859-7",
+	"locale"				=> "el_GR",
+	"utfdecode"				=> array(),
 	//http://en.wikipedia.org/wiki/ISO-8859-7
-	"UPPER_P"			=> "A-Z\xc1\xb6\xc2-\xc5\xb8\xc6\xc7\xb9\xc8\xc9\xba\xda\xca-\xcf\xbc\xd0-\xd5\xbe\xdb\xd6-\xd9\xbf",
-	"LOWER_P"			=> "a-z\xe1\xdc\xe2-\xe5\xdd\xe6\xe7\xde\xe8\xe9\xdf\xfa\xc0\xea-\xef\xfc\xf0-\xf5\xfd\xfb\xe0\xf6-\xf9\xfe\/'",
-	"ALPHA_P"			=> "A-Za-z\xb6\xb8-\xba\xbc\xbe-\xfe\/'",
+	"UPPER_P"				=> "A-Z\xc1\xb6\xc2-\xc5\xb8\xc6\xc7\xb9\xc8\xc9\xba\xda\xca-\xcf\xbc\xd0-\xd5\xbe\xdb\xd6-\xd9\xbf",
+	"LOWER_P"				=> "a-z\xe1\xdc\xe2-\xe5\xdd\xe6\xe7\xde\xe8\xe9\xdf\xfa\xc0\xea-\xef\xfc\xf0-\xf5\xfd\xfb\xe0\xf6-\xf9\xfe\/'",
+	"ALPHA_P"				=> "A-Za-z\xb6\xb8-\xba\xbc\xbe-\xfe\/'",
 	"TranslitLettersFrom"	=> "¶¸¹º¼¾¿ÀÚÛÜİŞßàúûüışáâãäåæçéêëìíïğñóòôõö÷ùÁÂÃÄÅÆÇÉÊËÌÍÏĞÑÓÔÕÖ×Ù",
 	"TranslitLettersTo"		=> "aehioywiiyaehiyiyoywavgdezhiklmnoprsstyfxwavgdezhiklmnoprstyfxw",
 	"TranslitCaps"			=> "ÀÁÂÃÄÅ¨ÆÇÈÉÊËÌÍÎÏĞÑÒÓÔÕÖ×ØÙÜÚÛİŞß",
 	"TranslitSmall"			=> "àáâãäå¸æçèéêëìíîïğñòóôõö÷øùüúûışÿ",
-	"TranslitBiLetters" => array(
-		"è"=>"th", "È"=>"th", "î"=>"ks", "Î"=>"ks", "ø"=>"ps", "Ø"=>"ps"
+	"TranslitBiLetters"		=> array(
+								"è"=>"th", "È"=>"th", "î"=>"ks", "Î"=>"ks", "ø"=>"ps", "Ø"=>"ps"
 	),
 	// http://tlt.psu.edu/suggestions/international/bylanguage/greekchart.html
 	"unicode_entities" => array(
