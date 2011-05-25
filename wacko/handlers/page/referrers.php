3264aefb9ff01ea6ee74ba4d8cfa466716394b8f
vendeeglobe <vendeeglobe@localhost>
2011-05-25 17:50:23 +0200

    improved referrers handler - part 1

--- wacko/handlers/page/referrers.php
+++ wacko/handlers/page/referrers.php
@@ -14,19 +14,74 @@ if (!$this->page) $this->redirect($this->href('show'));
 
 // deny for comment
 if ($this->page['comment_on_id'])
+{
 	$this->redirect($this->href('', $this->get_page_tag($this->page['comment_on_id']), 'show_comments=1')."#".$this->page['tag']);
+}
 
 if ($user = $this->get_user())
 {
-	if ($global = isset($_GET['global']))
+	if (isset($_GET['global']))
 	{
-		$title = str_replace('%1', $this->href('referrers_sites', '', 'global=1'), $this->get_translation('ExternalPagesGlobal'));
-		$referrers = $this->load_referrers();
+		echo "<h3>".$this->get_translation('ReferrersText')." &raquo; ".$this->get_translation('Referrers')."</h3>";
+		echo "<ul class=\"menu\">
+			<li><a href=\"".$this->href('referrers')."\">".str_replace('%1', $this->tag, $this->get_translation('ViewReferrersPage'))."</a></li>
+			<li><a href=\"".$this->href('referrers', '', 'perpage=1')."\">".$this->get_translation('ViewReferrersPerPage')."</a></li>
+			<li class=\"active\">".$this->get_translation('ViewReferrersGlobal')."</li>
+		</ul><br /><br />\n";
+	}
+	else if (isset($_GET['perpage']))
+	{
+		echo "<h3>".$this->get_translation('ReferrersText')." &raquo; ".$this->get_translation('Referrers')."</h3>";
+		echo "<ul class=\"menu\">
+			<li><a href=\"".$this->href('referrers')."\">".str_replace('%1', $this->tag, $this->get_translation('ViewReferrersPage'))."</a></li>
+			<li class=\"active\">".$this->get_translation('ViewReferrersPerPage')."</li>
+			<li><a href=\"".$this->href('referrers', '', 'global=1')."\">".$this->get_translation('ViewReferrersGlobal')."</a></li>
+		</ul><br /><br />\n";
 	}
 	else
 	{
-		$title = $this->get_translation('ReferringPages').":";
-		print("<strong>$title</strong><br /><br />\n");
+		echo "<h3>".$this->get_translation('ReferrersText')." &raquo; ".$this->get_translation('Referrers')."</h3>";
+		echo "<ul class=\"menu\">
+			<li class=\"active\">".str_replace('%1', $this->tag, $this->get_translation('ViewReferrersPage'))."</li>
+			<li><a href=\"".$this->href('referrers', '', 'perpage=1')."\">". $this->get_translation('ViewReferrersPerPage')."</a></li>
+			<li><a href=\"".$this->href('referrers', '', 'global=1')."\">". $this->get_translation('ViewReferrersGlobal')."</a></li>
+		</ul><br /><br />\n";
+	}
+
+	if ($global = isset($_GET['global']))
+	{
+		$title		= str_replace('%1', $this->href('referrers_sites', '', 'global=1'),
+		str_replace('%2',
+			($this->config['referrers_purge_time']
+			? ($this->config['referrers_purge_time'] == 1
+				? $this->get_translation('Last24Hours')
+				: str_replace('%1', $this->config['referrers_purge_time'], $this->get_translation('LastDays')))
+			: ''),
+			$this->get_translation('ExternalPagesGlobal')));
+		$referrers	= $this->load_referrers();
+	}
+	else if ($perpage = isset($_GET['perpage']))
+	{
+		$title		= str_replace('%1', $this->href('referrers_sites', '', 'perpage=1'),
+			str_replace('%2',
+			($this->config['referrers_purge_time']
+			? ($this->config['referrers_purge_time'] == 1
+				? $this->get_translation('Last24Hours')
+				: str_replace('%1', $this->config['referrers_purge_time'], $this->get_translation('LastDays')))
+			: ''),
+			$this->get_translation('ExternalPagesGlobal')));
+
+		$pages		= $this->load_all(
+			"SELECT r.page_id, count( r.referrer ) AS num, p.tag, p.title
+			FROM ".$this->config['table_prefix']."referrer r
+			LEFT JOIN ".$this->config['table_prefix']."page p ON ( p.page_id = r.page_id )
+			GROUP BY r.page_id
+			ORDER BY num DESC");
+	}
+	else
+	{
+		$title		= $this->get_translation('ReferringPages').":";
+		echo "<strong>$title</strong><br /><br />\n";
 
 		// show backlinks
 		if ($pages = $this->load_pages_linking_to($this->tag))
@@ -49,57 +104,110 @@ if ($user = $this->get_user())
 					}
 				}
 			}
+
 			print(implode("<br />\n", $links)."<p></p>");
 		}
 		else
 		{
-			print($this->get_translation('NoReferringPages')."<p></p>");
+			echo $this->get_translation('NoReferringPages')."<p></p>";
 		}
 
-		$title = str_replace('%1', $this->compose_link_to_page($this->tag),
-		str_replace('%2',
-		($this->config['referrers_purge_time']
-		? ($this->config['referrers_purge_time'] == 1
-			? $this->get_translation('Last24Hours')
-			: str_replace('%1', $this->config['referrers_purge_time'], $this->get_translation('LastDays')))
-		: ''),
-		str_replace('%3', $this->href('referrers_sites'), $this->get_translation('ExternalPages'))));
+		$title		= str_replace('%1', $this->compose_link_to_page($this->tag),
+			str_replace('%2',
+			($this->config['referrers_purge_time']
+			? ($this->config['referrers_purge_time'] == 1
+				? $this->get_translation('Last24Hours')
+				: str_replace('%1', $this->config['referrers_purge_time'], $this->get_translation('LastDays')))
+			: ''),
+			str_replace('%3', $this->href('referrers_sites'), $this->get_translation('ExternalPages'))));
 
 		$referrers = $this->load_referrers($this->page['page_id']);
 	}
 
-	print("<strong>$title</strong><br /><br />\n");
-	if ($referrers)
+	echo "<strong>$title</strong><br /><br />\n";
+
+	if ($referrers || $perpage)
 	{
+		if ($perpage)
 		{
-			print("<table border=\"0\" cellspacing=\"0\" cellpadding=\"0\">\n");
+			echo "<ul class=\"ul_list\">\n";
+
+			foreach ($pages as $page)
+			{
+				if (isset($page['page_id']))
+				{
+					if ($page['page_id'] == 0)
+					{
+						$access = true; // 404er
+					}
+					else if ($this->config['hide_locked'])
+					{
+						$access = $this->has_access('read', $page['page_id']);
+					}
+					else
+					{
+						$access = true;
+					}
+
+					if ($access)
+					{
+						if ($page['page_id'] == 0)
+						{
+							$page_link = '404';
+						}
+						else
+						{
+							#$page_link = $this->compose_link_to_page($page['tag']);
+							$page_link = $this->link('/'.$page['tag'], '', $page['title']);
+						}
+
+						echo '<li><strong>'.$page_link.'</strong>'.' ('.$page['num'].')';
+						$referrers = $this->load_referrers($page['page_id']);
+
+						echo "<ul>\n";
+
+						foreach ($referrers as $referrer)
+						{
+							echo "<li class=\"lined\">";
+							echo "<span class=\"\">".$referrer['num']."</span> &nbsp; ";
+							echo "<span class=\"\"><a href=\"".htmlspecialchars($referrer['referrer'])."\">".htmlspecialchars($referrer['referrer'])."</a></span >";
+							echo "</li>\n";
+						}
+
+						echo "</ul>\n<br /></li>\n";
+
+					}
+				}
+			}
+
+			echo "</ul>\n";
+		}
+		else
+		{
+			echo "<table border=\"0\" cellspacing=\"0\" cellpadding=\"0\">\n";
+
 			foreach ($referrers as $referrer)
 			{
-				print("<tr>");
-				print("<td width=\"30\" align=\"right\" valign=\"top\" style=\"padding-right: 10px\">".$referrer['num']."</td>");
-				print("<td valign=\"top\"><a href=\"".htmlspecialchars($referrer['referrer'])."\">".htmlspecialchars($referrer['referrer'])."</a></td>");
-				print("</tr>\n");
+				echo "<tr>";
+				echo "<td width=\"30\" align=\"right\" valign=\"top\" style=\"padding-right: 10px\">".$referrer['num']."</td>";
+				echo "<td valign=\"top\"><a href=\"".htmlspecialchars($referrer['referrer'])."\">".htmlspecialchars($referrer['referrer'])."</a></td>";
+				echo "</tr>\n";
 			}
-			print("</table>\n");
+
+			echo "</table>\n";
 		}
 	}
 	else
 	{
-		print($this->get_translation('NoneReferrers')."<br />\n");
+		echo $this->get_translation('NoneReferrers')."<br />\n";
 	}
 
-	if ($global)
-	{
-		print("<br />[".str_replace('%1', $this->href('referrers_sites'), str_replace('%2', $this->tag, $this->get_translation('ViewReferringSites')))." | ".str_replace('%1', $this->href('referrers'),str_replace('%2', $this->tag, $this->get_translation('ViewReferrersFor')))."]");
-	}
-	else
-	{
-		print("<br />[".str_replace('%1', $this->href('referrers_sites', '', 'global=1'),$this->get_translation('ViewReferringSitesGlobal')) ." | ".str_replace('%1', $this->href('referrers', '', 'global=1'), $this->get_translation('ViewReferrersForGlobal'))."]");
-	}
+
 }
 else
 {
-	print($this->get_translation('ReadAccessDenied'));
+	echo $this->get_translation('ReadAccessDenied');
 }
+
 ?>
 </div>
