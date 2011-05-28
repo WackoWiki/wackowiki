2ef22ec6f227c6a88f0c9adc3ccb2917066bcff9
Iliagi <60047205+Elar9000@users.noreply.github.com>
2011-05-29 01:18:07 +0400

    fixed captcha.js

--- wacko/js/captcha.js
+++ wacko/js/captcha.js
@@ -1,13 +1,9 @@
 function new_freecap()
 {
-   if(document.getElementById)
-      {
          thesrc = document.getElementById("freecap").src;
-         thesrc = thesrc.substring(0, thesrc.lastIndexOf(".") + 4);
-         document.getElementById("freecap").src = thesrc + "?" + Math.round(Math.random() * 100000);
-      }
-   else
-      {
-         alert("<?php echo $this->get_translation('CaptchaReloadWarning');?>");
-      }
+         if (thesrc.indexOf("&") !== -1) 
+	    {
+         	thesrc = thesrc.substring(0, thesrc.lastIndexOf("&"));
+            }
+	 document.getElementById("freecap").src = thesrc + "&" + Math.round(Math.random() * 100000);
 }
