d081acee9c784c33df3857cfd4c282995b821afe
vendeeglobe <vendeeglobe@localhost>
2011-07-09 14:44:12 +0200

    minor fixes

--- wacko/admin/modules/sysupdate.php
+++ wacko/admin/modules/sysupdate.php
@@ -32,37 +32,8 @@ function admin_sysupdate(&$engine, &$module)
 		if (isset($_REQUEST['i'])) $i = $_REQUEST['i'];
 		else $i = 0;
 
-		// acls
-		if ((int)$_REQUEST['step'] === 1)
-		{
-			if ($pages = $engine->load_all("SELECT tag FROM {$engine->config['table_prefix']}acl LIMIT ".($i*$limit).", $limit"))
-			{
-				foreach ($pages as $page)
-				{
-					#$engine->sql_query(
-					#	"UPDATE {$engine->config['table_prefix']}acl ".
-					#	"SET supertag = '".$engine->translit($page['tag'])."' ".
-					#	"WHERE tag = '".$page['tag']."'");
-				}
-				$engine->redirect(rawurldecode($engine->href('', 'admin.php?mode='.$module['mode'].'&start=1&step='.$_REQUEST['step'].'&i='.(++$i))));
-			}
-			else
-			{
-?>
-				<ol>
-					<li value="1"><s>Transliterate field `supertag` in table `acl`</s>.</li>
-				</ol>
-				<br />
-				<form action="admin.php" method="post" name="sysupdate">
-					<input type="hidden" name="mode" value="sysupdate" />
-					<input type="hidden" name="step" value="2" />
-					<input name="start" id="submit" type="submit" value="continue" />
-				</form>
-<?php
-			}
-		}
 		// links
-		else if ((int)$_REQUEST['step'] === 2)
+		if ((int)$_REQUEST['step'] === 1)
 		{
 			if ($pages = $engine->load_all("SELECT to_tag FROM {$engine->config['table_prefix']}link LIMIT ".($i*$limit).", $limit"))
 			{
@@ -73,25 +44,26 @@ function admin_sysupdate(&$engine, &$module)
 						"SET to_supertag = '".$engine->translit($page['to_tag'])."' ".
 						"WHERE to_tag = '".$page['to_tag']."'");
 				}
+
 				$engine->redirect(rawurldecode($engine->href('', 'admin.php?mode='.$module['mode'].'&start=1&step='.$_REQUEST['step'].'&i='.(++$i))));
 			}
 			else
 			{
 ?>
 				<ol>
-					<li value="2"><s>Transliterate field `to_supertag` in table `link`</s>.</li>
+					<li value="1"><del>Transliterate field `to_supertag` in table `link`</del>.</li>
 				</ol>
 				<br />
 				<form action="admin.php" method="post" name="sysupdate">
 					<input type="hidden" name="mode" value="sysupdate" />
-					<input type="hidden" name="step" value="3" />
+					<input type="hidden" name="step" value="2" />
 					<input name="start" id="submit" type="submit" value="continue" />
 				</form>
 <?php
 			}
 		}
 		// pages
-		else if ((int)$_REQUEST['step'] === 3)
+		else if ((int)$_REQUEST['step'] === 2)
 		{
 			if ($pages = $engine->load_all("SELECT page_id, tag FROM {$engine->config['table_prefix']}page LIMIT ".($i*$limit).", $limit"))
 			{
@@ -102,25 +74,26 @@ function admin_sysupdate(&$engine, &$module)
 							"supertag = '".$engine->translit($page['tag'])."' ".
 						"WHERE page_id = ".$page['page_id']);
 				}
+
 				$engine->redirect(rawurldecode($engine->href('', 'admin.php?mode='.$module['mode'].'&start=1&step='.$_REQUEST['step'].'&i='.(++$i))));
 			}
 			else
 			{
 ?>
 				<ol>
-					<li value="3"><s>Transliterate field `supertag` and `super_comment_on` in table `page`</s>.</li>
+					<li value="2"><del>Transliterate field `supertag` and `super_comment_on` in table `page`</del>.</li>
 				</ol>
 				<br />
 				<form action="admin.php" method="post" name="sysupdate">
 					<input type="hidden" name="mode" value="sysupdate" />
-					<input type="hidden" name="step" value="4" />
+					<input type="hidden" name="step" value="3" />
 					<input name="start" id="submit" type="submit" value="continue" />
 				</form>
 <?php
 			}
 		}
 		// revisions
-		else if ((int)$_REQUEST['step'] === 4)
+		else if ((int)$_REQUEST['step'] === 3)
 		{
 			if ($pages = $engine->load_all("SELECT revision_id, tag FROM {$engine->config['table_prefix']}revision LIMIT ".($i*$limit).", $limit"))
 			{
@@ -131,27 +104,29 @@ function admin_sysupdate(&$engine, &$module)
 						"SET supertag = '".$engine->translit($page['tag'])."' ".
 						"WHERE revision_id = ".$page['revision_id']);
 				}
+
 				$engine->redirect(rawurldecode($engine->href('', 'admin.php?mode='.$module['mode'].'&start=1&step='.$_REQUEST['step'].'&i='.(++$i))));
 			}
 			else
 			{
 ?>
 				<ol>
-					<li value="4"><s>Transliterate field `supertag` and `super_comment_on` in table `revision`</s>.</li>
+					<li value="3"><del>Transliterate field `supertag` and `super_comment_on` in table `revision`</del>.</li>
 				</ol>
 				<br />
 				<form action="admin.php" method="post" name="sysupdate">
 					<input type="hidden" name="mode" value="sysupdate" />
-					<input type="hidden" name="step" value="5" />
+					<input type="hidden" name="step" value="4" />
 					<input name="start" id="submit" type="submit" value="continue" />
 				</form>
 <?php
 			}
 		}
 		// files
-		else if ((int)$_REQUEST['step'] === 5)
+		else if ((int)$_REQUEST['step'] === 4)
 		{
 			$dir = $engine->config['upload_path_per_page'];
+
 			if ($dh = opendir($dir))
 			{
 				while (false !== ($file = readdir($dh)))
@@ -162,6 +137,7 @@ function admin_sysupdate(&$engine, &$module)
 						rename($dir.'/'.$file, $dir.'/'.$engine->translit($file));
 					}
 				}
+
 				closedir($dh);
 			}
 
@@ -169,11 +145,12 @@ function admin_sysupdate(&$engine, &$module)
 
 			$engine->sql_query(
 				"UPDATE {$engine->config['table_prefix']}config ".
-				"SET config_value = '$version' WHERE config_name = 'wackowiki_version'");
+				"SET config_value = '$version' ".
+				"WHERE config_name = 'wackowiki_version'");
 			$engine->log(1, 'Upgrading to version WackoWiki '.$version);
 ?>
 			<ol>
-				<li value="5"><s>Transliterate the names of attached files</s>.</li>
+				<li value="4"><del>Transliterate the names of attached files</del>.</li>
 			</ol>
 			<br />
 			<p>
