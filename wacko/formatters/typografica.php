bd1dadc87cfa60a0ab9701f669f6230670d32467
vendeeglobe <vendeeglobe@localhost>
2011-07-09 14:20:17 +0200

    minor fix

--- wacko/formatters/typografica.php
+++ wacko/formatters/typografica.php
@@ -14,7 +14,7 @@ $this->use_class('typografica', 'formatters/classes/');
 
 $typo = new typografica($this);
 
-// kuso@npj: since dashglued cause rendering bugs in Firefox, this option is now turned off.
+// kuso: since dashglued cause rendering bugs in Firefox, this option is now turned off.
 $typo->settings['dashglue'] = false;
 
 echo $typo->correct($text, false);
