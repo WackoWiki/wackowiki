b9cf6077f3f31e69e830362a305885a9f7314086
vendeeglobe <54716082+vendeeglobe@users.noreply.github.com>
2011-07-13 11:32:36 +0200

    minor fixes

--- wacko/actions/edit.php
+++ wacko/actions/edit.php
@@ -8,19 +8,30 @@ if (!defined('IN_WACKO'))
 // If User has rights to edit page, show Edit link
 // {{edit [page="yourPage"] [text="your text"]}}
 
-if (!isset($for)) $for = '';
-if (!isset($page)) $page = '';
-if (!isset($output)) $output = '';
-if (!isset($text)) $text = '';
+if (!isset($for))		$for = '';
+if (!isset($page))		$page = '';
+if (!isset($output))	$output = '';
+if (!isset($text))		$text = '';
 
-if ($for) $page=$for;
-	$editpage = $this->href('', $page.'/edit');
+if ($for)
+{
+	$page = $for;
+}
+
+$editpage = $this->href('', $page.'/edit');
+
 if (!$page)
-	{$editpage = $this->href('edit');}
-if (!$text)
-	$text = $this->get_translation('EditText');
-	$output .= $this->has_access('write') ? "<a href=\"".$editpage."\" accesskey=\"E\" title=\"".$this->get_translation('EditTip')."\">".$text."</a>\n" : "";
+{
+	$editpage = $this->href('edit');
+}
 
-	echo ($output);
+if (!$text)
+{
+	$text = $this->get_translation('EditText');
+}
+
+$output .= $this->has_access('write') ? "<a href=\"".$editpage."\" accesskey=\"E\" title=\"".$this->get_translation('EditTip')."\">".$text."</a>\n" : "";
+
+echo $output;
 
 ?>
