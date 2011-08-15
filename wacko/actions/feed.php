e0a810bfdfb81edefa1c024fcb68709a3767cc29
vendeeglobe <vendeeglobe@localhost>
2011-08-15 12:26:14 +0200

    minor fix

--- wacko/actions/feed.php
+++ wacko/actions/feed.php
@@ -43,8 +43,8 @@ if (!function_exists('intervalCalc'))
 	{
 		global $engine;
 
-		$now = time();
-		$interval_secs = $now  - $postedtime;
+		$now			= time();
+		$interval_secs	= $now - $postedtime;
 
 		if ($interval_secs > 2592000)
 		{
@@ -52,25 +52,25 @@ if (!function_exists('intervalCalc'))
 			if ($interval == 1) $interval.= $engine->get_translation('FeedMonthAgo');
 			else $interval.= $engine->get_translation('FeedMonthsAgo');
 		}
-		elseif ($interval_secs > 604800)
+		else if ($interval_secs > 604800)
 		{
 			$interval = (($interval_secs - ($interval_secs%604800))/604800);
 			if ($interval == 1) $interval.= $engine->get_translation('FeedWeekAgo');
 			else $interval.= $engine->get_translation('FeedWeeksAgo');
 		}
-		elseif ($interval_secs > 86400)
+		else if ($interval_secs > 86400)
 		{
 			$interval = (($interval_secs - ($interval_secs%86400))/86400);
 			if ($interval == 1) $interval.= $engine->get_translation('FeedDayAgo');
 			else $interval.= $engine->get_translation('FeedDaysAgo');
 		}
-		elseif ($interval_secs > 3600)
+		else if ($interval_secs > 3600)
 		{
 			$interval = (($interval_secs - ($interval_secs%3600))/3600);
 			if ($interval == 1) $interval.= $engine->get_translation('FeedHourAgo');
 			else $interval.= $engine->get_translation('FeedHoursAgo');
 		}
-		elseif ($interval_secs > 60)
+		else if ($interval_secs > 60)
 		{
 			$interval = (($interval_secs - ($interval_secs%60))/60);
 			if ($interval == 1) $interval.= $engine->get_translation('FeedMinuteAgo');
@@ -118,148 +118,174 @@ else
 	// Handle it all - goes after init()
 	$feed->handle_content_type();
 
-if (!$nomark)
-{
-	echo "<div class='layout-box'>\n";
-}
-	$counturlset=count($urlset);
+	if (!$nomark)
+	{
+		echo "<div class='layout-box'>\n";
+	}
+
+	$counturlset = count($urlset);
 
 	if (!$feed->get_title() && $counturlset == 1)
 	{
 		echo "<p><i>".$this->get_translation('FeedError')."</i></p>\n";
-		break;
-	}
-
-	$headertag = 'h4';
-
-	if ($title != 'no')
-	{
-		if (($max)&&($max > 0))
-		{
-			$lastitems = " (last ".$max." items)";
-		}
-
-		// Make nice if $nomark == 1
-		if ($nomark)
-		{
-			if ($title != '' && $counturlset == 1)
-			{
-				echo "<h3 class=\"feed_element_title\">".$this->link($feed->get_permalink(), '', $title, '', 1, 1)."</h3>";
-			}
-			if ($title != '' && $counturlset > 1)
-			{
-				echo "<h3>".$title."</h3>";
-			}
-			elseif (!$title && $counturlset == 1)
-			{
-				echo "<h3 class=\"feed_element_title\">".$this->link($feed->get_permalink(), '', $feed->get_title(), '', 1, 1)."</h3>";
-			}
-			elseif (!$title && $counturlset > 1)
-			{
-				echo "<h3>".$this->get_translation('FeedMulti')."</h3>";
-			}
-
-			echo $lastitems;
-		}
-
-		// Default
-		else
-		{
-			if ($title != '' && $counturlset == 1)
-			{
-				echo "<p class=\"layout-box\"><span>".$this->get_translation('FeedTitle').": <strong>".$this->link($feed->get_permalink(), '', $title, '', 1, 1)."</strong>".$lastitems."<span></p>";
-			}
-			if ($title != '' && $counturlset > 1)
-			{
-				echo "<p class=\"layout-box\"><span>".$this->get_translation('FeedTitle').": <strong>".$title."</strong>".$lastitems."</span></p>";
-			}
-			elseif (!$title && $counturlset == 1)
-			{
-				echo "<p class=\"layout-box\"><span>".$this->get_translation('FeedTitle').": <strong>".$this->link($feed->get_permalink(), '', $feed->get_title(), '', 1, 1)."</strong>".$lastitems."</span></p>";
-			}
-			elseif (!$title && $counturlset > 1)
-			{
-				echo "<p class=\"layout-box\"><span><strong>".$this->get_translation('FeedMulti')."</strong>".$lastitems."</span></p>";
-			}
-		}
-	}
-
-	$current = 1;
-
-	if ($counturlset > 1)
-	{
-		foreach ($feed->get_items() as $item)
-		{
-			/* Here, we'll use the $item->get_feed() method to gain access to the parent feed-level data for the specified item. */
-			$xfeed = $item->get_feed();
-
-			if ($item->get_date())
-				//Should have proper Unix time - 'U'
-				$date = $item->get_date('U');
-			else
-				$date = 0;
-
-			echo "<div class=\"feed\">";
-			echo "<".$headertag;
-
-			if($nomark)
-				echo " class=\"feed_element_title\"";
-
-			echo ">".$this->link($item->get_permalink(), '', $item->get_title(), '', 1, 1)."</".$headertag.">";
-			echo "<p class=\"note\"><span>".$this->get_translation('FeedSource')." ".$this->link($xfeed->get_permalink(), '', $xfeed->get_title(), '', 1, 1)." | ".$item->get_date('d.m.Y g:i')." | ";
-
-			if (($time == 'yes') && ($date != 0))
-				echo intervalCalc($date);
-
-			echo "</span></p>";
-			echo "<p class=\"feed-content\">".$item->get_content()."</p>";
-			echo "</div>";
-
-			if (($max)&&($current == $max))
-				break;
-			else
-				$current++;
-		}
+		#break;
 	}
 	else
 	{
-		// Go through all of the items in the feed
-		foreach ($feed->get_items() as $item)
+		$headertag = 'h4';
+
+		if ($title != 'no')
 		{
-			// get strings
-			$href = $item->get_permalink();
-			$title = $item->get_title();
+			if (($max)&&($max > 0))
+			{
+				$lastitems = " (last ".$max." items)";
+			}
 
-			if ($item->get_date())
-				//Should have proper Unix time - 'U'
-				$date = $item->get_date('U');
+			// Make nice if $nomark == 1
+			if ($nomark)
+			{
+				if ($title != '' && $counturlset == 1)
+				{
+					echo "<h3 class=\"feed_element_title\">".$this->link($feed->get_permalink(), '', $title, '', 1, 1)."</h3>";
+				}
+				if ($title != '' && $counturlset > 1)
+				{
+					echo "<h3>".$title."</h3>";
+				}
+				else if (!$title && $counturlset == 1)
+				{
+					echo "<h3 class=\"feed_element_title\">".$this->link($feed->get_permalink(), '', $feed->get_title(), '', 1, 1)."</h3>";
+				}
+				else if (!$title && $counturlset > 1)
+				{
+					echo "<h3>".$this->get_translation('FeedMulti')."</h3>";
+				}
+
+				echo $lastitems;
+			}
+
+			// Default
 			else
-				$date = 0;
+			{
+				if ($title != '' && $counturlset == 1)
+				{
+					echo "<p class=\"layout-box\"><span>".$this->get_translation('FeedTitle').": <strong>".$this->link($feed->get_permalink(), '', $title, '', 1, 1)."</strong>".$lastitems."<span></p>";
+				}
 
-			echo "<div class=\"feed\">";
+				if ($title != '' && $counturlset > 1)
+				{
+					echo "<p class=\"layout-box\"><span>".$this->get_translation('FeedTitle').": <strong>".$title."</strong>".$lastitems."</span></p>";
+				}
+				else if (!$title && $counturlset == 1)
+				{
+					echo "<p class=\"layout-box\"><span>".$this->get_translation('FeedTitle').": <strong>".$this->link($feed->get_permalink(), '', $feed->get_title(), '', 1, 1)."</strong>".$lastitems."</span></p>";
+				}
+				else if (!$title && $counturlset > 1)
+				{
+					echo "<p class=\"layout-box\"><span><strong>".$this->get_translation('FeedMulti')."</strong>".$lastitems."</span></p>";
+				}
+			}
+		}
 
-			// headline
-			#echo "<a target=\"_blank\" href=\"$href\" class=\"outerlink\">$title</a>";
-			echo "<".$headertag.">".$this->link($href, '', $title, '', 1, 1)."</".$headertag.">";
+		$current = 1;
 
-			if (($time == 'yes') && ($date != 0))
-				echo "<p class=\"note\"><span>".intervalCalc($date)."</span></p>";
+		if ($counturlset > 1)
+		{
+			foreach ($feed->get_items() as $item)
+			{
+				/* Here, we'll use the $item->get_feed() method to gain access to the parent feed-level data for the specified item. */
+				$xfeed = $item->get_feed();
 
-			echo "<p class=\"feed-content\">".$item->get_content()."</p>";
+				if ($item->get_date())
+				{
+					// should have proper Unix time - 'U'
+					$date = $item->get_date('U');
+				}
+				else
+				{
+					$date = 0;
+				}
 
-			// item-paragraph ending
-			echo "</div>";
+				echo "<div class=\"feed\">";
+				echo "<".$headertag;
 
-			if (($max) && ($current == $max))
-				break;
-			else
-				$current++;
+				if($nomark)
+				{
+					echo " class=\"feed_element_title\"";
+				}
+
+				echo ">".$this->link($item->get_permalink(), '', $item->get_title(), '', 1, 1)."</".$headertag.">";
+				echo "<p class=\"note\"><span>".$this->get_translation('FeedSource')." ".$this->link($xfeed->get_permalink(), '', $xfeed->get_title(), '', 1, 1)." | ".$item->get_date('d.m.Y g:i')." | ";
+
+				if (($time == 'yes') && ($date != 0))
+				{
+					echo intervalCalc($date);
+				}
+
+				echo "</span></p>";
+				echo "<p class=\"feed-content\">".$item->get_content()."</p>";
+				echo "</div>";
+
+				if (($max)&&($current == $max))
+				{
+					break;
+				}
+				else
+				{
+					$current++;
+				}
+			}
+		}
+		else
+		{
+			// Go through all of the items in the feed
+			foreach ($feed->get_items() as $item)
+			{
+				// get strings
+				$href	= $item->get_permalink();
+				$title	= $item->get_title();
+
+				if ($item->get_date())
+				{
+					// should have proper Unix time - 'U'
+					$date = $item->get_date('U');
+				}
+				else
+				{
+					$date = 0;
+				}
+
+				echo "<div class=\"feed\">";
+
+				// headline
+				#echo "<a target=\"_blank\" href=\"$href\" class=\"outerlink\">$title</a>";
+				echo "<".$headertag.">".$this->link($href, '', $title, '', 1, 1)."</".$headertag.">";
+
+				if (($time == 'yes') && ($date != 0))
+				{
+					echo "<p class=\"note\"><span>".intervalCalc($date)."</span></p>";
+				}
+
+				echo "<p class=\"feed-content\">".$item->get_content()."</p>";
+
+				// item-paragraph ending
+				echo "</div>";
+
+				if (($max) && ($current == $max))
+				{
+					break;
+				}
+				else
+				{
+					$current++;
+				}
+			}
 		}
 	}
-}
-if (!$nomark)
-{
-	echo "</div>\n";
-}
 
+	if (!$nomark)
+	{
+		echo "</div>\n";
+	}
+}
 ?>
