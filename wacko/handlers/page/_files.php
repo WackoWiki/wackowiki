02e2cba21b64dca5106a668fdf7f799bf35da3a7
vendeeglobe <54716082+vendeeglobe@users.noreply.github.com>
2011-10-20 13:51:56 +0200

    minor fix

---
+++ wacko/handlers/page/_files.php
@@ -0,0 +1,101 @@
+<?php
+
+if (!defined('IN_WACKO'))
+{
+	exit;
+}
+
+if ($this->has_access('read'))
+{
+	// store files display in session
+	if (!isset($_SESSION[$this->config['session_prefix'].'_'.'show_files'][$this->page['page_id']]))
+	{
+		$_SESSION[$this->config['session_prefix'].'_'.'show_files'][$this->page['page_id']] = ($this->user_wants_files() ? '1' : '0');
+	}
+
+	if(isset($_GET['show_files']))
+	{
+		switch($_GET['show_files'])
+		{
+			case '0':
+				$_SESSION[$this->config['session_prefix'].'_'.'show_files'][$this->page['page_id']] = 0;
+				break;
+			case '1':
+				$_SESSION[$this->config['session_prefix'].'_'.'show_files'][$this->page['page_id']] = 1;
+				break;
+		}
+	}
+
+	// display files!
+	if ($this->page && $_SESSION[$this->config['session_prefix'].'_'.'show_files'][$this->page['page_id']])
+	{
+		// display files header
+		?>
+	<div id="filesheader">
+	<?php echo "<a href=\"".$this->href('', '', 'show_files=0')."\" title=\"".$this->get_translation('HideFiles')."\">".$this->get_translation('Files_all')."</a>"; ?>
+	</div>
+
+		<?php
+		echo "<div class=\"files\">";
+		echo $this->action('files', array('nomark' => 1));
+		echo "</div>";
+
+		// display form
+		if ($user = $this->get_user())
+		{
+			$user_name = strtolower($this->get_user_name());
+			$registered = true;
+		}
+		else
+		{
+			$user_name = GUEST;
+		}
+
+		if (isset($registered)
+			&&
+				(
+					($this->config['upload'] === true) || ($this->config['upload'] == 1) ||
+					($this->check_acl($user_name, $this->config['upload']))
+				)
+			)
+		{
+			echo "<div class=\"filesform\">\n";
+			echo $this->action('upload', array('nomark' => 1));
+			echo "</div>\n";
+		}
+	}
+	else
+	{
+		echo "<div id=\"filesheader\">";
+
+		if ($this->page['page_id'])
+		{
+			// load files for this page
+			$files = $this->load_all(
+				"SELECT upload_id ".
+				"FROM ".$this->config['table_prefix']."upload ".
+				"WHERE page_id = '". quote($this->dblink, $this->page['page_id']) ."'");
+		}
+		else
+		{
+			$files = array();
+		}
+
+		switch ($c = count($files))
+		{
+			case 0:
+				$show_files = $this->get_translation('Files_0');
+				break;
+			case 1:
+				$show_files = $this->get_translation('Files_1');
+				break;
+			default:
+				$show_files = str_replace('%1', $c, $this->get_translation('Files_n'));
+		}
+
+		echo "<a href=\"".$this->href('', '', 'show_files=1#filesheader')."\" title=\"".$this->get_translation('ShowFiles')."\">".$show_files."</a>";
+		echo "</div>\n";
+	}
+}
+
+?>
---
+++ wacko/handlers/page/_rating.php
@@ -0,0 +1,87 @@
+<?php
+
+if (!defined('IN_WACKO'))
+{
+	exit;
+}
+
+if ($this->config['footer_rating'] != 0 && ($this->config['footer_rating'] != 2 || $this->get_user()))
+{
+	// registering local functions
+	// determine if user has rated a given page
+	function handler_show_page_is_rated(&$engine, $id)
+	{
+		$cookie	= $engine->get_cookie('rating');
+		$ids	= explode(';', $cookie);
+
+		if ($id = array_search($id, $ids))
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+}
+
+// determine if user has rated this page
+if (handler_show_page_is_rated($this, $this->page['page_id']) === false && (isset($_GET['show_rating']) && $_GET['show_rating'] != 1) )
+{
+	// display rating header
+	echo '<a name="rating"></a>';
+	echo '<div id="rateheader">';
+	echo $this->get_translation('RatingHeader').' [<a href="'.$this->href('', '', 'show_rating=1').'#rating">'.$this->get_translation('RatingResults').'</a>]';
+	echo "</div>\n";
+
+	// display rating form
+	echo '<div class="rating">'.$this->form_open('rate').'';
+	echo '<input id="minus3" name="value" type="radio" value="-3" /><label for="minus3">-3</label>'.
+				 '<input id="minus2" name="value" type="radio" value="-2" /><label for="minus2">-2</label>'.
+				 '<input id="minus1" name="value" type="radio" value="-1" /><label for="minus1">-1</label>'.
+				 '<input id="plus0" name="value" type="radio" value="0" /><label for="plus0"> 0</label>'.
+				 '<input id="plus1" name="value" type="radio" value="1" /><label for="plus1">+1</label>'.
+				 '<input id="plus2" name="value" type="radio" value="2" /><label for="plus2">+2</label>'.
+				 '<input id="plus3" name="value" type="radio" value="3" /><label for="plus3">+3</label>'.
+				 '<input name="rate" id="submit" type="submit" value="'.$this->get_translation('RatingSubmit').'" />';
+	echo ''.$this->form_close().'</div>';
+}
+else
+{
+	$results = $this->load_single(
+				"SELECT page_id, value, voters ".
+				"FROM {$this->config['table_prefix']}rating ".
+				"WHERE page_id = {$this->page['page_id']} ".
+				"LIMIT 1");
+
+	if ($results['voters'] > 0)			$results['ratio'] = $results['value'] / $results['voters'];
+	if (is_float($results['ratio']))	$results['ratio'] = round($results['ratio'], 2);
+	if ($results['ratio'] > 0)			$results['ratio'] = '+'.$results['ratio'];
+
+	// display rating header
+	echo '<a name="rating"></a>';
+	echo '<div id="rateheader">';
+	echo $this->get_translation('RatingHeaderResults').
+	(handler_show_page_is_rated($this, $this->page['page_id']) === false
+	? ' [<a href="'.$this->href('', '', 'show_rating=0').'#rating">'.$this->get_translation('RatingForm').'</a>]'
+	: '');
+	echo "</div>\n";
+
+	// display rating results
+	if (isset($results['ratio']))
+	{
+		echo '<div class="rating">';
+		echo ''.$this->get_translation('RatingTotal').': <strong>'.$results['ratio'].'</strong>'.
+					 ' '.
+					 ''.$this->get_translation('RatingVoters').': <strong>'.$results['voters'].'</strong>';
+		echo '</div>';
+	}
+	else
+	{
+		echo '<div class="rating">';
+		echo '<em>'.$this->get_translation('RatingNotRated').'</em>';
+		echo '</div>';
+	}
+}
+
+?>
