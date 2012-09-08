1903d14ef91a3ac89787332495c71a8441a21e26
vendeeglobe <vendeeglobe@localhost>
2012-09-08 10:01:41 +0200

    Fixed bug#427: patch to wrap lines in emails

--- wacko/formatters/highlight/email.php
+++ wacko/formatters/highlight/email.php
@@ -8,8 +8,8 @@ $text = str_replace("&gt;", ">", $text);
 $text = preg_replace("/^([^\s\n>]*?(>{2})*>)([^>].*)$/m","<span class=\"email1\">\\1\\3</span>",$text);
 $text = preg_replace("/^([^\s\n>]*?(>{2})+)([^>].*)$/m", "<span class=\"email2\">\\1\\3</span>",$text);
 
-echo "<pre class=\"code\">";
+echo '<pre class="code-break">';
 echo $text;
-echo "</pre>";
+echo '</pre>';
 
 ?>
