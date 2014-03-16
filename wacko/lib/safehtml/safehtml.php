fd1f77b40d3e886878d05f84648124daa5b278e2
vendeeglobe <vendeeglobe@localhost>
2014-03-16 15:17:46 +0100

    Fixed bug#436: Replace preg_replace() e modifier with preg_replace_callback - part 1

--- wacko/lib/safehtml/safehtml.php
+++ wacko/lib/safehtml/safehtml.php
@@ -350,10 +350,10 @@ class SafeHTML
                     }
                 }
 
-                $tempval = preg_replace('/&#(\d+);?/me', "chr('\\1')", $value); //"'
-                $tempval = preg_replace(
-                    '/&#x([0-9a-f]+);?/mei',
-                    "chr(hexdec('\\1'))",
+                $tempval = preg_replace_callback('/&#(\d+);?/m', function ($matches) { return chr($matches[1]); }, $value); //"'
+                $tempval = preg_replace_callback(
+                	'/&#x([0-9a-f]+);?/mi',
+                	function ($matches) { return chr(hexdec($matches[1])); },
                     $tempval
                 );
 
