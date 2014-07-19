7570b7801a5e2e263ca1da1a8af7aed3cdd44a1a
vendeeglobe <vendeeglobe@localhost>
2014-07-19 10:45:59 +0200

    renamed ms to wordprocessor - part 2

--- wacko/themes/coffee/appearance/headerwordprocessor.php
+++ wacko/themes/coffee/appearance/headerwordprocessor.php
@@ -5,18 +5,17 @@ header('Content-Type: text/html; charset='.$this->get_charset());
 
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->page['lang'] ?>" lang="<?php echo $this->page['lang'] ?>">
 <head>
-<title><?php echo htmlspecialchars($this->config['site_name'], ENT_COMPAT | ENT_HTML401, HTML_ENTITIES_CHARSET).' : '.(isset($this->page['title']) ? $this->page['title'] :$this->tag); ?></title>
+<title><?php echo htmlspecialchars($this->config['site_name'], ENT_COMPAT | ENT_HTML401, HTML_ENTITIES_CHARSET).' : '.(isset($this->page['title']) ? $this->page['title'] : $this->tag); ?></title>
 <?php // do not index alternative print pages
 echo "<meta name=\"robots\" content=\"noindex, nofollow\" />\n";?>
 <meta http-equiv="content-type" content="text/html; charset=<?php echo $this->get_charset(); ?>" />
 <meta name="keywords" content="<?php echo $this->config['meta_keywords'] ?>" />
 <meta name="description" content="<?php echo $this->config['meta_description'] ?>" />
-<link rel="stylesheet" type="text/css" href="<?php echo $this->config['theme_url'] ?>css/msword.css" />
+<link rel="stylesheet" type="text/css" href="<?php echo $this->config['theme_url'] ?>css/wordprocessor.css" />
 </head>
 
 <body>
 
 <div class="header">
-<h1><?php echo $this->config['site_name'] ?>: <?php echo $this->compose_link_to_page($this->translit($this->tag), '', (isset($this->page['title']) ? $this->page['title'] : $this->tag)); ?>
-(<?php echo $this->get_translation('MsWordVersion');?>)</h1>
+<h1><?php echo $this->config['site_name'] ?>: <?php echo '<a href="'.$this->config['base_url'].$this->get_translation('TextSearchPage').($this->config['rewrite_mode'] ? "?" : "&amp;").'phrase='.urlencode($this->tag).'">'.(isset($this->page['title']) ? $this->page['title'] : $this->tag).'</a>'; ?></h1>
 </div>
