6bf43c2a9f7a4174bc9217e8649118385c1d3084
vendeeglobe <vendeeglobe@localhost>
2014-07-19 10:37:26 +0200

    renamed ms to wordprocessor - part 1

--- community/themes/mambo/appearance/headerwordprocessor.php
+++ community/themes/mambo/appearance/headerwordprocessor.php
@@ -6,15 +6,16 @@
 <html>
 <head>
   <title><?php echo htmlspecialchars($this->config['site_name'], ENT_COMPAT | ENT_HTML401, HTML_ENTITIES_CHARSET)." : ".(isset($this->page['title']) ? $this->page['title'] : $this->tag); ?></title>
-<meta name="robots" content="noindex, nofollow" />
+<?php // do not index alternative print pages
+  echo "<meta name=\"robots\" content=\"noindex, nofollow\" />\n";?>
   <meta http-equiv="content-type" content="text/html; charset=<?php echo $this->get_charset(); ?>" />
   <meta name="keywords" content="<?php echo $this->config['meta_keywords'] ?>" />
   <meta name="description" content="<?php echo $this->config['meta_description'] ?>" />
-  <link rel="stylesheet" type="text/css" href="<?php echo $this->config['theme_url'] ?>css/msword.css" />
+  <link rel="stylesheet" type="text/css" href="<?php echo $this->config['theme_url'] ?>css/wordprocessor.css" />
 </head>
 
 <body>
-<!--BEGINN: SEITE-->
-<div id="page"><div class="header">
-  <h1><?php echo $this->config['site_name'] ?>: <?php echo '<a href="'.$this->config['base_url'].$this->get_translation('TextSearchPage').($this->config['rewrite_mode'] ? "?" : "&amp;").'phrase='.urlencode($this->tag).'">'.(isset($this->page['title']) ? $this->page['title'] : $this->tag).'</a>'; ?></h1>
+
+<div class="header">
+<h1><?php echo $this->config['site_name'] ?>: <?php echo '<a href="'.$this->config['base_url'].$this->get_translation('TextSearchPage').($this->config['rewrite_mode'] ? "?" : "&amp;").'phrase='.urlencode($this->tag).'">'.(isset($this->page['title']) ? $this->page['title'] : $this->tag).'</a>'; ?></h1>
 </div>
