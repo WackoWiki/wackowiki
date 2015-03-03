a3eed202e3acb4ad1a85c1c9d4a9894ff62ebde4
EoNy <12676106+Trojer@users.noreply.github.com>
2015-03-03 15:40:15 +0100

    initial files for bootstrap theme

---
+++ community/themes/bootstrap/appearance/footerprint.php
@@ -0,0 +1,7 @@
+<div id="footer">
+	&copy; <?php echo date("Y"); ?> &quot;<?php echo $this->config['site_name']; ?>&quot;.
+	<?php echo $this->get_translation('TermsOfUse'); ?>: <a href="<?php echo htmlspecialchars($this->href('', $this->config['policy_page']), ENT_COMPAT | ENT_HTML401, HTML_ENTITIES_CHARSET); ?>"><?php echo $this->config['base_url'].$this->config['policy_page']; ?></a>
+</div>
+<?php
+// Don't place final </body></html> here. Wacko closes HTML automatically.
+?>
