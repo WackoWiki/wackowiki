5a9060681676f0bffb2f74bf1864fc101d7f7bd6
vendeeglobe <vendeeglobe@localhost>
2015-04-12 13:29:40 +0200

    code cleanup

--- community/themes/bluenatic/appearance/headerwordprocessor.php
+++ community/themes/bluenatic/appearance/headerwordprocessor.php
@@ -1,7 +1,7 @@
 <?php
 header('Content-Type: text/html; charset='.$this->get_charset());
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html>
 
 <head>
 <title><?php echo htmlspecialchars($this->config['site_name'], ENT_COMPAT | ENT_HTML401, HTML_ENTITIES_CHARSET)." : ".(isset($this->page['title']) ? $this->page['title'] : $this->tag); ?></title>
--- community/themes/nifty/appearance/header.php
+++ community/themes/nifty/appearance/header.php
@@ -7,7 +7,7 @@
 // HTTP header with right charset settings
 header("Content-Type: text/html; charset=".$this->get_charset());
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html>
 <html lang="<?php echo $this->page['lang'] ?>">
 <head>
 	<title><?php echo htmlspecialchars($this->config['site_name'], ENT_COMPAT | ENT_HTML401, HTML_ENTITIES_CHARSET).' : '.(isset($this->page['title']) ? $this->page['title'] : $this->add_spaces($this->tag)).($this->method != 'show' ? ' ('.$this->method.')' : '');?></title>
