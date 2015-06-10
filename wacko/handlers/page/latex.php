0af9a775276d5237bdbc03740a7eddf9c9ee149b
vendeeglobe <vendeeglobe@localhost>
2015-06-10 14:05:57 +0200

    minor fixes

--- wacko/handlers/page/latex.php
+++ wacko/handlers/page/latex.php
@@ -23,12 +23,12 @@ if ($this->has_access('read'))
 		// comment header?
 		if ($this->page['comment_on_id'])
 		{
-			print("<div class=\"commentinfo\">".$this->get_translation('ThisIsCommentOn')." ".$this->compose_link_to_page($this->get_page_tag($this->page['comment_on_id']), "", "", 0).", ".$this->get_translation('PostedBy')." ".($this->is_wiki_name($this->page['user_name']) ? $this->link($this->page['user_name']) : $this->page['user_name'])." ".$this->get_translation('At')." ".$this->page['modified']."</div>");
+			echo '<div class="commentinfo">'.$this->get_translation('ThisIsCommentOn')." ".$this->compose_link_to_page($this->get_page_tag($this->page['comment_on_id']), "", "", 0).", ".$this->get_translation('PostedBy')." ".($this->is_wiki_name($this->page['user_name']) ? $this->link($this->page['user_name']) : $this->page['user_name'])." ".$this->get_translation('At')." ".$this->page['modified']."</div>";
 		}
 
 		if ($this->page['latest'] == 0)
 		{
-			 '<div class="revisioninfo">'.
+			echo '<div class="revisioninfo">'.
 			str_replace('%1',$this->href(),
 			str_replace('%2',$this->tag,
 			str_replace('%3',$this->page['modified'],
