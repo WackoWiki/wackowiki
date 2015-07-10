cdb157b18003d0b113dcf2c8f1b3b427415c1532
vendeeglobe <vendeeglobe@localhost>
2015-07-10 12:53:34 +0200

    only code styling

--- wacko/js/captcha.js
+++ wacko/js/captcha.js
@@ -1,9 +1,8 @@
-function new_freecap()
-{
-         thesrc = document.getElementById("freecap").src;
-         if (thesrc.indexOf("&") !== -1) 
-	    {
-         	thesrc = thesrc.substring(0, thesrc.lastIndexOf("&"));
-            }
-	 document.getElementById("freecap").src = thesrc + "&" + Math.round(Math.random() * 100000);
+function new_freecap() {
+  thesrc = document.getElementById('freecap').src;
+  if (thesrc.indexOf('&') !== - 1) {
+    thesrc = thesrc.substring(0, thesrc.lastIndexOf('&'));
+  }
+  document.getElementById('freecap').src = thesrc + '&'
+  + Math.round(Math.random() * 100000);
 }
