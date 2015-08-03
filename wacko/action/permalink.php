7b0320f252e0eb390a66c53db041ee17c1e94ae1
vendeeglobe <vendeeglobe@localhost>
2015-08-04 01:23:52 +0200

    minor fixes

--- wacko/action/permalink.php
+++ wacko/action/permalink.php
@@ -12,6 +12,6 @@ if (isset($_GET['revision_id']))
 	$revision_id = (int)$_GET['revision_id'];
 }
 
-echo '<a href="'.$this->href('', $this->config['permalink_page'], 'page_id='.$this->page['page_id'].(isset($revision_id) ? '&rev_id='.$revision_id : '')).'" title="'.$this->get_translation('PermaLinkTip').'" rel="nofollow">'.$this->get_translation('PermaLink').'</a>';
+echo '<a href="'.$this->href('', $this->config['permalink_page'], 'page_id='.$this->page['page_id'].(isset($revision_id) ? '&amp;rev_id='.$revision_id : '')).'" title="'.$this->get_translation('PermaLinkTip').'" rel="nofollow">'.$this->get_translation('PermaLink').'</a>';
 
 ?>
