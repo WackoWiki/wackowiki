bb62c691166cdd7dbd56fd61affd4a8150704d42
vendeeglobe <54716082+vendeeglobe@users.noreply.github.com>
2015-08-14 10:20:33 +0200

    minor fixes

--- community/theme/oppfra/appearance/headerprint.php
+++ community/theme/oppfra/appearance/headerprint.php
@@ -17,7 +17,7 @@
 <!--BEGINN: SEITE-->
 <div id="page"><div class="header">
   <h1>
-  <?php echo file_exists("images/".$this->config['site_name'].".png")?"<img src='/images/".$this->config['site_name'].".png' alt='".$this->config['site_name']."' />":$this->config["site_name"] ?> : <?php echo $this->tag ?>
+  <?php echo file_exists("image/".$this->config['site_name'].".png")?"<img src='/image/".$this->config['site_name'].".png' alt='".$this->config['site_name']."' />":$this->config["site_name"] ?> : <?php echo $this->tag ?>
   </h1>
 </div>
 
