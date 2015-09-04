1301b5d95c38f945c29ff725d02d1723e872aff2
vendeeglobe <54716082+vendeeglobe@users.noreply.github.com>
2015-09-04 14:32:09 +0200

    localized WikiEdit - part 2

---
+++ wacko/js/lang/wikiedit.fr.js
@@ -0,0 +1,57 @@
+lang = {
+	// wikiedit.js
+	HelpAbout:				"Help & About",
+	HelpFormatting :		"Help",
+	HelpFormattingTip :		"Help on Wiki-formatting",
+	HelpFormattingPage :	"Doc/Francophone/MiseEnForme",
+	
+	Heading1 :				"Heading 1",
+	Heading2 :				"Heading 2",
+	Heading3 :				"Heading 3",
+	Heading4 :				"Heading 4",
+	Heading5 :				"Heading 5",
+	Bold :					"Bold",
+	Italic :				"Italic",
+	Underline :				"Underline",
+	Strikethrough :			"Strikethrough",
+	Code :					"Code",
+	List :					"List",
+	NumberedList :			"Numbered list",
+	Left :					"Left",
+	Center :				"Center",
+	Right :					"Right",
+	Justify :				"Justify",
+	Outdent :				"Outdent",
+	Indent :				"Indent",
+	CodeWrapper :			"Code Wrapper",
+	HTML :					"HTML",
+	Action :				"Action",
+	Line :					"Line",
+	Signature :				"Signature",
+	Quote :					"Quote",
+	MarkedText :			"Marked text",
+	HighlightText :			"Highlight text",
+	Shade :					"Shade",
+	Hyperlink :				"Hyperlink",
+	InsertTable :			"Insert table",
+	
+	HelpAboutTip : 			' Shortcuts:\n'+
+							' Ctrl+B - Bold\n'+
+							' Ctrl+I - Italic\n'+
+							' Ctrl+U - Underline\n'+
+							' Ctrl+Shift+S - Strikethrough\n'+
+							' Ctrl+Shift+1 .. 4 - Heading 1..4\n'+
+							' Alt+I or Ctrl+T - Indent\n'+
+							' Alt+U or Ctrl+Shift+T - Unindent\n'+
+							' Ctrl+J - MarkUp (!!)\n'+
+							' Ctrl+H - MarkUp (??)\n'+
+							' Alt+L - Link\n'+
+							' Ctrl+L - Link with description\n'+
+							' Ctrl+Shift+L - Unordered List\n'+
+							' Ctrl+Shift+N - Ordered List\n'+
+							' Ctrl+Shift+O - Ordered List\n'+
+							' Ctrl+= - Small text\n'+
+							' Ctrl+Shift+Minus - Horizontal line\n'+
+							' NB: all Alt-shortcuts do not work in Opera.\n',
+
+};
