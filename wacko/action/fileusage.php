95b4ead3dda78d727b0b12411ed8e1805b523317
vendeeglobe <54716082+vendeeglobe@users.noreply.github.com>
2015-10-08 12:51:40 +0200

    added action fileusage
    shows pages linking to a uploaded file
    e.g. file:path/image.png
    
    used in remove handler to show related pages linking recent uploaded file

---
+++ wacko/action/fileusage.php
@@ -0,0 +1,63 @@
+<?php
+
+if (!defined('IN_WACKO'))
+{
+	exit;
+}
+
+if (!isset($file_id))	$file_id = '';
+if (!isset($nomark))	$nomark = '';
+if (!isset($title))		$title = '';
+
+if ($file_id)
+{
+	if ($pages = $this->load_files_linked($file_id))
+	{
+		if(!$nomark)
+		{
+			echo '<div class="layout-box"><p class="layout-box"><span>'.$this->get_translation('FileUsage').': '.''."</span></p>\n";
+		}
+
+		foreach ($pages as $page)
+		{
+			if ($page['tag'])
+			{
+				if ($this->config['hide_locked'])
+				{
+					$access = $this->has_access('read', $page['page_id']);
+				}
+				else
+				{
+					$access = true;
+				}
+
+				if ($access)
+				{
+					if ($title == 1)
+					{
+						$_link = $this->link('/'.$page['tag'], '', $page['title']);
+					}
+					else
+					{
+						$_link = $this->link('/'.$page['tag'], '', $page['tag'], $page['title']);
+					}
+
+					if (strpos($_link, 'span class="missingpage"') === false)
+					{
+						echo ($_link."<br />\n");
+					}
+				}
+			}
+		}
+
+		if(!$nomark)
+		{
+			echo "</div>\n";
+		}
+	}
+	else
+	{
+		echo $this->get_translation('NoFileUsage');
+	}
+}
+?>
