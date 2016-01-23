58ce38f66ad9de716600f27784985930054b3b87
vendeeglobe <vendeeglobe@localhost>
2016-01-23 14:59:08 +0100

    minor fixes

--- wacko/lib/bad_behavior/bad-behavior-wackowiki.php
+++ wacko/lib/bad_behavior/bad-behavior-wackowiki.php
@@ -18,8 +18,8 @@ with this program. If not, see <http://www.gnu.org/licenses/>.
 Please report any problems to bad . bots AT ioerror DOT us
 http://bad-behavior.ioerror.us/
 
-WackoWiki implementation, 2015 WackoWiki Team
-Version 0.4
+WackoWiki implementation, 2016 WackoWiki Team
+Version 0.5
 http://wackowiki.sourceforge.net/doc/Dev/PatchesHacks/BadBehavior
 
 */
@@ -65,20 +65,22 @@ function bb2_table_structure($name)
 	// It's not paranoia if they really are out to get you.
 	$name_escaped = bb2_db_escape($name);
 	return "CREATE TABLE IF NOT EXISTS `$name_escaped` (
-		`log_id` INT(11) NOT NULL auto_increment,
-		`ip` VARCHAR(45) NOT NULL,
-		`host` VARCHAR(2083) NOT NULL,
+		`log_id` INT(11) NOT NULL AUTO_INCREMENT,
+		`ip` VARCHAR(45) NOT NULL DEFAULT '',
+		`host` VARCHAR(2083) NOT NULL DEFAULT '',
 		`date` DATETIME NOT NULL default '0000-00-00 00:00:00',
-		`request_method` VARCHAR(8) NOT NULL,
-		`request_uri` VARCHAR(2083) NOT NULL,
-		`server_protocol` VARCHAR(12) NOT NULL,
+		`request_method` VARCHAR(8) NOT NULL DEFAULT '',
+		`request_uri` VARCHAR(2083) NOT NULL DEFAULT '',
+		`request_uri_hash` CHAR(40) NOT NULL DEFAULT '',
+		`server_protocol` VARCHAR(12) NOT NULL DEFAULT '',
 		`http_headers` TEXT NOT NULL,
 		`user_agent` TEXT NOT NULL,
-		`user_agent_hash` CHAR(40) NOT NULL,
+		`user_agent_hash` CHAR(40) NOT NULL DEFAULT '',
 		`request_entity` TEXT NOT NULL,
-		`status_key` VARCHAR(10) NOT NULL,
+		`status_key` VARCHAR(10) NOT NULL DEFAULT '',
 		PRIMARY KEY (`log_id`),
 		KEY `idx_staus_key` (`status_key`),
+		KEY `idx_request_uri_hash` (`request_uri_hash`),
 		KEY `idx_user_agent_hash` (`user_agent_hash`),
 		KEY `idx_ip` (`ip`),
 		KEY `idx_request_method` (`request_method`)
@@ -95,6 +97,7 @@ function bb2_insert($settings, $package, $status_key)
 	$date				= bb2_db_date();
 	$request_method		= bb2_db_escape($package['request_method']);
 	$request_uri		= bb2_db_escape($package['request_uri']);
+	$request_uri_hash	= hash('sha1', $request_uri);
 	$server_protocol	= bb2_db_escape($package['server_protocol']);
 	$user_agent			= bb2_db_escape($package['user_agent']);
 	$user_agent_hash	= hash('sha1', $user_agent);
@@ -113,8 +116,8 @@ function bb2_insert($settings, $package, $status_key)
 	}
 
 	return "INSERT INTO `" . bb2_db_escape($settings['log_table']) . "`
-		(`ip`, `host`, `date`, `request_method`, `request_uri`, `server_protocol`, `http_headers`, `user_agent`, `user_agent_hash`, `request_entity`, `status_key`) VALUES
-		('$ip', '$host', '$date', '$request_method', '$request_uri', '$server_protocol', '$headers', '$user_agent', '$user_agent_hash', '$request_entity', '$status_key')";
+		(`ip`, `host`, `date`, `request_method`, `request_uri`, `request_uri_hash`, `server_protocol`, `http_headers`, `user_agent`, `user_agent_hash`, `request_entity`, `status_key`) VALUES
+		('$ip', '$host', '$date', '$request_method', '$request_uri', '$request_uri_hash', '$server_protocol', '$headers', '$user_agent', '$user_agent_hash', '$request_entity', '$status_key')";
 }
 
 // Return current time in the format preferred by your database.
