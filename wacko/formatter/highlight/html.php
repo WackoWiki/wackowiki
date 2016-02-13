83effa9dfcea8637632d4cbe4ef5098a31c696de
vendeeglobe <54716082+vendeeglobe@users.noreply.github.com>
2016-02-13 12:01:30 +0100

    only code styling

--- wacko/formatter/highlight/html.php
+++ wacko/formatter/highlight/html.php
@@ -14,122 +14,122 @@ $options['color']['entities'] = "orange";
 $options['line_numbers'] = false;
 
 $xhtml_tags = array(
-  "!DOCTYPE",
-  "a",
-  "abbr",
-  "acronym",
-  "address",
-  "applet",
-  "area",
-  "b",
-  "base",
-  "basefont",
-  "bgsound",
-  "bdo",
-  "big",
-  "blink",
-  "blockquote",
-  "body",
-  "br",
-  "button",
-  "caption",
-  "center",
-  "cite",
-  "code",
-  "col",
-  "colgroup",
-  "comment",
-  "dd",
-  "del",
-  "dfn",
-  "dir",
-  "div",
-  "dl",
-  "dt",
-  "em",
-  "embed",
-  "fieldset",
-  "font",
-  "form",
-  "frame",
-  "frameset",
-  "h",
-  "h1",
-  "h2",
-  "h3",
-  "h4",
-  "h5",
-  "h6",
-  "head",
-  "hr",
-  "hta:application",
-  "html",
-  "i",
-  "iframe",
-  "img",
-  "input",
-  "ins",
-  "isindex",
-  "kbd",
-  "label",
-  "legend",
-  "li",
-  "link",
-  "listing",
-  "map",
-  "marquee",
-  "menu",
-  "meta",
-  "multicol",
-  "nextid",
-  "nobr",
-  "noframes",
-  "noscript",
-  "object",
-  "ol",
-  "optgroup",
-  "option",
-  "p",
-  "param",
-  "plaintext",
-  "pre",
-  "q",
-  "s",
-  "samp",
-  "script",
-  "select",
-  "server",
-  "small",
-  "sound",
-  "spacer",
-  "span",
-  "strike",
-  "strong",
-  "style",
-  "sub",
-  "sup",
-  "table",
-  "tbody",
-  "td",
-  "textarea",
-  "textflow",
-  "tfoot",
-  "th",
-  "thead",
-  "title",
-  "tr",
-  "tt",
-  "u",
-  "ul",
-  "var",
-  "wbr",
-  "xmp"
-  );
+	"!DOCTYPE",
+	"a",
+	"abbr",
+	"acronym",
+	"address",
+	"applet",
+	"area",
+	"b",
+	"base",
+	"basefont",
+	"bgsound",
+	"bdo",
+	"big",
+	"blink",
+	"blockquote",
+	"body",
+	"br",
+	"button",
+	"caption",
+	"center",
+	"cite",
+	"code",
+	"col",
+	"colgroup",
+	"comment",
+	"dd",
+	"del",
+	"dfn",
+	"dir",
+	"div",
+	"dl",
+	"dt",
+	"em",
+	"embed",
+	"fieldset",
+	"font",
+	"form",
+	"frame",
+	"frameset",
+	"h",
+	"h1",
+	"h2",
+	"h3",
+	"h4",
+	"h5",
+	"h6",
+	"head",
+	"hr",
+	"hta:application",
+	"html",
+	"i",
+	"iframe",
+	"img",
+	"input",
+	"ins",
+	"isindex",
+	"kbd",
+	"label",
+	"legend",
+	"li",
+	"link",
+	"listing",
+	"map",
+	"marquee",
+	"menu",
+	"meta",
+	"multicol",
+	"nextid",
+	"nobr",
+	"noframes",
+	"noscript",
+	"object",
+	"ol",
+	"optgroup",
+	"option",
+	"p",
+	"param",
+	"plaintext",
+	"pre",
+	"q",
+	"s",
+	"samp",
+	"script",
+	"select",
+	"server",
+	"small",
+	"sound",
+	"spacer",
+	"span",
+	"strike",
+	"strong",
+	"style",
+	"sub",
+	"sup",
+	"table",
+	"tbody",
+	"td",
+	"textarea",
+	"textflow",
+	"tfoot",
+	"th",
+	"thead",
+	"title",
+	"tr",
+	"tt",
+	"u",
+	"ul",
+	"var",
+	"wbr",
+	"xmp"
+	);
 
-  $source = htmlspecialchars($text, ENT_COMPAT | ENT_HTML401, HTML_ENTITIES_CHARSET);
+	$source = htmlspecialchars($text, ENT_COMPAT | ENT_HTML401, HTML_ENTITIES_CHARSET);
 
-  $source = preg_replace_callback(
-             '/&lt;!--(.*?)--&gt;/s',
+	$source = preg_replace_callback(
+			'/&lt;!--(.*?)--&gt;/s',
 			function ($matches)
 			{
 				return
@@ -138,11 +138,11 @@ $xhtml_tags = array(
 				str_replace("=","=<!-- -->",
 				$matches[1])).
 				"--&gt;</span>"';
-  			},
-  $source);
+			},
+	$source);
 
-  $source = preg_replace_callback(
-             '/(&lt;style.*?&gt;)(.*?)&lt;\/style&gt;/s',
+	$source = preg_replace_callback(
+			'/(&lt;style.*?&gt;)(.*?)&lt;\/style&gt;/s',
 			function ($matches)
 			{
 				return
@@ -150,43 +150,44 @@ $xhtml_tags = array(
 				$this->format($matches[2], "highlight/css", array("nopre" => true, "notypo" => false)).
 				"&lt;/style&gt;"';
 			},
-  $source);
+	$source);
 
-  foreach($xhtml_tags as $i)
-  {
-   $source = preg_replace('/&lt;' . $i . '(&gt;|[[:space:]])/', '<span style="color: ' .$options['color']['tags']. ';font-weight:bold;">&lt;' .$i. '\\1</span>', $source);
-   $source = str_replace('&lt;/' .$i. '&gt;','<span style="color: ' .$options['color']['tags']. ';font-weight:bold;">&lt;/' .$i. '&gt;</span>',$source);
-  }
+	foreach($xhtml_tags as $i)
+	{
+		$source = preg_replace('/&lt;' . $i . '(&gt;|[[:space:]])/', '<span style="color: ' .$options['color']['tags']. ';font-weight:bold;">&lt;' .$i. '\\1</span>', $source);
+		$source = str_replace('&lt;/' .$i. '&gt;','<span style="color: ' .$options['color']['tags']. ';font-weight:bold;">&lt;/' .$i. '&gt;</span>',$source);
+	}
 
-  $source = str_replace('/&gt;','<span style="color: ' .$options['color']['tags']. ';font-weight:bold;">/&gt;</span>',$source);
-  $source = preg_replace(
-  '/([[:space:]]|&quot;|\'|\?)&gt;/',
-  '\\1<span style="color: ' .$options['color']['tags']. ';font-weight:bold;">&gt;</span>',
-  $source);
+	$source = str_replace('/&gt;','<span style="color: ' .$options['color']['tags']. ';font-weight:bold;">/&gt;</span>',$source);
+	$source = preg_replace(
+	'/([[:space:]]|&quot;|\'|\?)&gt;/',
+	'\\1<span style="color: ' .$options['color']['tags']. ';font-weight:bold;">&gt;</span>',
+	$source);
 
-  $source = preg_replace( "/([a-z-]+)=(&quot;|\')(.*?)\\2/i",
-             '<span style="color: ' .$options['color']['attributes']. ';font-weight:bold;">$1=</span><span style="color: '
-             .$options['color']['attributevalues']. ';">$2$3$2</span>', $source);
-             $source = preg_replace("/&amp;([a-z0-9]*?;)/i", '&amp;<span style="color: ' .$options['color']['entities']. ';">$1</span>', $source);
+	$source = preg_replace( "/([a-z-]+)=(&quot;|\')(.*?)\\2/i",
+			'<span style="color: ' .$options['color']['attributes']. ';font-weight:bold;">$1=</span><span style="color: '
+			.$options['color']['attributevalues']. ';">$2$3$2</span>', $source);
+			$source = preg_replace("/&amp;([a-z0-9]*?;)/i", '&amp;<span style="color: ' .$options['color']['entities']. ';">$1</span>', $source);
 
-             if ($options['line_numbers'] == true)
-             {
-             	$lines = preg_split("/(\n|<br \/>)/s",$source);
-             	$source = '<ol>';
-             	$i = 0;
+	if ($options['line_numbers'] == true)
+	{
+		$lines = preg_split("/(\n|<br \/>)/s",$source);
+		$source = '<ol>';
+		$i = 0;
 
-             	foreach ($lines as $line)
-             	{
-             		$i += 1;
-             		$source .= '<li id="l' .$i. '">' .rtrim($line). "</li>";
-             	}
+		foreach ($lines as $line)
+		{
+			$i += 1;
+			$source .= '<li id="l' .$i. '">' .rtrim($line). "</li>";
+		}
 
-             	$source .= '</ol>';
-             }
+		$source .= '</ol>';
+	}
 
-             echo '<!--notypo-->';
-             echo '<pre class="code">';
-             echo  str_replace("\t","  ",$source);
-             echo "</pre>";
-             echo '<!--/notypo-->';
-             ?>
+	echo '<!--notypo-->';
+	echo '<pre class="code">';
+	echo  str_replace("\t","  ",$source);
+	echo "</pre>";
+	echo '<!--/notypo-->';
+
+?>
