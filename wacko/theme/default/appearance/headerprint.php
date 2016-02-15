94884a0d7248c6e7084a420efd73bebb7f57ba19
vendeeglobe <vendeeglobe@localhost>
2016-02-15 17:04:37 +0100

    minor fixes

--- wacko/theme/default/appearance/headerprint.php
+++ wacko/theme/default/appearance/headerprint.php
@@ -2,24 +2,23 @@
 header('Content-Type: text/html; charset='.$this->get_charset());
 ?>
 <!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->page['page_lang'] ?>" lang="<?php echo $this->page['page_lang'] ?>">
-<head>
-<title><?php echo htmlspecialchars(($this->config['site_name']).' : '.(isset($this->page['title']) ? $this->page['title'] : $this->tag), ENT_COMPAT | ENT_HTML401, HTML_ENTITIES_CHARSET); ?></title>
-<meta name="robots" content="noindex, nofollow" />
-<meta http-equiv="content-type" content="text/html; charset=<?php echo $this->get_charset(); ?>" />
-<link rel="stylesheet" href="<?php echo $this->config['theme_url'] ?>css/print.css" />
-
-<link rel="start" href="/" />
-<link rel="copyright" href="<?php echo htmlspecialchars($this->href('', $this->config['policy_page']), ENT_COMPAT | ENT_HTML401, HTML_ENTITIES_CHARSET); ?>" />
-<link rel="prev" href="<?php echo $this->href() ?>" />
-</head>
-<body id="print">
-<div id="header">
-	<div class="container">
-		<?php // Print wackoname and wackopath ?>
-		<h1><?php echo $this->config['site_name']; ?>: <?php echo (isset($this->page['title']) ? $this->page['title'] : $this->get_page_path()); ?></h1>
-		<a href="<?php echo $this->config['base_url'] ?>"><?php echo rtrim($this->config['base_url'], "/"); ?></a><?php if ($this->page) { ?>&nbsp;&nbsp;&nbsp;&nbsp;<?php echo $this->get_translation('Version').": ".$this->get_time_formatted($this->page['modified']); } ?><br />
-		<?php echo $this->get_page_path() ?><br />
-	</div>
-</div>
+<html lang="<?php echo $this->page['page_lang'] ?>">
+	<head>
+		<meta charset="<?php echo $this->get_charset(); ?>" />
+		<title><?php echo htmlspecialchars(($this->config['site_name']).' : '.(isset($this->page['title']) ? $this->page['title'] : $this->tag), ENT_COMPAT | ENT_HTML401, HTML_ENTITIES_CHARSET); ?></title>
+		<meta name="robots" content="noindex, nofollow" />
+		<link rel="stylesheet" href="<?php echo $this->config['theme_url'] ?>css/print.css" />
+		<link rel="start" href="/" />
+		<link rel="copyright" href="<?php echo htmlspecialchars($this->href('', $this->config['policy_page']), ENT_COMPAT | ENT_HTML401, HTML_ENTITIES_CHARSET); ?>" />
+		<link rel="prev" href="<?php echo $this->href() ?>" />
+	</head>
+	<body id="print">
+		<header>
+			<div class="container">
+				<?php // Print wackoname and wackopath ?>
+				<h1><?php echo $this->config['site_name']; ?>: <?php echo (isset($this->page['title']) ? $this->page['title'] : $this->get_page_path()); ?></h1>
+				<a href="<?php echo $this->config['base_url'] ?>"><?php echo rtrim($this->config['base_url'], "/"); ?></a><?php if ($this->page) { ?>&nbsp;&nbsp;&nbsp;&nbsp;<?php echo $this->get_translation('Version').": ".$this->get_time_formatted($this->page['modified']); } ?><br />
+				<?php echo $this->get_page_path() ?><br />
+			</div>
+		</header>
 <!-- End of header //-->
