060965ccb6aaad91786f3caca34a63ab844ed13b
vendeeglobe <54716082+vendeeglobe@users.noreply.github.com>
2016-03-04 15:19:57 +0100

    added message sets for registration approval - part 1

--- wacko/lang/wacko.el.php
+++ wacko/lang/wacko.el.php
@@ -40,6 +40,16 @@ $wacko_translation = array(
 	'NewAccountIP'				=> 'Remote IP address:',
 	'NewAccountSignupInfo'		=> 'The following account has been created:',
 
+	'UserWaitingApproval'		=> 'An email has been sent to the site administrator. The administrator will review the information that has been submitted and either approve or deny your request.',
+	'UserRequiresApproval'		=> 'To approve or deny this user access to %1 go to',
+	'UserApprovalInfo'			=> 'After you register, your request will be sent to the site administrator for approval. You will then receive an email with further instructions.',
+
+	'UserApprovalPending'		=> 'Your account is still pending approval.',
+	'UserApprovalDenied'		=> 'Your account has been denied access to this site.',
+
+	'UserApprovedInfo'			=> 'You have been approved to access %1',
+	'UserDeniedInfo'			=> 'You have been denied access to %1.',
+
 	// login panel
 	'LoginWelcome' => 'Σύνδεση',
 	'LoginWelcome2' => '((/Registration Εγγραφή))',
