bdfd2d789083be6439caf92b406d4ecea8cf3fb9
vendeeglobe <vendeeglobe@localhost>
2016-03-08 11:58:28 +0100

    localized backup and restore module - part 1

--- wacko/admin/lang/ap.en.php
+++ wacko/admin/lang/ap.en.php
@@ -39,15 +39,57 @@ $ap_translation = array(
 	'LogLevel6'					=> 'lowest',
 	'LogLevel7'					=> 'debugging',
 
+	// Users module
+	'UsersAdded'				=> 'User added',
+	'UsersDeleteInfo'			=> '[User delete Info here..]',
+
+
 	// Massemail
 	'SendToGroup'				=> 'Send to group',
 
-	// db restore module
+	// DB Restore module
 	'ConfirmDbRestore'			=> 'Do you want to restore backup',
 	'ConfirmDbRestoreInfo'		=> 'Please wait this can take some minutes.',
+	'RestoreWrongVersion'		=> 'Wrong WackoWiki version!',
 	'BackupDelete'				=> 'Are you sure you want to remove backup',
 	'BackupDeleteInfo'			=> '',
+	'RestoreOptions'			=> 'Additional restore options',
+	'RestoreOptionsInfo'		=> '* Before restoring the <span class="underline">cluster backup</span>, '.
+									'the target tables are not destroyed (to prevent loss of information from the clusters that have not been backed up). '.
+									'Thus, during the recovery process duplicate records will occur. '.
+									'In normal mode, all of them will be replaced by the records form backup (using SQL-instruction <code>REPLACE</code>), '.
+									'but if this checkbox is checked, all duplicates are skipped (the current values of records will be kept), '.
+									'and only the records with new keys are added to the table (SQL-instruction <code>INSERT IGNORE</code>).<br />'.
+									'<span class="underline">Notice</span>: When restore complete backup of the site, this option has no value.<br />'.
+									'<br />'.
+									'** If the backup contains the user files (global and perpage, cache files, etc.), '.
+									'in normal mode they replace the existing files with the same names and are placed in the same directory when being restored. '.
+									'This option allows you to save the current copies of the files and restore from a backup only new files (missing on the server).',
+	'IgnoreDuplicatedKeys'		=> 'Ignore duplicated table keys (not replace)',
+	'IgnoreSameFiles'			=> 'Ignore the same files (not overwrite)',
+	'NoBackupsAvailable'		=> 	'No backups available.',
+	'BackupEntireSite'			=> 'Entire site',
+	'BackupRestored'			=> 'The backup is restored, a summary report is attached below. To delete this backup package, click',
+	'BackupRemoved'				=> 'The selected backup has been successfully removed.',
 
+
+	// DB Backup module
+	'BackupStructure'			=> 'Structure',
+	'BackupData'				=> 'Data',
+	'BackupFolder'				=> 'Folder',
+	'BackupTable'				=> 'Table',
+	'BackupCluster'				=> 'Cluster',
+	'BackupFiles'				=> 'Files',
+	'BackupSettings'			=> 'Specify the desired scheme of Backup.<br />'.
+									'The root cluster does not affect the global files backup and cache files backup (being chosen they are always saved in full).<br />'.
+									'<br />'.
+									'<span class="underline">Attention</span>: To avoid loss of information from the database when specifying the root cluster the tables from this backup will not be restructured, '.
+									'same when backing up only table structure without saving the data. '.
+									'To make a complete conversion of the tables to the backup format you must make the <em> full database backup (structure and contents) without specifying the cluster</em>.',
+	'BackupCompleted'			=> 'Backing up and archiving completed.<br />'.
+									'Backup package files stored in the "(date)YYYYMMDD_(time)HHMMSS" named sub-directory of <code>files/backup</code> directory.<br />'.
+									'To download it use FTP (maintain the directory structure and file names when copying).<br />'.
+									'To restore a backup copy or remove a package, go to <a href="?mode=db_restore">Restore database</a>.',
 );
 
 ?>
