62798115fd955d343289e4e3d7654af7d8c188f3
vendeeglobe <vendeeglobe@localhost>
2016-04-17 22:22:18 +0200

    minor fixes

--- wacko/formatter/highlight/wrapper_box.php
+++ wacko/formatter/highlight/wrapper_box.php
@@ -1,16 +1,20 @@
 <?php
 
-if (!isset($options['wrapper_align'])) $options['wrapper_align'] = "right";
-if (!isset($options['wrapper_width'])) $options['wrapper_width'] = "250";
+if (!isset($options['wrapper_align'])) $options['wrapper_align'] = 'right';
+if (!isset($options['wrapper_width'])) $options['wrapper_width'] = 250;
 
 if ($options['wrapper_align'] == 'center')
-	$align_style = "margin:0 auto;";
+{
+	$align_style = 'margin: 0 auto;';
+}
 else
-	$align_style = "float:".$options['wrapper_align'].";";
+{
+	$align_style = 'float: '.$options['wrapper_align'];
+}
 
-echo '<div class="action" style="'.$align_style.'; width:'.$options['wrapper_width'].'px">';
+echo '<aside class="action" style="'.$align_style.'; width: '.$options['wrapper_width'].'px">';
 echo '<div class="action-content">';
 echo $text;
-echo '</div></div>';
+echo '</div></aside>';
 
 ?>
