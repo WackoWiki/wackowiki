0527304c011894932dfbf495abfb7a5b85532185
vendeeglobe <vendeeglobe@localhost>
2016-04-23 12:40:41 +0200

    minor fixes

--- wacko/action/mychanges.php
+++ wacko/action/mychanges.php
@@ -18,30 +18,91 @@ if ($user_id = $this->get_user_id())
 	$limit		= $this->get_list_count($max);
 	$prefix		= $this->config['table_prefix'];
 
-	if(isset($_GET['bydate']) && $_GET['bydate'] == 1)
+	if(isset($_GET['byname']) && $_GET['byname'] == 1)
+	{
+		echo $this->get_translation('MyChangesTitle2').
+		" [<a href=\"".$this->href('', '', 'mode=mychanges&amp;bydate=1')."#list\">".
+		$this->get_translation('OrderChange')."</a>].</strong><br /><br />\n";
+
+		$count	= $this->load_single(
+				"SELECT COUNT(tag) AS n ".
+				"FROM {$prefix}page ".
+				"WHERE user_id = '".(int)$user_id."' ".
+				"AND deleted <> '1' ".
+				"AND comment_on_id = '0'", true);
+
+		$pagination = $this->pagination($count['n'], $limit, 'p', 'mode=mychanges&amp;byname=1#list');
+
+		if ($pages = $this->load_all(
+				"SELECT tag, title, modified ".
+				"FROM {$prefix}page ".
+				"WHERE user_id = '".(int)$user_id."' ".
+				"AND deleted <> '1' ".
+				"AND comment_on_id = '0' ".
+				"ORDER BY tag ASC, modified DESC ".
+				"LIMIT {$pagination['offset']}, $limit", true))
+		{
+			echo '<ul class="ul_list">'."\n";
+
+			foreach ($pages as $page)
+			{
+				$first_char = strtoupper($page['tag'][0]);
+
+				if (!preg_match('/'.$this->language['ALPHA'].'/', $first_char))
+				{
+					$first_char = '#';
+				}
+
+				if ($first_char != $cur_char)
+				{
+					if ($cur_char)
+					{
+						echo "</ul>\n<br /></li>\n";
+					}
+
+					echo "<li><strong>$first_char</strong><ul>\n";
+					$cur_char = $first_char;
+				}
+
+				// print entry
+				echo '<li>'.$this->compose_link_to_page($page['tag'], 'revisions', $this->get_time_formatted($page['modified']), 0, $this->get_translation('RevisionTip')).' &mdash; '.$this->compose_link_to_page($page['tag'], '', '', 0)."</li>\n";
+			}
+
+			echo "</ul>\n</li>\n</ul>\n";
+
+			// pagination
+			$show_pagination = $this->show_pagination(isset($pagination['text']) ? $pagination['text'] : '');
+			echo $show_pagination;
+		}
+		else
+		{
+			echo $this->get_translation('DidntEditAnyPage');
+		}
+	}
+	else
 	{
 		echo '<ul class="menu">'."\n".
 				'<li class="active">'.$this->get_translation('MyChangesTitle1')."</li>\n".
-				'<li>'." [<a href=\"".$this->href('', '', 'mode=mychanges', '', 'list')."\">".$this->get_translation('OrderABC')."</a>]"."</li>\n".
-			"</ul>\n";
+				'<li>'." [<a href=\"".$this->href('', '', 'mode=mychanges&amp;byname=1', '', 'list')."\">".$this->get_translation('OrderABC')."</a>]"."</li>\n".
+				"</ul>\n";
 
 		$count	= $this->load_single(
-			"SELECT COUNT(tag) AS n ".
-			"FROM {$prefix}page ".
-			"WHERE user_id = '".(int)$user_id."' ".
+				"SELECT COUNT(tag) AS n ".
+				"FROM {$prefix}page ".
+				"WHERE user_id = '".(int)$user_id."' ".
 				"AND deleted <> '1' ".
 				"AND comment_on_id = '0'", true);
 
 		$pagination = $this->pagination($count['n'], $limit, 'p', 'mode=mychanges&amp;bydate=1#list');
 
 		if ($pages = $this->load_all(
-			"SELECT tag, title, modified, edit_note ".
-			"FROM {$prefix}page ".
-			"WHERE user_id = '".(int)$user_id."' ".
+				"SELECT tag, title, modified, edit_note ".
+				"FROM {$prefix}page ".
+				"WHERE user_id = '".(int)$user_id."' ".
 				"AND deleted <> '1' ".
 				"AND comment_on_id = '0' ".
-			"ORDER BY modified DESC, tag ASC ".
-			"LIMIT {$pagination['offset']}, $limit", true))
+				"ORDER BY modified DESC, tag ASC ".
+				"LIMIT {$pagination['offset']}, $limit", true))
 		{
 			echo '<ul class="ul_list">'."\n";
 
@@ -85,67 +146,6 @@ if ($user_id = $this->get_user_id())
 			$show_pagination = $this->show_pagination(isset($pagination['text']) ? $pagination['text'] : '');
 
 			// pagination
-			echo $show_pagination;
-		}
-		else
-		{
-			echo $this->get_translation('DidntEditAnyPage');
-		}
-	}
-	else
-	{
-		echo $this->get_translation('MyChangesTitle2').
-			" [<a href=\"".$this->href('', '', 'mode=mychanges&amp;bydate=1')."#list\">".
-			$this->get_translation('OrderChange')."</a>].</strong><br /><br />\n";
-
-		$count	= $this->load_single(
-			"SELECT COUNT(tag) AS n ".
-			"FROM {$prefix}page ".
-			"WHERE user_id = '".(int)$user_id."' ".
-				"AND deleted <> '1' ".
-				"AND comment_on_id = '0'", true);
-
-		$pagination = $this->pagination($count['n'], $limit, 'p', 'mode=mychanges#list');
-
-		if ($pages = $this->load_all(
-			"SELECT tag, title, modified ".
-			"FROM {$prefix}page ".
-			"WHERE user_id = '".(int)$user_id."' ".
-				"AND deleted <> '1' ".
-				"AND comment_on_id = '0' ".
-			"ORDER BY tag ASC, modified DESC ".
-			"LIMIT {$pagination['offset']}, $limit", true))
-		{
-			echo '<ul class="ul_list">'."\n";
-
-			foreach ($pages as $page)
-			{
-				$first_char = strtoupper($page['tag'][0]);
-
-				if (!preg_match('/'.$this->language['ALPHA'].'/', $first_char))
-				{
-					$first_char = '#';
-				}
-
-				if ($first_char != $cur_char)
-				{
-					if ($cur_char)
-					{
-						echo "</ul>\n<br /></li>\n";
-					}
-
-					echo "<li><strong>$first_char</strong><ul>\n";
-					$cur_char = $first_char;
-				}
-
-				// print entry
-				echo '<li>'.$this->compose_link_to_page($page['tag'], 'revisions', $this->get_time_formatted($page['modified']), 0, $this->get_translation('RevisionTip')).' &mdash; '.$this->compose_link_to_page($page['tag'], '', '', 0)."</li>\n";
-			}
-
-			echo "</ul>\n</li>\n</ul>\n";
-
-			// pagination
-			$show_pagination = $this->show_pagination(isset($pagination['text']) ? $pagination['text'] : '');
 			echo $show_pagination;
 		}
 		else
--- wacko/action/userpages.php
+++ wacko/action/userpages.php
@@ -7,8 +7,8 @@ if (!defined('IN_WACKO'))
 
 if ($this->get_user_name())
 {
-	$output1 = '<a id="list"></a><h3>'.$this->get_translation('ListMyWatches')."</h3>".
-				'<ul class="menu">'."\n";
+	$output1 = #'<h3>'.$this->get_translation('UserPages')."</h3>".
+				'<ul class="menu" id="list">'."\n";
 	$output2 = '<li><a href="'.$this->href('', '', 'mode=mypages').'#list">'.$this->get_translation('ListMyPages')."</a></li>\n";
 	$output3 = '<li><a href="'.$this->href('', '', 'mode=mychanges').'#list">'.$this->get_translation('ListMyChanges')."</a></li>\n";
 	$output4 = '<li><a href="'.$this->href('', '', 'mode=mywatches').'#list">'.$this->get_translation('ListMyWatches')."</a></li>";
@@ -23,41 +23,41 @@ if ($this->get_user_name())
 				$output4 .
 				$output5 .
 				$output6;
-		echo '<a id="list"></a><h4>'.$this->get_translation('ListMyPages')."</h3>";
-		echo $this->action('mypages');
+		echo	'<h3>'.$this->get_translation('ListMyPages')."</h3>";
+		echo	$this->action('mypages');
 	}
 	else if (isset($_GET['mode']) && $_GET['mode'] == 'mywatches')
 	{
 		echo	$output1 .
-		$output2 .
-		$output3 .
-		'<li class="active">'.$this->get_translation('ListMyWatches')."</a></li>\n". #$output4 .
-		$output5 .
-		$output6;
-		echo '<a id="list"></a><h4>'.$this->get_translation('ListMyWatches')."</h3>";
-		echo $this->action('mywatches');
+				$output2 .
+				$output3 .
+				'<li class="active">'.$this->get_translation('ListMyWatches')."</a></li>\n". #$output4 .
+				$output5 .
+				$output6;
+		echo	'<h3>'.$this->get_translation('ListMyWatches')."</h3>";
+		echo	$this->action('mywatches');
 	}
 	else if (!isset($_GET['mode']) || $_GET['mode'] == 'mychangeswatches')
 	{
 		echo	$output1 .
-		$output2 .
-		$output3 .
-		$output4 .
-		'<li class="active">'.$this->get_translation('ListMyChangesWatches')."</a></li>\n". #$output5 .
-		$output6;
-		echo '<a id="list"></a><h4>'.$this->get_translation('ListMyChangesWatches')."</h3>";
-		echo $this->action('mychangeswatches');
+				$output2 .
+				$output3 .
+				$output4 .
+				'<li class="active">'.$this->get_translation('ListMyChangesWatches')."</a></li>\n". #$output5 .
+				$output6;
+		echo	'<h3>'.$this->get_translation('ListMyChangesWatches')."</h3>";
+		echo	$this->action('mychangeswatches');
 	}
 	else if (isset($_GET['mode']) && $_GET['mode'] == 'mychanges')
 	{
 		echo	$output1 .
-		$output2 .
-		'<li class="active">'.$this->get_translation('ListMyChanges')."</a></li>\n". #$output3 .
-		$output4 .
-		$output5 .
-		$output6;
-		echo '<a id="list"></a><h4>'.$this->get_translation('ListMyChanges')."</h3>";
-		echo $this->action('mychanges');
+				$output2 .
+				'<li class="active">'.$this->get_translation('ListMyChanges')."</a></li>\n". #$output3 .
+				$output4 .
+				$output5 .
+				$output6;
+		echo	'<h3>'.$this->get_translation('ListMyChanges')."</h3>";
+		echo	$this->action('mychanges');
 	}
 }
 else
