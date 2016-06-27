051d1d9105b0d5b752714e116121c5aacdd2c21d
vendeeglobe <54716082+vendeeglobe@users.noreply.github.com>
2016-06-27 19:36:48 +0200

    changed 'title' field to VARCHAR(250)

--- wacko/setup/_insert_queries.php
+++ wacko/setup/_insert_queries.php
@@ -138,13 +138,16 @@ $upgrade['5.4.0'][]		= array($lang['UpdateTable'],	'page',			$update_page_r5_4_0
 $upgrade['5.4.0'][]		= array($lang['UpdateTable'],	'page',			$update_page_r5_4_1,		$lang['ErrorUpdatingTable']);
 $upgrade['5.4.0'][]		= array($lang['UpdateTable'],	'page',			$update_page_r5_4_2,		$lang['ErrorUpdatingTable']);
 
+$upgrade['5.5.rc'][]	= array($lang['AlterTable'],	'page',			$alter_page_r5_4_1,			$lang['ErrorAlteringTable']);
+
 // referrer
 $upgrade['5.4.0'][]		= array($lang['AlterTable'],	'referrer',		$alter_referrer_r5_4_0,		$lang['ErrorAlteringTable']);
-
 
 // revision
 $upgrade['5.4.0'][]		= array($lang['AlterTable'],	'revision',		$alter_revision_r5_4_0,		$lang['ErrorAlteringTable']);
 $upgrade['5.4.0'][]		= array($lang['UpdateTable'],	'revision',		$update_revision_r5_4_0,	$lang['ErrorUpdatingTable']);
+
+$upgrade['5.5.rc'][]	= array($lang['AlterTable'],	'revision',		$alter_revision_r5_4_1,		$lang['ErrorAlteringTable']);
 
 // tag
 $upgrade['5.4.0'][]		= array($lang['AlterTable'],	'tag',			$alter_tag_r5_4_0,			$lang['ErrorAlteringTable']);
