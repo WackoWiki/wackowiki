e1e4bde22fceb433e7cae9c9a0d9a5ba0d2c4466
serge terekhov <sts@carve.ru>
2016-07-07 18:56:53 +0600

    small multilang fixes

--- wacko/formatter/source.php
+++ wacko/formatter/source.php
@@ -5,62 +5,13 @@ if (!defined('IN_WACKO'))
 	exit;
 }
 
-$output = '';
+extract(array_merge(['default' => 'wacko', 'source' => $this->tag, 'copy_button' => false], $options));
 
-$options['default']		= '';
-$options['source']		= $this->tag;
-$options['copy_button']	= false;
-$options['no<p>']		= true;
-
-if (empty($options['default'])) $options['default'] = 'wacko';
-
-if ($options['default'] == 'wacko')
-{
-	// strip comments
-	$text = preg_replace( '/(\n?)%%\((comment)\).*?%%([\n\r]*)/ims', '', $text );
-
-	// insert about the source
-	$text = $text.= "\n\n----\n".$this->get_translation('SourceFrom').'((/'.$options['source'].'))';
-
-	// prepare a text to the conclusion
-	$output = htmlspecialchars($text, ENT_COMPAT | ENT_HTML401, HTML_ENTITIES_CHARSET);
-	$output = preg_replace_callback('/^ +/m',
-		function ($matches)
-		{
-			$m = strlen($matches[0]);
-			return str_repeat('&nbsp; ', $m >> 1) . (($m & 1)? '&nbsp;' : '');
-		},
-		$output);
-
-	$output = $this->format($output, 'simplebr', null, 0, array('no<p>' => 1) );
-}
-else
-{
-	if ($options['default'] == 'rawhtml')
-	{
-		// insert about the source
-		$text = $text.="\n\n<br /><br /><hr />\n\n<p>".
-			$this->get_translation('SourceFrom').
-				$this->link($options['source'], '', $this->get_translation('SourceFromLink')).
-				"</p>";
-
-		$output = htmlspecialchars($text, ENT_COMPAT | ENT_HTML401, HTML_ENTITIES_CHARSET);
-		$output = str_replace("\n", '<br />', $output);
-	}
-	else if ($options['default'] == 'simplebr')
-	{
-		// insert about the source
-		$text = $text.="\n\n<hr />\n".$this->get_translation('SourceFrom').$this->href($options['source']);
-
-		$output = htmlspecialchars($text, ENT_COMPAT | ENT_HTML401, HTML_ENTITIES_CHARSET);
-	}
-}
-
-$div_id = "document_source_".hash('sha1', $options['source']);
+$div_id = "document_source_" . hash('sha1', $source);
 echo '<!--notypo-->';
 
 // copy to clipboard is implemented only for MSIE for now
-if ($options['copy_button'])
+if ($copy_button)
 {
 	echo '<button id="button_'.$div_id.'" style="margin:5px" onclick="
 		ta  = document.getElementById(\'textarea_'.$div_id.'\');
@@ -74,8 +25,48 @@ if ($options['copy_button'])
 	echo '<textarea style="display:none" id="textarea_'.$div_id.'" ></textarea>';
 }
 
-echo '<pre id="'.$div_id.'" class="code" style="padding:5px">';
-echo $output;
-echo '</pre>';
+echo '<pre id="' . $div_id . '" class="code" style="padding:5px">';
 
+switch ($default)
+{
+	case 'wacko':
+		// strip comments
+		$text = preg_replace('/(\n?)%%\((comment)\).*?%%([\n\r]*)/ims', '', $text);
+
+		$text = Ut::html($text);
+
+		// insert about the source
+		$text .= "\n\n----\n" . $this->get_translation('SourceFrom') . '((/' . $source . '))';
+
+		// prepare a text to the conclusion
+		$text = preg_replace_callback('/^ +/m',
+			function ($matches)
+			{
+				$m = strlen($matches[0]);
+				return str_repeat('&nbsp; ', $m >> 1) . (($m & 1)? '&nbsp;' : '');
+			},
+			$text);
+
+		echo $this->format($text, 'simplebr', ['no<p>' => 1]);
+		break;
+
+	case 'rawhtml':
+		echo str_replace("\n", '<br />', Ut::html($text));
+
+		// about the source
+		echo "\n\n<br /><br /><hr />\n\n<p>".
+			$this->get_translation('SourceFrom').
+				$this->link($source, '', $this->get_translation('SourceFromLink')).
+				"</p>";
+		break;
+
+	case 'simplebr':
+		echo Ut::html($text);
+
+		// about the source
+		echo "\n\n<hr />\n" . $this->get_translation('SourceFrom') . $this->href($source);
+		break;
+}
+
+echo '</pre>';
 echo '<!--/notypo-->';
