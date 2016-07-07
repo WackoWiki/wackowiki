4ab772d807ee3312c90ca0f7b0bb54dce9a96097
serge terekhov <sts@carve.ru>
2016-07-07 23:11:21 +0600

    hacking on source formatter

--- wacko/formatter/source.php
+++ wacko/formatter/source.php
@@ -7,22 +7,17 @@ if (!defined('IN_WACKO'))
 
 extract(array_merge(['default' => 'wacko', 'source' => $this->tag, 'copy_button' => false], $options));
 
-$div_id = "document_source_" . hash('sha1', $source);
+$div_id = Ut::random_token(13, 2);
 echo '<!--notypo-->';
 
 // copy to clipboard is implemented only for MSIE for now
 if ($copy_button)
 {
-	echo '<button id="button_'.$div_id.'" style="margin:5px" onclick="
-		ta  = document.getElementById(\'textarea_'.$div_id.'\');
-		div = document.getElementById(\''.$div_id.'\');
-		ta.value = div.innerText;
-		range = ta.createTextRange();
-		range.execCommand(\'Copy\');
-		this.style.backgroundColor=\'#ffffdd\';
-		setTimeout(\'document.getElementById(\''+this.id+'\').style.backgroundColor = \'#dddddd\';\', 100);
-		">'. $this->get_translation('SourceCopyToClipboard').'</button>';
-	echo '<textarea style="display:none" id="textarea_'.$div_id.'" ></textarea>';
+	$this->add_html_head('<script type="text/javascript" src="' . $this->config['base_url'] . 'js/clipboard.min.js" async onload="new Clipboard(\'.clipb\')"></script>' . "\n");
+
+	echo '<button class="clipb" style="margin:5px" data-clipboard-target="#' . $div_id . '">'.
+		$this->get_translation('SourceCopyToClipboard').
+		'</button>';
 }
 
 echo '<pre id="' . $div_id . '" class="code" style="padding:5px">';
@@ -33,7 +28,7 @@ switch ($default)
 		// strip comments
 		$text = preg_replace('/(\n?)%%\((comment)\).*?%%([\n\r]*)/ims', '', $text);
 
-		$text = Ut::html($text);
+		$text = Ut::html($text, false);
 
 		// insert about the source
 		$text .= "\n\n----\n" . $this->get_translation('SourceFrom') . '((/' . $source . '))';
