c589daad62b2372ca5625cc47b75586a240f3eec
vendeeglobe <54716082+vendeeglobe@users.noreply.github.com>
2017-10-30 10:31:33 +0100

    only code cleanup

--- wacko/formatter/highlight/code.php
+++ wacko/formatter/highlight/code.php
@@ -1,19 +1,9 @@
 <?php
-$num = substr_count($text, "\n") + 2;
 
-if ($this->method != 'print' && $num >= 20)
-{
-	$num = 20;
-}
+echo	'<!--notypo-->' .
+			'<pre class="code">' .
+				htmlspecialchars($text, ENT_COMPAT | ENT_HTML5, HTML_ENTITIES_CHARSET) .
+			'</pre>' .
+		'<!--/notypo-->';
 
-if ($this->method != 'print')
-{
-	$wrap = 'off';
-}
-else
-{
-	$wrap = 'on';
-}
-
-echo '<!--notypo--><pre class="code">' . htmlspecialchars($text, ENT_COMPAT | ENT_HTML5, HTML_ENTITIES_CHARSET) . '</pre><!--/notypo-->';
 ?>
