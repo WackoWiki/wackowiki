c4f045f19917fefad406473557bad7d9606b2b68
vendeeglobe <54716082+vendeeglobe@users.noreply.github.com>
2017-11-29 21:33:29 +0100

    minor fixes

--- wacko/action/orphaned.php
+++ wacko/action/orphaned.php
@@ -7,12 +7,10 @@ if (!defined('IN_WACKO'))
 
 $load_orphaned_pages = function ($tag, $limit, $deleted = 0)
 {
-	$pagination	= '';
+	$pagination	= [];
 	$pref		= $this->db->table_prefix;
 
-	// count pages
-	if ($count_pages = $this->db->load_all(
-		"SELECT DISTINCT page_id " .
+	$selector =
 		"FROM " . $pref . "page p " .
 			"LEFT JOIN " . $pref . "page_link l ON " .
 			"((l.to_tag = p.tag " .
@@ -26,36 +24,30 @@ $load_orphaned_pages = function ($tag, $limit, $deleted = 0)
 			($deleted != 1
 				? "AND p.deleted <> 1 "
 				: "") .
-			"AND p.comment_on_id = 0 "
+			"AND p.comment_on_id = 0 ";
+
+	// count pages
+	if ($count = $this->db->load_single(
+		"SELECT DISTINCT COUNT(page_id) AS n " .
+		$selector
 		, true));
 
-	if ($count_pages)
+	if ($count)
 	{
-		$count		= count($count_pages);
-		$pagination = $this->pagination($count, $limit);
+		$pagination = $this->pagination($count['n'], $limit);
 
 		$orphaned = $this->db->load_all(
 			"SELECT DISTINCT page_id, tag, title " .
-			"FROM " . $pref . "page p " .
-				"LEFT JOIN " . $pref . "page_link l ON " .
-				"((l.to_tag = p.tag " .
-					"AND l.to_supertag = '') " .
-					"OR l.to_supertag = p.supertag) " .
-			"WHERE " .
-				($tag
-					? "p.tag LIKE " . $this->db->q($tag . '/%') . " AND "
-					: "") .
-				"l.to_page_id IS NULL " .
-				($deleted != 1
-					? "AND p.deleted <> 1 "
-					: "") .
-				"AND p.comment_on_id = 0 " .
+			$selector .
 			"ORDER BY tag " .
 			$pagination['limit']);
 
 		return [$orphaned, $pagination];
 	}
 };
+
+if (!isset($root))		$root	= ''; // depreciated
+if ($root)				$page	= $root;
 
 if (!isset($root))
 {
@@ -74,22 +66,26 @@ if (list ($pages, $pagination) = $load_orphaned_pages($root, $max))
 {
 	if (is_array($pages))
 	{
+		foreach ($pages as $page)
+		{
+			$page_ids[] = (int) $page['page_id'];
+		}
+
+		// cache acls
+		$this->preload_acl($page_ids);
+
 		$this->print_pagination($pagination);
 
-		echo "<ol>\n";
+		echo '<ol start="' . ($pagination['offset'] + 1) . '">' . "\n";
 
-		//!!!! unoptimized
-		if (is_array($pages))
+		foreach ($pages as $page)
 		{
-			foreach ($pages as $page)
+			if (!$this->db->hide_locked || $this->has_access('read', $page['page_id']))
 			{
-				if (!$this->db->hide_locked || $this->has_access('read', $page['page_id']))
-				{
-					// cache page_id for for has_access validation in link function
-					$this->page_id_cache[$page['tag']] = $page['page_id'];
+				// cache page_id for for has_access validation in link function
+				$this->page_id_cache[$page['tag']] = $page['page_id'];
 
-					echo '<li>' . $this->link('/' . $page['tag'], '', '', '', 0) . "</li>\n";
-				}
+				echo '<li>' . $this->link('/' . $page['tag'], '', '', '', 0) . "</li>\n";
 			}
 		}
 
