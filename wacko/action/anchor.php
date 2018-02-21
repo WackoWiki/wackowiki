58160b813a62d5dd19250b6fb83f750aed7ac341
vendeeglobe <54716082+vendeeglobe@users.noreply.github.com>
2018-02-21 12:14:43 +0100

    added parameter alias 'name' for 'href'
    just legacy support

--- wacko/action/anchor.php
+++ wacko/action/anchor.php
@@ -5,16 +5,19 @@ if (!defined('IN_WACKO'))
 	exit;
 }
 
+if (!isset($name))		$name	= ''; // depreciated
+if ($name)				$href	= $name;
+
 if (!isset($text))	$text = '';
 if (!isset($title))	$title = '';
 
 // Param name
 if (isset($href))
 {
-	$text = str_replace('~', $href, $text);
-	$text = Ut::html($text);
-	$title = Ut::html($title);
-	$href = Ut::html($href);
+	$text	= str_replace('~', $href, $text);
+	$text	= Ut::html($text);
+	$title	= Ut::html($title);
+	$href	= Ut::html($href);
 
 	echo '<a id="' . $href . '" href="#' . $href . '" title="' . $title . '">' . $text . "</a>\n";
 }
