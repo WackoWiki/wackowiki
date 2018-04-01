daa442f6e05508c7ae4d01e0759c5550a31928cd
vendeeglobe <54716082+vendeeglobe@users.noreply.github.com>
2018-04-01 17:13:35 +0200

    update to Hashids 3.0.0

--- wacko/lib/hashids/Math/Bc.php
+++ wacko/lib/hashids/Math/Bc.php
@@ -9,12 +9,16 @@
  * file that was distributed with this source code.
  */
 
+#namespace Hashids\Math;
+
 /**
- * This is the math class.
+ * This is the Bc math class.
  *
  * @author Vincent Klaiber <hello@vinkla.com>
+ * @author Jakub Kramarz <lenwe@lenwe.net>
+ * @author Johnson Page <jwpage@gmail.com>
  */
-class Math
+class Bc implements MathInterface
 {
     /**
      * Add two arbitrary-length integers.
@@ -24,12 +28,8 @@ class Math
      *
      * @return string
      */
-    public static function add($a, $b)
+    public function add($a, $b)
     {
-        if (function_exists('gmp_add')) {
-            return gmp_add($a, $b);
-        }
-
         return bcadd($a, $b, 0);
     }
 
@@ -41,12 +41,8 @@ class Math
      *
      * @return string
      */
-    public static function multiply($a, $b)
+    public function multiply($a, $b)
     {
-        if (function_exists('gmp_mul')) {
-            return gmp_mul($a, $b);
-        }
-
         return bcmul($a, $b, 0);
     }
 
@@ -58,30 +54,9 @@ class Math
      *
      * @return string
      */
-    public static function divide($a, $b)
+    public function divide($a, $b)
     {
-        if (function_exists('gmp_div_q')) {
-            return gmp_div_q($a, $b);
-        }
-
         return bcdiv($a, $b, 0);
-    }
-
-    /**
-     * Raise arbitrary-length integer into power.
-     *
-     * @param string $base
-     * @param string $exp
-     *
-     * @return string
-     */
-    public static function pow($base, $exp)
-    {
-        if (function_exists('gmp_pow')) {
-            return gmp_pow($base, $exp);
-        }
-
-        return bcpow($base, $exp, 0);
     }
 
     /**
@@ -92,30 +67,22 @@ class Math
      *
      * @return string
      */
-    public static function mod($n, $d)
+    public function mod($n, $d)
     {
-        if (function_exists('gmp_mod')) {
-            return gmp_mod($n, $d);
-        }
-
         return bcmod($n, $d);
     }
 
     /**
-     * Compares two arbitrary-length integer modulo.
+     * Compares two arbitrary-length integers.
      *
      * @param string $a
      * @param string $b
      *
-     * @return int
+     * @return bool
      */
-    public static function comp($a, $b)
+    public function greaterThan($a, $b)
     {
-        if (function_exists('gmp_cmp')) {
-            return gmp_cmp($a, $b);
-        }
-
-        return bccomp($a, $b, 0);
+        return bccomp($a, $b, 0) > 0;
     }
 
     /**
@@ -125,12 +92,8 @@ class Math
      *
      * @return int
      */
-    public static function intval($a)
+    public function intval($a)
     {
-        if (function_exists('gmp_intval')) {
-            return gmp_intval($a);
-        }
-
         return intval($a);
     }
 
@@ -141,12 +104,8 @@ class Math
      *
      * @return string
      */
-    public static function strval($a)
+    public function strval($a)
     {
-        if (function_exists('gmp_strval')) {
-            return gmp_strval($a);
-        }
-
         return $a;
     }
 
@@ -157,12 +116,8 @@ class Math
      *
      * @return string
      */
-    public static function get($a)
+    public function get($a)
     {
-        if (function_exists('gmp_init')) {
-            return gmp_init($a);
-        }
-
         return $a;
     }
 }
