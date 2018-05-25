d203997b0c476d758129d7562d78134326be5e7c
vendeeglobe <54716082+vendeeglobe@users.noreply.github.com>
2018-05-25 09:12:59 +0200

    minor fixes

--- wacko/setup/database_mysql_updates.php
+++ wacko/setup/database_mysql_updates.php
@@ -49,10 +49,11 @@ $update_category_assignment_r5_4_0 = "UPDATE {$pref}category_assignment SET obje
 
 // CONFIG
 $update_config_r5_4_0 = "UPDATE {$pref}config SET config_value = 'addcomment|admin\\.php|categories|claim|clone|diff|edit|export\\.xml|file|latex|moderate|new|permissions|purge|print|properties|rate|referrers|referrers_sites|remove|rename|review|revisions|revisions\\.xml|robots\\.txt|sitemap\\.xml|show|source|upload|watch|wordprocessor' WHERE config_name = 'standard_handlers'";
-$update_config_r5_4_1 = "DELETE FROM {$pref}config WHERE config_name IN ('session_expiration', 'x_csp', 'x_frame_option', 'session_encrypt_cookie', 'allow_swfobject', 'revisions_hide_cancel', 'date_precise_format', 'date_macro_format', 'phpmailer', 'npjlinks', 'meta_description', 'meta_keywords')";
+$update_config_r5_4_1 = "DELETE FROM {$pref}config WHERE config_name IN ('session_expiration', 'x_csp', 'x_frame_option', 'session_encrypt_cookie', 'allow_swfobject', 'revisions_hide_cancel', 'date_precise_format', 'date_macro_format', 'phpmailer', 'npjlinks', 'meta_description', 'meta_keywords', 'session_match_ip', 'session_match_useragent')";
 $update_config_r5_4_2 = "UPDATE {$pref}config SET config_value = config_value * 1024 WHERE config_name = 'upload_max_size'";
 $update_config_r5_4_3 = "UPDATE {$pref}config SET config_value = config_value * 1024 WHERE config_name = 'upload_quota'";
 $update_config_r5_4_4 = "UPDATE {$pref}config SET config_value = config_value * 1024 WHERE config_name = 'upload_quota_per_user'";
+#$update_config_r5_4_5 = "UPDATE {$pref}config SET config_value = config_value 'policy_page' WHERE config_name = 'terms_page'";
 
 // FILE
 $alter_file_r5_1_0 = "ALTER TABLE {$pref}upload ADD INDEX idx_deleted (deleted)";
