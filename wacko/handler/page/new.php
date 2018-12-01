1c0e83881007401cbb95bf9c0215c3be5c5b8941
vendeeglobe <54716082+vendeeglobe@users.noreply.github.com>
2018-12-01 12:12:41 +0100

    minor fixes

--- wacko/handler/page/new.php
+++ wacko/handler/page/new.php
@@ -23,7 +23,7 @@ if (isset($_POST['tag']) && $new_tag = trim($_POST['tag'], '/ '))
 			$prefix = '';
 	}
 
-	// check if reserved word
+	// check reserved word
 	if ($result = $this->validate_reserved_words($new_tag))
 	{
 		$this->set_message(Ut::perc_replace($this->_t('PageReservedWord'), $result));
@@ -36,7 +36,7 @@ if (isset($_POST['tag']) && $new_tag = trim($_POST['tag'], '/ '))
 		// check existing page write access
 		if ($this->has_access('write', $this->get_page_id($prefix . $new_tag)))
 		{
-			$message .= Ut::perc_replace($this->_t('PageAlreadyExistsEdit'), "<a href=\"" . $this->href('edit', $prefix . $new_tag) . "\">" . $this->_t('PageAlreadyExistsEdit2') . " </a>");
+			$message .= Ut::perc_replace($this->_t('PageAlreadyExistsEdit'), '<a href="' . $this->href('edit', $prefix . $new_tag) . '">' . $this->_t('PageAlreadyExistsEdit2') . '</a>');
 		}
 		else
 		{
@@ -66,13 +66,13 @@ if (isset($_POST['tag']) && $new_tag = trim($_POST['tag'], '/ '))
 // create a peer page
 if ($this->has_access('create', $this->get_page_id($this->tag)))
 {
-	$tpl->p_base	= (strlen($this->tag) > 50 ? '...' . substr($this->tag, -50) : $this->tag);
-	$tpl->p_tag		= (isset($_POST['option']) && $_POST['option'] === 1 ? Ut::html($new_tag) : '');
+	$tpl->p_f_base	= (strlen($this->tag) > 50 ? '...' . substr($this->tag, -50) : $this->tag);
+	$tpl->p_f_tag	= (isset($_POST['option']) && $_POST['option'] === 1 ? Ut::html($new_tag) : '');
 }
 else
 {
 	$message = '<em>' . $this->_t('CreatePageDenied') . '</em>';
-	$tpl->p_message	= $this->show_message($message, 'info', false);
+	$tpl->p_d_message	= $this->show_message($message, 'info', false);
 }
 
 // create a child page. only inside a cluster
@@ -85,14 +85,14 @@ if (substr_count($this->tag, '/') > 0)
 		// hide users cluster
 		if ($parent != $this->db->users_page)
 		{
-			$tpl->c_base	= (strlen($parent) > 50 ? '...' . substr($parent, -50) : $parent);
-			$tpl->c_tag		= (isset($_POST['option']) && $_POST['option'] === 2 ? Ut::html($new_tag) : '');
+			$tpl->c_f_base	= (strlen($parent) > 50 ? '...' . substr($parent, -50) : $parent);
+			$tpl->c_f_tag	= (isset($_POST['option']) && $_POST['option'] === 2 ? Ut::html($new_tag) : '');
 		}
 	}
 	else
 	{
 		$message = '<em>' . $this->_t('CreatePageDenied') . '</em>';
-		$tpl->c_message	= $this->show_message($message, 'info', false);
+		$tpl->c_d_message	= $this->show_message($message, 'info', false);
 	}
 }
 
