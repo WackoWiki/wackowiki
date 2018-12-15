d71026f9dc66498504e59e38d80df2fed3fe08e4
vendeeglobe <vendeeglobe@localhost>
2018-12-16 00:13:36 +0100

    added missing inserts and message sets for Hungarian

---
+++ wacko/setup/lang/inserts.hu.php
@@ -0,0 +1,43 @@
+<?php
+
+$page_lang = 'hu';
+
+// insert these pages only for default language
+if ($config['language'] == $page_lang)
+{
+	if ($config['is_update'] == false)
+	{
+		$home_page_body		= "file:/wacko_logo.png\n**Welcome to your ((WackoWiki:Doc/English WackoWiki)) site!**\n\nClick after you have ((login logged in)) on the \"Edit this page\" link at the bottom to get started.\n\nDocumentation can be found at WackoWiki:Doc/English.\n\nUseful pages: ((WackoWiki:Doc/English/Formatting Formatting)), ((Search)).\n\n";
+		$admin_page_body	= sprintf($config['name_date_macro'], '((user:' . $config['admin_name'] . ' ' . $config['admin_name'] . '))', date($config['date_format'] . ' ' . $config['time_format']));
+
+		insert_page($config['root_page'], 'Kezdõlap', $home_page_body, $page_lang, 'Admins', true, false, null, 0);
+		insert_page($config['users_page'] . '/' . $config['admin_name'], $config['admin_name'], $admin_page_body."\n\n", $page_lang, $config['admin_name'], true, false, null, 0);
+	}
+	else
+	{
+		// ...
+	}
+
+	insert_page($config['category_page'],	'Kategória',		'{{category}}',			$page_lang, 'Admins', false, false);
+	insert_page($config['groups_page'],		'Csoportok',		'{{groups}}',			$page_lang, 'Admins', false, false);
+	insert_page($config['users_page'],		'Felhasználók',		'{{users}}',			$page_lang, 'Admins', false, false);
+
+	insert_page($config['help_page'],		'Segítség',			'',			$page_lang, 'Admins', false, false);
+	insert_page($config['terms_page'],		'Terms',		'',			$page_lang, 'Admins', false, false);
+	insert_page($config['privacy_page'],	'Privacy',		'',			$page_lang, 'Admins', false, false);
+
+	#insert_page('LapTalálomra',			'Lap találomra',	'{{randompage}}',		$page_lang, 'Admins', false, true, 'Random');
+}
+
+//
+insert_page('FrissVáltoztatások',	'Friss változtatások',	'{{changes}}',			$page_lang, 'Admins', false, true, 'Changes');
+insert_page('RecentlyCommented',	'Recently Commented',	'{{commented}}',		$page_lang, 'Admins', false, true, 'Comments');
+insert_page('PageIndex',			'Page Index',			'{{pageindex}}',		$page_lang, 'Admins', false, true, 'Index');
+
+insert_page('Regisztráció',			'Fiók létrehozása',		'{{registration}}',		$page_lang, 'Admins', false, false);
+insert_page('Jelszó',				'Jelszó',				'{{changepassword}}',	$page_lang, 'Admins', false, false);
+insert_page('Keresés',				'Keresés',				'{{search}}',			$page_lang, 'Admins', false, false);
+insert_page('Bejelentkezés',		'Bejelentkezés',		'{{login}}',			$page_lang, 'Admins', false, false);
+insert_page('Beállítások',			'Beállítások',			'{{usersettings}}',		$page_lang, 'Admins', false, false);
+
+?>
---
+++ wacko/setup/lang/installer.hu.php
@@ -0,0 +1,219 @@
+<?php
+$lang = [
+
+/*
+   Language Settings
+*/
+'Charset' => 'iso-8859-2',
+'LangISO' => 'hu',
+'LangName' => 'Magyar',
+
+/*
+   Config Defaults
+*/
+'ConfigDefaults'	=> [
+	// site name
+	'site_name'			=> 'MyWikiSite',
+
+	// pages
+	'category_page'		=> 'Kategória',
+	'groups_page'		=> 'Csoportok',
+	'users_page'		=> 'Felhasználók',
+
+	#'help_page'		=> 'Segítség',
+	#'terms_page'		=> 'Terms',
+	#'privacy_page'		=> 'Privacy',
+
+	// time
+	#'date_format'					=> 'd.m.Y',
+	#'time_format'					=> 'H:i',
+	#'time_format_seconds'			=> 'H:i:s',
+],
+
+/*
+   Generic Page Text
+*/
+'Title' => 'WackoWiki Telepítés',
+'Continue' => 'Continue',
+'Back' => 'Back',
+'Recommended' => 'recommended',
+'InvalidAction' => 'Invalid action',
+
+/*
+   Language Selection Page
+*/
+'lang' => 'Language Configuration',
+'PleaseUpgradeToR5' => 'You aware to be running an old (pre 5.0.0) release of WackoWiki. To update to this release of WackoWiki, you must first update your installation to <code class="version">5.0.x</code>.',
+'UpgradeFromWacko' => 'Welcome to WackoWiki, it appears that you are upgrading from WackoWiki <code class="version">%1</code> to <code class="version">%2</code>.  The next few pages will guide you through the upgrade process.',
+'FreshInstall' => 'Welcome to WackoWiki, you are about to install WackoWiki <code class="version">%1</code>.  The next few pages will guide you through the installation process.',
+'PleaseBackup' => 'Please, <strong>backup</strong> your database, config file and all changed files such as those which have hacks and patches applied to them before starting upgrade process. This can save you from big headache.',
+'LangDesc' => 'Please choose a language for the installation process. This language will also be used as the default language of your WackoWiki installation.',
+
+/*
+   System Requirements Page
+*/
+'version-check' => 'System Requirements',
+'PHPVersion' => 'PHP Version',
+'PHPDetected' => 'Detected PHP',
+'ModRewrite' => 'Apache Rewrite Extension (Optional)',
+'ModRewriteInstalled' => 'Rewrite Extension (mod_rewrite) Installed?',
+'Database' => 'Database',
+'PHPExtensions' => 'PHP Extensions',
+'Permissions' => 'Permissions',
+'ReadyToInstall' => 'Ready to Install?',
+'Requirements' => 'Your server must meet the requirements listed below.',
+'OK' => 'OK',
+'Problem' => 'Problem',
+'NotePHPExtensions' => '',
+'ErrorPHPExtensions' => 'Your PHP installation appears to be missing the noted PHP extensions which are required by WackoWiki.',
+'PCREwithoutUTF8' => 'PCRE is not compiled with UTF-8 support.',
+'NotePermissions' => 'This installer will try to write the configuration data to the file %1, located in your WackoWiki directory. In order for this to work, you must make sure the web server has write access to that file.  If you can\'t do this, you will have to edit the file manually (the installer will tell you how).<br><br>See <a href="https://wackowiki.org/doc/Doc/English/Installation" target="_blank">WackoWiki:Doc/English/Installation</a> for details.',
+'ErrorPermissions' => 'It would appear that the installer cannot automatically set the required file permissions for WackoWiki to work correctly. You will be prompted later in the installation process to manually configure the required file permissions on your server.',
+'ErrorMinPHPVersion' => 'The PHP Version must be greater than <strong>' . PHP_MIN_VERSION . '</strong>, your server appears to be running an earlier version.  You must upgrade to a more recent PHP version for WackoWiki to work correctly.',
+'Ready' => 'Congratulations, it appears that your server is capable of running WackoWiki. The next few pages will take you through the configuration process.',
+
+/*
+   Site Config Page
+*/
+'site-config' => 'Site Configuration',
+'SiteName' => 'Wiki Name',
+'SiteNameDesc' => 'Please enter the name of your Wiki site.',
+'HomePage' => 'Home Page',
+'HomePageDesc' => 'Enter the name you would like your home page to have, this will be the default page users will see when they visit your site and should be a <a href="https://wackowiki.org/doc/Doc/English/WikiName" title="View Help" target="_blank">WikiName</a>.',
+'HomePageDefault' => 'HomePage',
+'MultiLang' => 'Multi Language Mode',
+'MultiLangDesc' => 'Multilanguage mode allows to have pages with different language settings within single installation. If this mode is enabled, installer will create initial pages for all languages available in distribution.',
+'AllowedLang' => 'Allowed Languages',
+'AllowedLangDesc' => 'It is recomended to select only the set of languages you want to use, other wise all languages are selected.',
+'Admin' => 'Admin Name',
+'AdminDesc' => 'Enter the admins username, this should be a <a href="https://wackowiki.org/doc/Doc/English/WikiName" title="View Help" target="_blank">WikiName</a> (e.g. <code>WikiAdmin</code>).',
+'Password' => 'Admin Password',
+'PasswordDesc' => 'Choose a password for the admin with a minimum of %1 characters.',
+'Password2' => 'Repeat Password:',
+'Mail' => 'Admin Email Address',
+'MailDesc' => 'Enter the admins email address.',
+'Base' => 'Base URL',
+'BaseDesc' => 'Your WackoWiki site base URL.  Page names get appended to it, so if you are using mod_rewrite the address should end with a forward slash i.e.</p><ul><li><strong><code>http://example.com/</code></strong></li><li><strong><code>http://example.com/wiki/</code></strong></li></ul>',
+'Rewrite' => 'Rewrite Mode',
+'RewriteDesc' => 'Rewrite mode should be enabled if you are using WackoWiki with URL rewriting.',
+'Enabled' => 'Enabled:',
+'ErrorAdminEmail' => 'You have entered an invalid email address!',
+'ErrorAdminPasswordMismatch' => 'The passwords do not match!.',
+'ErrorAdminPasswordShort' => 'The admin password is too short, the minimum length is %1 characters!',
+'WarningRewriteMode' => 'ATTENTION!\nYour base URL and rewrite-mode settings looks suspicious. Usually there is no ? mark in the base URL if rewrite-mode is set - but in your case there is one.\n\nTo continue with these settings click OK.\nTo return to the form and change your settings click CANCEL.\n\nIf you are about to proceed with these settings, please note that they COULD cause problems with your WackoWiki installation.',
+'ModRewriteStatusUnknown' => 'The installer cannot verify that mod_rewrite is enabled, however this does not mean it is disabled',
+
+'LanguageArray'	=> [
+	'bg' => 'bulgarian',
+	'da' => 'danish',
+	'nl' => 'dutch',
+	'el' => 'greek',
+	'en' => 'english',
+	'et' => 'estonian',
+	'fr' => 'french',
+	'de' => 'german',
+	'hu' => 'hungarian',
+	'it' => 'italian',
+	'pl' => 'polish',
+	'pt' => 'portuguese',
+	'ru' => 'russian',
+	'es' => 'spanish',
+],
+
+/*
+   Database Config Page
+*/
+'database-config' => 'Database Configuration',
+'DBDriver' => 'Driver',
+'DBDriverDesc' => 'The database driver you want to use. You must choose a legacy driver if you do not have <a href="http://de2.php.net/pdo" target="_blank">PDO</a> installed.',
+'DBCharset' => 'Charset',
+'DBCharsetDesc' => 'The database charset you want to use.',
+'DBEngine' => 'Engine',
+'DBEngineDesc' => 'The database engine you want to use. You must choose MyISAM engine if you do not have at least MariaDB 10 or MySql 5.6 (or greater) and InnoDB support available.',
+'DBHost' => 'Host',
+'DBHostDesc' => 'The host your database server is running on. Usually <code>127.0.0.1</code> or <code>localhost</code> (ie, the same machine your WackoWiki site is on).',
+'DBPort' => 'Port (Optional)',
+'DBPortDesc' => 'The port number your database server is accessable through, leave it blank to use the default port number.',
+'DB' => 'Database Name',
+'DBDesc' => 'The database WackoWiki should use. This database needs to exist already once you continue!',
+'DBUserDesc' => 'Name of the user used to connect to your database.',
+'DBUser' => 'User Name',
+'DBPasswordDesc' => 'Password of the user used to connect to your database.',
+'DBPassword' => 'Password',
+'PrefixDesc' => 'Prefix of all tables used by WackoWiki. This allows you to run multiple WackoWiki installations using the same database by configuring them to use different table prefixes (e.g. wacko_).',
+'Prefix' => 'Table Prefix',
+'ErrorNoDbDriverDetected' => 'No database driver has been detected, please enable either the mysql, mysqli or pdo extension in your php.ini file.',
+'ErrorNoDbDriverSelected' => 'No database driver has been selected, please pick one from the list.',
+'DeleteTables' => 'Delete Existing Tables?',
+'DeleteTablesDesc' => 'ATTENTION! If you proceed with this option selected all current wiki data will be erased from your database.  This cannot be undone unless you manually restore the data from a backup.',
+'ConfirmTableDeletion' => 'Are you sure you want to delete all current wiki tables?',
+
+/*
+   Database Installation Page
+*/
+'database-install' => 'Database Installation',
+'TestingConfiguration' => 'Testing Configuration',
+'TestConnectionString' => 'Testing database connection settings',
+'TestDatabaseExists' => 'Checking if the database you specified exists',
+'InstallingTables' => 'Installing Tables',
+'ErrorDBConnection' => 'There was a problem with the database connection details you specified, please go back and check they are correct.',
+'ErrorDBExists' => 'The database you configured was not found. Remember, it needs to exist before you can install/upgrade WackoWiki!',
+'To' => 'to',
+'AlterTable' => 'Altering <code>%1</code> Table',
+'RenameTable' => 'Renaming <code>%1</code> Table',
+'UpdateTable' => 'Updating <code>%1</code> Table',
+'InstallingDefaultData' => 'Adding Default Data',
+'InstallingPagesBegin' => 'Adding Default Pages',
+'InstallingPagesEnd' => 'Finished Adding Default Pages',
+'InstallingSystemAccount' => 'Adding System User',
+'InstallingAdmin' => 'Adding Admin User',
+'InstallingAdminSetting' => 'Adding Admin User Preferences',
+'InstallingAdminGroup' => 'Adding Admins Group',
+'InstallingAdminGroupMember' => 'Adding Admins Group Member',
+'InstallingEverybodyGroup' => 'Adding Everybody Group',
+'InstallingModeratorGroup' => 'Adding Moderator Group',
+'InstallingReviewerGroup' => 'Adding Reviewer Group',
+'InstallingLogoImage' => 'Adding Logo Image',
+'InstallingConfigValues' => 'Adding Config Values',
+'ErrorInsertingPage' => 'Error inserting <code>%1</code> page',
+'ErrorInsertingPageReadPermission' => 'Error setting read permission for <code>%1</code> page',
+'ErrorInsertingPageWritePermission' => 'Error setting write permission for <code>%1</code> page',
+'ErrorInsertingPageCommentPermission' => 'Error setting comment permissions for <code>%1</code> page',
+'ErrorInsertingPageCreatePermission' => 'Error setting create permissions for <code>%1</code> page',
+'ErrorInsertingPageUploadPermission' => 'Error setting upload permissions for <code>%1</code> page',
+'ErrorInsertingDefaultMenuItem' => 'Error setting page <code>%1</code> as default menu item',
+'ErrorPageAlreadyExists' => 'The <code>%1</code> page already exists',
+'ErrorAlteringTable' => 'Error altering <code>%1</code> table',
+'ErrorRenamingTable' => 'Error renaming <code>%1</code> table',
+'ErrorUpdatingTable' => 'Error updating <code>%1</code> table',
+'CreatingTable' => 'Creating <code>%1</code> table',
+'ErrorAlreadyExists' => 'The <code>%1</code> already exists',
+'ErrorCreatingTable' => 'Error creating <code>%1</code> table, does it already exist?',
+'ErrorMovingRevisions' => 'Error moving revision data',
+'MovingRevisions' => 'Moving data to revisions table',
+'DeletingTables' => 'Deleting Tables',
+'DeletingTablesEnd' => 'Finished Deleting Tables',
+'ErrorDeletingTable' => 'Error deleting <code>%1</code> table, the most likely reason is that the table does not exist in which case you can ignore this warning.',
+'DeletingTable' => 'Deleting <code>%1</code> table',
+
+/*
+   Write Config Page
+*/
+'write-config' => 'Write Config File',
+'FinalStep' => 'Final Step',
+'Writing' => 'Writing Configuration File',
+'RemovingWritePrivilege' => 'Removing Write Privilege',
+'InstallationComplete' => 'Installation Complete',
+'ThatsAll' => 'That\'s all! You can now <a href="%1">view your WackoWiki site</a>.',
+'SecurityConsiderations' => 'Security Considerations',
+'SecurityRisk' => 'You are advised to remove write access to %1 again now that it\'s been written. Leaving the file writable can be a security risk!',
+'RemoveSetupDirectory' => 'You should delete the <code>setup/</code> directory now that the installation process has been completed.',
+'ErrorGivePrivileges' => 'The configuration file %1 could not be written. You will need to give your web server temporary write access to either your WackoWiki directory, or a blank file called %1<br>%2<br>; don\'t forget to remove write access again later, i.e. %3.<br>If, for any reason, you can\'t do this, you\'ll have to copy the text below into a new file and save/upload it as %1 into the WackoWiki directory. Once you\'ve done this, your WackoWiki site should work. If not, please visit <a href="https://wackowiki.org/doc/Doc/English/Installation" target="_blank">WackoWiki:Doc/English/Installation</a>',
+'NextStep' => 'In the next step, the installer will try to write the updated configuration file, %1.  Please make sure the web server has write access to the file, or you will have to edit it manually.  Once again, see  <a href="https://wackowiki.org/doc/Doc/English/Installation" target="_blank">WackoWiki:Doc/English/Installation</a> for details.',
+'WrittenAt' => 'written at ',
+'DontChange' => 'do not change wacko_version manually!',
+'ConfigDescription' => 'detailed description https://wackowiki.org/doc/Doc/English/Configuration',
+'TryAgain' => 'Try Again',
+
+];
+?>
