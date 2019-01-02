496779e0dbd73d0bae3abacb1729b60cb8d466f2
vendeeglobe <vendeeglobe@localhost>
2019-01-02 15:10:16 +0100

    added system user for holding records from deleted users

--- wacko/setup/_insert_queries.php
+++ wacko/setup/_insert_queries.php
@@ -68,6 +68,7 @@ $create_table[]			= ['word',					$tbl_word];
 //		$value[1] - SQL query
 //		$value[2] - record
 $insert_records[]		= [$lang['InstallingSystemAccount'],		$insert_system,					'system account'];
+$insert_records[]		= [$lang['InstallingDeletedAccount'],		$insert_deleted,				'deleted account'];
 $insert_records[]		= [$lang['InstallingAdmin'],				$insert_admin,					'admin user'];
 $insert_records[]		= [$lang['InstallingAdminSetting'],			$insert_admin_setting,			'admin user settings'];
 $insert_records[]		= [$lang['InstallingAdminGroup'],			$insert_admin_group,			'admin group'];
@@ -180,13 +181,13 @@ $upgrade['5.5.rc2'][]	= ['alter',		'page',				$alter_page_r5_4_4];
 $upgrade['5.5.rc2'][]	= ['update',	'page',				$update_page_r5_4_3];
 $upgrade['5.5.rc2'][]	= ['update',	'page',				$update_page_r5_4_4];
 
-$upgrade['5.5.0'][]		= ['alter',	'page',					$alter_page_r5_4_5];
-$upgrade['5.5.0'][]		= ['alter',	'page',					$alter_page_r5_4_6];
+$upgrade['5.5.0'][]		= ['alter',		'page',				$alter_page_r5_4_5];
+$upgrade['5.5.0'][]		= ['alter',		'page',				$alter_page_r5_4_6];
 
 $upgrade['5.5.0'][]		= ['update',	'page',				$update_page_r5_4_5];
 $upgrade['5.5.0'][]		= ['update',	'page',				$update_page_r5_4_6];
 
-$upgrade['5.5.5'][]		= ['alter',	'page',					$alter_page_r5_4_7];
+$upgrade['5.5.5'][]		= ['alter',		'page',				$alter_page_r5_4_7];
 
 // page link
 $upgrade['5.5.rc2'][]	= ['rename',	'page_link',		$rename_page_link_r5_4_0];
