1fdc66a5a7422f0db97a7a707fdfd261eb87e967
vendeeglobe <vendeeglobe@localhost>
2019-01-30 21:59:31 +0100

    added missing escape for INSERT and UPDATE queries

--- wacko/setup/config-database.php
+++ wacko/setup/config-database.php
@@ -43,7 +43,7 @@
 	// -->
 </script>
 
-<form action="<?php echo my_location() ?>?installAction=database-install" method="post" name="form1">
+<form action="<?php echo my_location() ?>?installAction=install-database" method="post" name="form1">
 <?php
 
 write_config_hidden_nodes([
