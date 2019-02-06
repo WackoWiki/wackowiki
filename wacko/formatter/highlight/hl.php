3b1fb342e37eae55d3e4139c1b6a8dc6314ba4e0
vendeeglobe <vendeeglobe@localhost>
2019-02-06 14:21:18 +0100

    fixed some Text_Highlighter issues
    pass numbers settings
    format error message

--- wacko/formatter/highlight/hl.php
+++ wacko/formatter/highlight/hl.php
@@ -5,20 +5,41 @@ $hl = new Text_Highlighter();
 if ($options['_default'])
 {
 	$language	= $options['_default'];
-	$numbers	= (bool) $options['numbers'];
+	$numbers	= false;
 	$start		= (int)($options['start'] ?? 1);
+
+	if (!empty($options['numbers']))
+	{
+		// HL_NUMBERS_LI HL_NUMBERS_TABLE FALSE -> 1 2 0
+		switch ($options['numbers'])
+		{
+			case 1:
+				$numbers = HL_NUMBERS_LI;
+				break;
+			case 2:
+				$numbers = HL_NUMBERS_TABLE;
+				break;
+			default:
+				$numbers = false;
+				break;
+		}
+	}
+
+	$table = $numbers == HL_NUMBERS_TABLE ? true: false;
 
 	$hl =& Text_Highlighter::factory(strtoupper($language), ['numbers' => $numbers, 'numbers_start' => $start]);
 
 	if (!is_object($hl))
 	{
-		echo $hl;
+		$err = '<em>' . Ut::perc_replace($this->_t('FormatterNotFound'), '<code>Highlighter/' . $hl . '</code>') . '</em>';
+		echo $this->show_message($err, 'error', false);
 	}
 	else
 	{
 		echo '<!--notypo-->';
-		// Highlighter class adds a <pre> tag wrapper
+		echo $table ? '<div class="hl-numbers-table">' : '';
 		echo $hl->highlight($text);
+		echo $table ? '</div>' : '';
 		echo '<!--/notypo-->';
 	}
 }
