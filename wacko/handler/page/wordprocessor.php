ee4f0078e8438c0b0c8fd3c59c1269239f377530
vendeeglobe <54716082+vendeeglobe@users.noreply.github.com>
2019-06-22 10:23:03 +0200

    added function msg_is_comment_on()

--- wacko/handler/page/wordprocessor.php
+++ wacko/handler/page/wordprocessor.php
@@ -20,10 +20,11 @@ if ($this->has_access('read'))
 	if ($this->page['comment_on_id'])
 	{
 		$comment_on = $this->load_page('', $this->page['comment_on_id'], '', '', LOAD_META);
-		$message = $this->_t('ThisIsCommentOn') . ' ' .
-			$this->compose_link_to_page($comment_on['tag'], '', $comment_on['title'], $comment_on['tag']) . ', ' .
-			$this->_t('PostedBy') . ' ' . $this->user_link($this->page['user_name'], '', true, false) . ' ' .
-			$this->_t('At') . ' ' . $this->get_time_formatted($this->page['modified']);
+		$message = $this->msg_is_comment_on(
+			$comment_on['tag'],
+			$comment_on['title'],
+			$this->page['user_name'],
+			$this->page['modified']);
 		$this->show_message($message, 'comment-info');
 	}
 
