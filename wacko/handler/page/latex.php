0e62ecd1f1d1435ff9209735b0f71ef23e710387
vendeeglobe <vendeeglobe@localhost>
2019-06-22 11:18:12 +0200

    moved latex formatter in seperate file

--- wacko/handler/page/latex.php
+++ wacko/handler/page/latex.php
@@ -23,7 +23,7 @@ if ($this->has_access('read'))
 		if ($this->page['comment_on_id'])
 		{
 			$comment_on = $this->load_page('', $this->page['comment_on_id'], '', '', LOAD_META);
-			$message = $this->this_is_comment_on(
+			$message = $this->msg_is_comment_on(
 				$comment_on['tag'],
 				$comment_on['title'],
 				$this->page['user_name'],
@@ -40,76 +40,10 @@ if ($this->has_access('read'))
 
 		// display page
 		$this->context[++$this->current_context] = $this->tag;
-		$text = preg_replace('/{{(toc).*?}}/i', '', $this->page['body']);
-		$data = $this->format($text, 'wiki');
 
-		// Convert everything that doesn't need regexps
-		$data = str_replace(
-		[
-			"<br>\n",						// Strip newlines
-			'&nbsp;',						// Blanks to blanks for easier handling
-			'<strong>',						// Bold
-			'</strong>',
-			'<em>',							// Emphasized
-			'</em>',
-			'<small>',						// Small
-			'</small>',
-			'<code>',						// Monospaced
-			'</code>',
-			'<li>',							// List item
-			'</li>',
-			'</ul>',						// End of unnumbered list
-			'</ol>',						// End of numbered list
-		],
-		[
-			"\n",
-			' ',
-			'\textbf{',
-			'}',
-			'\emph{',
-			'}',
-			'\textsmaller{',
-			'}',
-			'\texttt{',
-			'}',
-			'&nbsp;&nbsp;&nbsp;&nbsp;\item ',
-			"\n",
-			"\\end{itemize}\n\n",
-			"\\end{enumerate}\n\n",
-		],
-		$data
-		);
+		$data = $this->format($this->page['body'], 'latex');
 
-		// Convert the cool stuff
-		$data = preg_replace(
-		[
-			'|%%\(math\)(.*)%%|Us',			// Math formula
-			'|%%\(math outline\)(.*)%%|Us',	// Math outline
-			'|<h1>\s*?(.*)\s*</h1>|U',		// Headings
-			'|<h2>\s*?(.*)\s*</h2>|U',
-			'|<h3>\s*?(.*)\s*</h3>|U',
-			'|<ul[^>]*>|U',					// Unnumbered list
-			'|<ol[^>]*>|U',					// Numbered list
-			'|<p.*>(.*)</p>|Us',			// Separate paragraphs by blank line
-			'|</?.*>|U',					// Strip all other HTML tags
-			'|( *\n){3,}|m',				// Cut all \n\n\n... to \n\n
-		],
-		[
-			'$\\1$',
-			'$$\1$$',
-			"\n\\section{\\1}\n",
-			"\n\\subsection{\\1}\n",
-			"\n\\subsubsection{\\1}\n",
-			"\n\\begin{itemize}\n",
-			"\n\\begin{enumerate}\n",
-			"\\1\n\n",
-			'',
-			"\n\n",
-		],
-		$data
-		);
-
-		echo '<code>' . nl2br(trim($data)) . '</code>';
+		echo '<pre class="code">' . $data . '</pre>';
 
 		$this->current_context--;
 
