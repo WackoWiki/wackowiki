cfe17c134dfd20002f0d483e4cd8f1a2843843eb
vendeeglobe <vendeeglobe@localhost>
2019-06-22 12:32:31 +0200

    minor fixes

--- wacko/formatter/latex.php
+++ wacko/formatter/latex.php
@@ -26,6 +26,9 @@ $data = str_replace(
 	'</em>',
 	'<small>',						// Small
 	'</small>',
+	'<blockquote>',					// Blockquote
+	'</blockquote>',
+	'</pre>',						// Preformatted
 	'<code>',						// Monospaced
 	'</code>',
 	'<li>',							// List item
@@ -42,6 +45,9 @@ $data = str_replace(
 	'}',
 	'\textsmaller{',
 	'}',
+	"\n\\begin{quotation}\n",
+	"\n\\end{quotation}\n\n",
+	"\n\\end{verbatim}",
 	'\texttt{',
 	'}',
 	'&nbsp;&nbsp;&nbsp;&nbsp;\item ',
@@ -55,16 +61,17 @@ $data
 // Convert the cool stuff
 $data = preg_replace(
 [
-	'|%%\(math\)(.*)%%|Us',			// Math formula
-	'|%%\(math outline\)(.*)%%|Us',	// Math outline
+	'|%%\(math\)(.*)%%|Us',				// Math formula
+	'|%%\(math outline\)(.*)%%|Us',		// Math outline
 	'|<h1[^>]*>\s*?(.*)\s*</h1>|U',		// Headings
 	'|<h2[^>]*>\s*?(.*)\s*</h2>|U',
 	'|<h3[^>]*>\s*?(.*)\s*</h3>|U',
-	'|<ul[^>]*>|U',					// Unnumbered list
-	'|<ol[^>]*>|U',					// Numbered list
-	'|<p.*>(.*)</p>|Us',			// Separate paragraphs by blank line
-	'|</?.*>|U',					// Strip all other HTML tags
-	'|( *\n){3,}|m',				// Cut all \n\n\n... to \n\n
+	'|<pre[^>]*>|U',					// Preformatted
+	'|<ul[^>]*>|U',						// Unnumbered list
+	'|<ol[^>]*>|U',						// Numbered list
+	'|<p.*>(.*)</p>|Us',				// Separate paragraphs by blank line
+	'|</?.*>|U',						// Strip all other HTML tags
+	'|( *\n){3,}|m',					// Cut all \n\n\n... to \n\n
 ],
 [
 	'$\\1$',
@@ -72,6 +79,7 @@ $data = preg_replace(
 	"\n\\section{\\1}\n",
 	"\n\\subsection{\\1}\n",
 	"\n\\subsubsection{\\1}\n",
+	"\n\\begin{verbatim}\n",
 	"\n\\begin{itemize}\n",
 	"\n\\begin{enumerate}\n",
 	"\\1\n\n",
