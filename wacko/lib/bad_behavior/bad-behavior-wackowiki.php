fd7d9018a0c2f94d0f6f6b872540afd3332912e2
vendeeglobe <54716082+vendeeglobe@users.noreply.github.com>
2019-08-09 10:19:53 +0200

    updated Bad Behavior to 2.2.23

--- wacko/lib/bad_behavior/bad-behavior-wackowiki.php
+++ wacko/lib/bad_behavior/bad-behavior-wackowiki.php
@@ -16,7 +16,7 @@ You should have received a copy of the GNU Lesser General Public License along
 with this program. If not, see <http://www.gnu.org/licenses/>.
 
 Please report any problems to bad . bots AT ioerror DOT us
-http://bad-behavior.ioerror.us/
+https://bad-behavior.ioerror.us/
 
 WackoWiki implementation, 2018 WackoWiki Team
 Version 0.7
