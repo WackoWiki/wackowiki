0c8a69cf5132b0a32682c49a34fa3f7ba35fdeaf
vendeeglobe <vendeeglobe@localhost>
2019-08-23 12:45:12 +0200

    only code clean up

--- wacko/setup/common.php
+++ wacko/setup/common.php
@@ -176,17 +176,21 @@ function insert_page($tag, $title = false, $body, $lang, $rights = 'Admins', $cr
 {
 	global $config_global, $dblink_global, $lang_global;
 
-	$page_select			= "SELECT page_id FROM " . $config_global['table_prefix'] . "page WHERE tag='" . $tag . "'";
-	$owner_id				= "SELECT user_id FROM " . $config_global['table_prefix'] . "user WHERE user_name = 'System' LIMIT 1";
+	$prefix					= $config_global['table_prefix'];
+	$page_select			= "SELECT page_id FROM " . $prefix . "page WHERE tag='" . $tag . "'";
+	$owner_id				= "SELECT user_id FROM " . $prefix . "user WHERE user_name = 'System' LIMIT 1";
 
 	// user_id for user 'System'
 	// we specify values for columns body_r (MEDIUMTEXT) and body_toc (TEXT) that don't have defaults
-	$page_insert			= "INSERT INTO " . $config_global['table_prefix'] . "page (tag, supertag, title, body, body_r, body_toc, user_id, owner_id, created, modified, latest, page_size, page_lang, footer_comments, footer_files, footer_rating, noindex) VALUES ('" . _quote($tag) . "', '" . translit($tag, $lang) . "', '" . _quote($title) . "' , '" . _quote($body) . "', '', '', (" . $owner_id . "), (" . $owner_id . "), UTC_TIMESTAMP(), UTC_TIMESTAMP(), 1, " . strlen($body) . ", '" . _quote($lang) . "', 0, 0, 0, " . $noindex . ")";
+	$page_insert			= "INSERT INTO " .
+									$prefix . "page (tag, supertag, title, body, body_r, body_toc, user_id, owner_id, created, modified, latest, page_size, page_lang, footer_comments, footer_files, footer_rating, noindex)
+								VALUES
+									('" . _quote($tag) . "', '" . translit($tag, $lang) . "', '" . _quote($title) . "' , '" . _quote($body) . "', '', '', (" . $owner_id . "), (" . $owner_id . "), UTC_TIMESTAMP(), UTC_TIMESTAMP(), 1, " . strlen($body) . ", '" . _quote($lang) . "', 0, 0, 0, " . $noindex . ")";
 
-	$page_id				= "SELECT page_id FROM " . $config_global['table_prefix'] . "page WHERE tag = '" . $tag . "' LIMIT 1";
+	$page_id				= "SELECT page_id FROM " . $prefix . "page WHERE tag = '" . $tag . "' LIMIT 1";
 
 	$perm_insert				= "INSERT INTO " .
-									$config_global['table_prefix'] . "acl (page_id, privilege, list)
+									$prefix . "acl (page_id, privilege, list)
 								VALUES
 									((" . $page_id . "), 'read',		'*'),
 									((" . $page_id . "), 'write',		'" . $rights . "'),
@@ -194,7 +198,10 @@ function insert_page($tag, $title = false, $body, $lang, $rights = 'Admins', $cr
 									((" . $page_id . "), 'create',		'$'),
 									((" . $page_id . "), 'upload',		'')";
 
-	$default_menu_item		= "INSERT INTO " . $config_global['table_prefix'] . "menu (user_id, page_id, menu_lang, menu_title) VALUES ((" . $owner_id . "), (" . $page_id . "), '" . _quote($lang) . "', '" . _quote($menu_title) . "')";
+	$default_menu_item		= "INSERT INTO " .
+									$prefix . "menu (user_id, page_id, menu_lang, menu_title)
+								VALUES
+									((" . $owner_id . "), (" . $page_id . "), '" . _quote($lang) . "', '" . _quote($menu_title) . "')";
 
 	$insert_data[]			= [$page_insert,			$lang_global['ErrorInsertingPage']];
 	$insert_data[]			= [$perm_insert,			$lang_global['ErrorInsertingPagePermission']];
