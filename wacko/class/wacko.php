3275b9d70c44b5d17b9bd971fc7acb634940155b
vendeeglobe <54716082+vendeeglobe@users.noreply.github.com>
2019-08-28 17:33:38 +0200

    set width parameter for SVG files
    e.g. file:/diagram.svg?300

--- wacko/class/wacko.php
+++ wacko/class/wacko.php
@@ -3718,7 +3718,14 @@ class Wacko
 					{
 						if ($file_data['file_ext'] == 'svg')
 						{
-							$scale = '';
+							if ($param['width'])
+							{
+								$scale	= ' width="' . $param['width'] . '"';
+							}
+							else
+							{
+								$scale = '';
+							}
 						}
 						else
 						{
