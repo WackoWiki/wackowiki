4534387ffd374bb60427f3afbe86221a16fccbc0
vendeeglobe <vendeeglobe@localhost>
2019-10-04 22:12:56 +0200

    minor fix

--- wacko/handler/page/rename.php
+++ wacko/handler/page/rename.php
@@ -80,7 +80,7 @@ if ($registered
 					// set redirect on original page
 					if ($this->save_page($this->tag, '', '{{redirect page="/' . $new_tag . '"}}', $this->_t('RedirectedTo') . ' ' . $new_tag))
 					{
-						$message .= '<li>' . Ut::perc_replace($this->_t('RedirectCreated'), $this->link($this->tag)) . "</li>\n";
+						$message .= '<li>' . Ut::perc_replace($this->_t('RedirectCreated'), $this->link('/' . $this->tag)) . "</li>\n";
 					}
 
 					$this->clear_cache_wanted_page($this->tag);
@@ -272,7 +272,7 @@ function move(&$engine, $old_page, $new_tag)
 
 					if ($engine->save_page($old_page['tag'], '', '{{redirect page="/' . $new_tag . '"}}', $engine->_t('RedirectedTo') . ' ' . $new_tag))
 					{
-						$message .= '<li>' . Ut::perc_replace($engine->_t('RedirectCreated'), $engine->link($old_page['tag'])) . "</li>\n";
+						$message .= '<li>' . Ut::perc_replace($engine->_t('RedirectCreated'), $engine->link('/' . $old_page['tag'])) . "</li>\n";
 					}
 
 					$engine->clear_cache_wanted_page($old_page['tag']);
