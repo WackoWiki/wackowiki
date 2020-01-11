20a2b07329bc57f82f9c4295d83fb562249f8c9e
vendeeglobe <54716082+vendeeglobe@users.noreply.github.com>
2020-01-11 15:11:28 +0100

    minor fixes

--- wacko/handler/page/new.php
+++ wacko/handler/page/new.php
@@ -67,7 +67,7 @@ if (isset($_POST['tag']) && $new_tag = trim($_POST['tag'], '.-/ '))
 if ($this->has_access('create', $this->get_page_id($this->tag)))
 {
 	$tpl->p_f_base	= (mb_strlen($this->tag) > 50 ? '...' . mb_substr($this->tag, -50) : $this->tag);
-	$tpl->p_f_tag	= (isset($_POST['option']) && $_POST['option'] === 1 ? Ut::html($new_tag) : '');
+	$tpl->p_f_tag	= (isset($_POST['option']) && $_POST['option'] === 1 ? $new_tag : '');
 }
 else
 {
@@ -86,7 +86,7 @@ if (mb_substr_count($this->tag, '/') > 0)
 		if ($parent != $this->db->users_page)
 		{
 			$tpl->c_f_base	= (mb_strlen($parent) > 50 ? '...' . mb_substr($parent, -50) : $parent);
-			$tpl->c_f_tag	= (isset($_POST['option']) && $_POST['option'] === 2 ? Ut::html($new_tag) : '');
+			$tpl->c_f_tag	= (isset($_POST['option']) && $_POST['option'] === 2 ? $new_tag : '');
 		}
 	}
 	else
