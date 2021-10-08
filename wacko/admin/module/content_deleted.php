9b656aef6abfb349265dfd86d3c882e41c7f8097
vendeeglobe <54716082+vendeeglobe@users.noreply.github.com>
2021-10-08 12:48:05 +0200

    minor fixes

--- wacko/admin/module/content_deleted.php
+++ wacko/admin/module/content_deleted.php
@@ -26,73 +26,75 @@ function admin_content_deleted(&$engine, &$module)
 ?>
 	<h1><?php echo $module['title']; ?></h1>
 	<br>
-<?php
-
-$type			= (int)		($_GET['type'] ?? OBJECT_PAGE);
-$action			= (string)	($_GET['action'] ?? null);
-$page_id		= (int)		($_GET['page_id'] ?? null);
-$revision_id	= (int)		($_GET['revision_id'] ?? null);
-$file_id		= (int)		($_GET['file_id'] ?? null);
-
-if ($type == OBJECT_PAGE && $page_id)
-{
-	if ($action == 'delete')
-	{
-		$engine->delete_pages([$page_id]);
-		$engine->delete_acls([$page_id], true);
-		// page_id -> tag -> page_id (really)
-		$tag = $engine->get_page_tag($page_id);
-		$engine->remove_revisions($tag, false, true);
-		$engine->remove_files_perpage($tag, false, true);
-	}
-	else if ($action == 'restore')
-	{
-		$engine->restore_page($page_id);
-		$engine->restore_files_perpage($page_id);
-	}
-}
-else if ($type == OBJECT_REVISION && $revision_id)
-{
-	if ($action == 'delete')
-	{
-		$engine->remove_revision($page_id, $revision_id, true);
-	}
-	else if ($action == 'restore')
-	{
-		$engine->restore_revision($page_id, $revision_id);
-	}
-}
-else if ($type == OBJECT_FILE && $file_id)
-{
-	if ($action == 'delete')
-	{
-		$engine->remove_file($file_id, true);
-	}
-	else if ($action == 'restore')
-	{
-		$engine->restore_file($file_id);
-	}
-}
-
-?>
 	<p>
-	<?php echo $engine->_t('DeletedObjectsInfo');?>
+		<?php echo $engine->_t('DeletedObjectsInfo');?>
 	</p>
-<?php
+	<?php
 
-	$filter_type =
-		'<p class="right">' .
-		($type == OBJECT_PAGE
-			? '<span class="active">' . $engine->_t('UsersPages') . '</span>'
-			: '<a href="' . $engine->href('', '', ['type' => OBJECT_PAGE]) . '">' . $engine->_t('UsersPages') . '</a>' ) .
-		($type == OBJECT_REVISION
-			? ' | <span class="active">' . $engine->_t('UsersRevisions') . '</span>'
-			: ' | <a href="' . $engine->href('', '', ['type' => OBJECT_REVISION]) . '">' . $engine->_t('UsersRevisions') . '</a>' ) .
-		($type == OBJECT_FILE
-			? ' | <span class="active">' . $engine->_t('Files') . '</span>'
-			: ' | <a href="' . $engine->href('', '', ['type' => OBJECT_FILE]) . '">' . $engine->_t('Files') . '</a>') .
-		'</p>';
+	$mode_selector	= 'type';
+	$mode			= @$_GET[$mode_selector];
 
+	// navigation
+	$tabs	= [
+		''		=> 'BbSummary',
+		'1'		=> 'UsersPages',
+		'3'		=> 'UsersRevisions',
+		'2'		=> 'Files',
+	];
+
+	if (!array_key_exists($mode, $tabs))
+	{
+		$mode = '';
+	}
+
+	echo '<h2>' . $engine->_t($tabs[$mode]) . "</h2>";
+	echo '<p>' . $engine->tab_menu($tabs, $mode, '', [], $mode_selector) . '</p><br>';
+
+	$type			= (int)		($_GET['type'] ?? 0);
+	$action			= (string)	($_GET['action'] ?? null);
+	$page_id		= (int)		($_GET['page_id'] ?? null);
+	$revision_id	= (int)		($_GET['revision_id'] ?? null);
+	$file_id		= (int)		($_GET['file_id'] ?? null);
+
+	if ($type == OBJECT_PAGE && $page_id)
+	{
+		if ($action == 'delete')
+		{
+			$engine->delete_pages([$page_id]);
+			$engine->delete_acls([$page_id], true);
+			// page_id -> tag -> page_id (really)
+			$tag = $engine->get_page_tag($page_id);
+			$engine->remove_revisions($tag, false, true);
+			$engine->remove_files_perpage($tag, false, true);
+		}
+		else if ($action == 'restore')
+		{
+			$engine->restore_page($page_id);
+			$engine->restore_files_perpage($page_id);
+		}
+	}
+	else if ($type == OBJECT_REVISION && $revision_id)
+	{
+		if ($action == 'delete')
+		{
+			$engine->remove_revision($page_id, $revision_id, true);
+		}
+		else if ($action == 'restore')
+		{
+			$engine->restore_revision($page_id, $revision_id);
+		}
+	}
+	else if ($type == OBJECT_FILE && $file_id)
+	{
+		if ($action == 'delete')
+		{
+			$engine->remove_file($file_id, true);
+		}
+		else if ($action == 'restore')
+		{
+			$engine->restore_file($file_id);
+		}
+	}
 
 	if ($type == OBJECT_FILE)
 	{
@@ -102,12 +104,58 @@ else if ($type == OBJECT_FILE && $file_id)
 	{
 		[$revisions, $pagination] = $engine->load_deleted_revisions(50, false);
 	}
-	else
+	else if ($type == OBJECT_PAGE)
 	{
 		[$pages, $pagination] = $engine->load_deleted_pages(50, false);
 	}
 
-	echo $filter_type;
+	// show summary
+	if ($type == 0)
+	{
+		$count_pages = $engine->db->load_single(
+			"SELECT COUNT(page_id) AS n " .
+			"FROM " . $engine->db->table_prefix . "page " .
+			"WHERE deleted = 1 LIMIT 1");
+
+		$count_revisions = $engine->db->load_single(
+			"SELECT COUNT(revision_id) AS n " .
+			"FROM " . $engine->db->table_prefix . "revision " .
+			"WHERE deleted = 1 LIMIT 1");
+
+		$count_files = $engine->db->load_single(
+			"SELECT COUNT(file_id) AS n " .
+			"FROM " . $engine->db->table_prefix . "file " .
+			"WHERE deleted = 1 LIMIT 1");
+
+		$results[] = [$engine->href('', '', ['type' => OBJECT_PAGE]),		$engine->_t('UsersPages'),		$count_pages['n']];
+		$results[] = [$engine->href('', '', ['type' => OBJECT_REVISION]),	$engine->_t('UsersRevisions'),	$count_revisions['n']];
+		$results[] = [$engine->href('', '', ['type' => OBJECT_FILE]),		$engine->_t('Files'),			$count_files['n']];
+		?>
+		<table class="formation">
+			<colgroup>
+				<col span="1" style="width: 5%;">
+				<col span="1">
+			</colgroup>
+			<thead>
+				<tr>
+					<th scope="col"><?php echo $engine->_t('BbHits');?></th>
+					<th scope="col"><?php echo $engine->_t('Category'); ?></th>
+				</tr>
+			</thead>
+			<tbody>
+			<?php
+				foreach ($results as $result)
+				{
+					echo '<tr class="lined">' . "\n";
+					echo '<td class="label">' . $result[2] . '</td>' . "\n";
+					echo '<td>' . '<a href="' . $result[0] . '">' . $result[1] . '</a>' . '</td>' . "\n";
+					echo '</tr>' . "\n";
+				}
+			?>
+			</tbody>
+		</table>
+	<?php
+	}
 
 	if (!empty($pages))
 	{
@@ -236,7 +284,7 @@ else if ($type == OBJECT_FILE && $file_id)
 		{
 			echo '<br><p>' . $engine->_t('NoDeletedPages') . '</p>';
 		}
-		if ($type == OBJECT_REVISION)
+		else if ($type == OBJECT_REVISION)
 		{
 			echo '<br><p>' . $engine->_t('NoDeletedRevisions') . '</p>';
 		}
