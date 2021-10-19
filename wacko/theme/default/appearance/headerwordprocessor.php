45ca7333df103e3113269797952c54ec12f82b98
vendeeglobe <54716082+vendeeglobe@users.noreply.github.com>
2021-10-19 14:10:08 +0200

    show custom site favicon also in print handler and admin panel

--- wacko/theme/default/appearance/headerwordprocessor.php
+++ wacko/theme/default/appearance/headerwordprocessor.php
@@ -10,4 +10,7 @@ header('Content-Type: text/html; charset=' . $this->get_charset());
 $tpl->lang		= $this->page_lang;
 $tpl->charset	= $this->get_charset();
 $tpl->title		= !Ut::is_empty(@$this->page['title'])? $this->page['title'] : $this->tag;
+$tpl->favicon	= $this->db->site_favicon
+	? $this->db->base_path . Ut::join_path(IMAGE_DIR, $this->db->site_favicon)
+	: $this->db->theme_url . 'icon/favicon.ico';
 $this->db->terms_page and $tpl->terms_url = $this->href('', $this->db->terms_page);
