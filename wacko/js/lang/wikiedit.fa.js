6d7563f056c2ffdbd17003ed58dde33ab886a544
vendeeglobe <54716082+vendeeglobe@users.noreply.github.com>
2021-11-14 20:40:42 +0100

    add Persian language - part 2

---
+++ wacko/js/lang/wikiedit.fa.js
@@ -0,0 +1,70 @@
+lang = {
+	// default.js
+	ReallySave :			"Really save?",
+	ModifiedHint :			"Field has changed, do not forget to save the changes.",
+	NotSavedWarning :		"You did not save changes. Are you sure you want to leave?",
+	SessionExpiredEditor :	"Your session has expired!\nYou\'ve been logged off due to long inactivity.\n1. Save your form edits to text file to prevent loss.\n2. Refresh the page.\n3. Log in again and re-submit.",
+
+	// wikiedit.js
+	HelpAbout :				"راهنما & درباره",
+	HelpFormatting :		"راهنما",
+	HelpFormattingTip :		"Help on Wiki-formatting",
+	HelpFormattingPage :	"Doc/English/Formatting",
+
+	Heading1 :				"عنوان سطح ۱",
+	Heading2 :				"عنوان سطح ۲",
+	Heading3 :				"عنوان سطح ۳",
+	Heading4 :				"عنوان سطح ۴",
+	Heading5 :				"عنوان سطح ۵",
+	Heading6 :				"عنوان سطح ۶",
+	Bold :					"برجسته",
+	Italic :				"متن ایتالیک",
+	Underline :				"زیرنویس شده",
+	Strikethrough :			"خط خورده",
+	Small :					"Small",
+	Code :					"Inline code",
+	Superscript :			"بالانویس",
+	Subscript :				"زیرنویس",
+	List :					"لیست نامرتب",
+	NumberedList :			"فهرست مرتبشده",
+	Left :					"چپ‌چین",
+	Center :				"وسط‌چین",
+	Right :					"راست‌چین",
+	Justify :				"Justify",
+	Outdent :				"Outdent",
+	Indent :				"Indent",
+	CodeWrapper :			"Code Wrapper",
+	HTML :					"HTML",
+	Action :				"Action",
+	Line :					"خط افقی",
+	Signature :				"امضا",
+	Quote :					"نقل قول",
+	MarkedText :			"Marked text",
+	HighlightText :			"Highlight text",
+	Shade :					"Shade",
+	Hyperlink :				"Hyperlink",
+	InsertTable :			"Insert table",
+
+	Link :					"پیوند",
+	TextForLinking :		"Text for linking",
+
+	HelpAboutTip :			' Shortcuts:\n'+
+							' Ctrl+B - Bold\n'+
+							' Ctrl+I - Italic\n'+
+							' Ctrl+U - Underline\n'+
+							' Ctrl+Shift+S - Strikethrough\n'+
+							' Ctrl+Shift+1 .. 4 - Heading 1..4\n'+
+							' Alt+I or Ctrl+T - Indent\n'+
+							' Alt+U or Ctrl+Shift+T - Unindent\n'+
+							' Ctrl+J - MarkUp (!!)\n'+
+							' Ctrl+H - MarkUp (??)\n'+
+							' Alt+L - Link\n'+
+							' Ctrl+L - Link with description\n'+
+							' Ctrl+Shift+L - Unordered List\n'+
+							' Ctrl+Shift+N - Ordered List\n'+
+							' Ctrl+Shift+O - Ordered List\n'+
+							' Ctrl+= - Small text\n'+
+							' Ctrl+Shift+Minus - Horizontal line\n'+
+							' \n'
+
+};
