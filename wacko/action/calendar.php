a9571689b1d3209d63f460c784b448848e495d71
vendeeglobe <54716082+vendeeglobe@users.noreply.github.com>
2021-12-12 11:06:02 +0100

    minor fixes

--- wacko/action/calendar.php
+++ wacko/action/calendar.php
@@ -98,7 +98,7 @@ $generate_calendar = function ($year, $month, $days = [], $day_name_length = 3, 
 	[$month, $year, $month_name]	= explode(',', $fmt->format($first_of_month));
 	$weekday						= date('w', $first_of_month);
 
-	#$weekday	= ($weekday + 7 - $first_day) % 7; // adjust for $first_day
+	$weekday	= ($weekday + 7 - $first_day) % 7; // adjust for $first_day
 	$title		= Ut::html(utf8_ucfirst($month_name)) . NBSP . $year;
 	// begin calendar
 
