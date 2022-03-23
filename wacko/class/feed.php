496b999cd8a8865ed068a1ec7c0054f180e18a23
vendeeglobe <54716082+vendeeglobe@users.noreply.github.com>
2022-03-23 17:32:14 +0100

    set missing context for feed
    solves wrong context for relative file links resulting in a 404

--- wacko/class/feed.php
+++ wacko/class/feed.php
@@ -180,7 +180,9 @@ class Feed
 				}
 
 				// TODO: format -> add ['feed' => true]
+				$this->engine->context[++$this->engine->current_context] = $page['tag'];
 				$text	= $this->engine->format($page['body_r'], 'post_wacko');
+				$this->engine->current_context--;
 
 				$xml .= '<item>' . "\n" .
 							'<title>' . $title . '</title>' . "\n" .
