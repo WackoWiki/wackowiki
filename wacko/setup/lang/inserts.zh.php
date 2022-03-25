08df4655a0ffcd2396a91d057abe486cd78d38a6
vendeeglobe <54716082+vendeeglobe@users.noreply.github.com>
2022-03-25 08:26:05 +0100

    updated message sets

--- wacko/setup/lang/inserts.zh.php
+++ wacko/setup/lang/inserts.zh.php
@@ -5,20 +5,20 @@ $insert = [
 
 	'home_page_body'	=> 'file:/wacko_logo.png?right' . "\n" .
 			'**欢迎来到您的 ((WackoWiki:Doc/English WackoWiki)) 网站！**' . "\n\n" .
-			'Click after you have ((/登录 logged in)) on the "Edit this page" link at the bottom to get started.' . "\n\n" .
+			'点击后在底部"编辑此页面"链接((/登录 登录)) 开始操作。' . "\n\n" .
 			'文件可在以下网址找到 WackoWiki:Doc/English.' . "\n" .
-			'Useful pages: ((WackoWiki:Doc/English/Formatting Formatting)), ((/搜索 搜索)).',
+			'有用的页面：((WackoWiki:Doc/English/Formatting Formatting)), ((/搜索 搜索)).',
 	'root_page'			=> '首页',
 
-	'category_page'		=> '类别',
-	'groups_page'		=> '群组',
+	'category_page'		=> '分类',
+	'groups_page'		=> '用户组',
 	'users_page'		=> '用户',
 
 	'help_page'			=> '帮助',
 	'terms_page'		=> '使用条款',
 	'privacy_page'		=> '隐私政策',
 
-	'registration_page'	=> '创建账户',
+	'registration_page'	=> '注册',
 	'password_page'		=> '密码',
 	'search_page'		=> '搜索',
 	'login_page'		=> '登录',
@@ -27,7 +27,7 @@ $insert = [
 	'changes_page'		=> '最近更改',
 	'comments_page'		=> '最近评论',
 	'index_page'		=> '页面索引',
-	'random_page'		=> '随机条目',
+	'random_page'		=> '随机页面',
 
 	'changes_page_bm'	=> '更改',
 	'comments_page_bm'	=> '评论',
