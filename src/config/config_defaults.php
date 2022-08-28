b68bdca068daddda2b42111b9e3b120eb5024c77
vendeeglobe <54716082+vendeeglobe@users.noreply.github.com>
2022-08-28 11:44:19 +0200

    turned Typografica corrector off by default

--- src/config/config_defaults.php
+++ src/config/config_defaults.php
@@ -122,7 +122,7 @@ $wacko_config_defaults = [
 	'numerate_links'				=> 0,
 
 	'allow_x11colors'				=> 0,
-	'default_typografica'			=> 1,
+	'default_typografica'			=> 0,
 	'paragrafica'					=> 1,
 
 	'disable_bracketslinks'			=> 0,
