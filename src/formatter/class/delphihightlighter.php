b5cf6f4b2b914c4c641a4391b49eb1eaa5f49aed
vendeeglobe <54716082+vendeeglobe@users.noreply.github.com>
2022-09-09 21:43:12 +0200

    only code styling

--- src/formatter/class/delphihightlighter.php
+++ src/formatter/class/delphihightlighter.php
@@ -19,37 +19,37 @@ class DelphiHightlighter
 	public $tok;			// word being cut out
 	public $char;			// current character
 	public $i;				// current position in the code
-	public $codelength;	// length of the code string
+	public $code_length;	// length of the code string
 	/****************************************************************/
 	/* The variables that define the behavior of the analyzer       */
 	/****************************************************************/
 	public $case_sensitive	= false;                   // case sensitive language or not
-	public $tokdelimiters	= " []()=+-/*:;,.\n\t\r  "; // word delimiters
+	public $tok_delimiters	= " []()=+-/*:;,.\n\t\r  "; // word delimiters
 
 	/***************************************************/
 	/* The colors associated with each type of data    */
 	/***************************************************/
-	public $colorkeyword	= '';
-	public $colortext		= '';
-	public $colorstring	= '#000000';
-	public $colorcomment	= '#FF0000';
-	public $colorsymbol	= '';
-	public $colornumber	= '#000080';
-	public $colorpreproc	= '#008000';
+	public $color_keyword	= '';
+	public $color_text		= '';
+	public $color_string	= '#000000';
+	public $color_comment	= '#FF0000';
+	public $color_symbol	= '';
+	public $color_number	= '#000080';
+	public $color_preproc	= '#008000';
 
 	/*************************************************/
 	/* Data styles for each data type                */
 	/*************************************************/
-	public $stylekeyword	= ['<strong>', '</strong>'];
-	public $styletext		= ['', ''];
-	public $stylestring	= ['<span style="background-color: yellow;">', '</span>'];
-	public $stylecomment	= ['<em>', '</em>'];
-	public $stylesymbol	= ['', ''];
-	public $stylenumber	= ['', ''];
-	public $stylepreproc	= ['<em>', '</em>'];
+	public $style_keyword	= ['<strong>', '</strong>'];
+	public $style_text		= ['', ''];
+	public $style_string	= ['<span style="background-color: yellow;">', '</span>'];
+	public $style_comment	= ['<em>', '</em>'];
+	public $style_symbol	= ['', ''];
+	public $style_number	= ['', ''];
+	public $style_preproc	= ['<em>', '</em>'];
 
 	/*****************/
-	/* The keywords  */
+	/* Keywords */
 	/*****************/
 	public $keywords = [
 	'unit','interface','implementation','initialization','finalization','uses',
@@ -66,7 +66,7 @@ class DelphiHightlighter
 	/***********************************/
 	/* Delimiters for comment */
 	/***********************************/
-	public $commentdelimiters = [
+	public $comment_delimiters = [
 		["//", "\n"],
 		["{", "}"],
 		["(*", "*)"]
@@ -75,14 +75,14 @@ class DelphiHightlighter
 	/********************************************/
 	/* Delimiters for Strings */
 	/********************************************/
-	public $stringdelimiters = [
+	public $string_delimiters = [
 		["'", "'"]
 	];
 
 	/********************************************************/
 	/* Delimiters for pre-processor-instructions */
 	/********************************************************/
-	public $preprocdelimiters = [
+	public $preproc_delimiters = [
 		["(*\$", "*)"],
 		["{\$", "}"]
 	];
@@ -94,7 +94,7 @@ class DelphiHightlighter
 	/************************************************************************/
 	/* Returns true if a character is visible and can be colored            */
 	/************************************************************************/
-	function visiblechar($char)
+	function visible_char($char)
 	{
 		$inviblechars = " \t\n\r  ";
 
@@ -104,7 +104,7 @@ class DelphiHightlighter
 	/************************************************************/
 	/* Format a word in a special way (color + style)           */
 	/************************************************************/
-	function formatspecialtok($tok, $color, $style)
+	function format_specialtok($tok, $color, $style)
 	{
 		if (empty($color))
 		{
@@ -136,12 +136,12 @@ class DelphiHightlighter
 	/*****************************************************/
 	/* Analyzes a word and returns it in formatted form  */
 	/*****************************************************/
-	function analyseword($tok)
+	function analyse_word($tok)
 	{
 		// If it's a number
 		if ((isset($tok[0]) && $tok[0] == '$') || (isset($tok[0]) && $tok[0] == '#') || ($tok == (string) intval($tok)))
 		{
-			return $this->formatspecialtok($tok, $this->colornumber, $this->stylenumber);
+			return $this->format_specialtok($tok, $this->color_number, $this->style_number);
 		}
 
 		// If it's empty, we return an empty string
@@ -151,17 +151,17 @@ class DelphiHightlighter
 		if (   (($this->case_sensitive) && (is_integer(array_search($tok, $this->keywords, false))))
 			|| ((!$this->case_sensitive) && (is_integer($this->array_search_case($tok, $this->keywords)))))
 		{
-			return $this->formatspecialtok($tok, $this->colorkeyword, $this->stylekeyword);
+			return $this->format_specialtok($tok, $this->color_keyword, $this->style_keyword);
 		}
 
 		// Otherwise, the word is returned without formatting
-		return $this->formatspecialtok($tok, $this->colortext, $this->styletext);
+		return $this->format_specialtok($tok, $this->color_text, $this->style_text);
 	}
 
 	/****************************************************************/
 	/* We're checking to see if we're not running into a delimiter. */
 	/****************************************************************/
-	function parsearray($array, $color = '#000080', $style = ['<em>', '</em>'])
+	function parse_array($array, $color = '#000080', $style = ['<em>', '</em>'])
 	{
 		// We're doing some verifications.
 		if (!is_array($array))		return false;
@@ -170,9 +170,9 @@ class DelphiHightlighter
 
 		// We will try to compare the current character with the 1st
 		// character of each first delimiter
-		foreach ($array as $delimiterarray)
+		foreach ($array as $delimiter_array)
 		{
-			$delimiter1 = $delimiterarray[0];
+			$delimiter1 = $delimiter_array[0];
 
 			// If the 1st char matches
 			if ($this->char == $delimiter1[0])
@@ -189,7 +189,7 @@ class DelphiHightlighter
 				// If we have it in its entirety
 				if ($match)
 				{
-					$delimiter2 = $delimiterarray[1];
+					$delimiter2 = $delimiter_array[1];
 					// So we're looking for the end delimiter
 					$delimiterend = strpos($this->code, $delimiter2, $this->i + strlen($delimiter1));
 					// If we don't find the end delimiter, we take the whole file
@@ -202,17 +202,17 @@ class DelphiHightlighter
 
 					if (!empty($this->tok))
 					{
-						$this->newcode .= $this->analyseword($this->tok);
+						$this->newcode .= $this->analyse_word($this->tok);
 						$this->tok = '';
 					}
 
 					// Then, the text is placed between the delimiters
-					$this->newcode .= $this->formatspecialtok(substr($this->code, $this->i, $delimiterend - $this->i + strlen($delimiter2)), $color, $style);
+					$this->newcode .= $this->format_specialtok(substr($this->code, $this->i, $delimiterend - $this->i + strlen($delimiter2)), $color, $style);
 					// We put the clue back in the right place
 					$this->i = $delimiterend + strlen($delimiter2);
 
 					// Finally we get the current character
-					if ($this->i > $this->codelength)
+					if ($this->i > $this->code_length)
 					{
 						$this->char = null;
 					}
@@ -233,25 +233,25 @@ class DelphiHightlighter
 	/****************************/
 	/* It handles special cases */
 	/****************************/
-	function parsearrays()
+	function parse_arrays()
 	{
-		$haschanged = true;
+		$has_changed = true;
 
 		// With each change, the entire loop is restarted
-		while ($haschanged)
+		while ($has_changed)
 		{
 			// We're checking to see if we're not running into a comment delimiter
-			$haschanged = $this->parsearray($this->preprocdelimiters, $this->colorpreproc, $this->stylepreproc);
+			$has_changed = $this->parse_array($this->preproc_delimiters, $this->color_preproc, $this->style_preproc);
 
-			if (!$haschanged)
+			if (!$has_changed)
 			{
 				// We're checking to see if we're not running into a comment delimiter
-				$haschanged = $this->parsearray($this->commentdelimiters, $this->colorcomment, $this->stylecomment);
+				$has_changed = $this->parse_array($this->comment_delimiters, $this->color_comment, $this->style_comment);
 
-				if (!$haschanged)
+				if (!$has_changed)
 				{
 					// Or a string of characters
-					$haschanged = $this->parsearray($this->stringdelimiters, $this->colorstring, $this->stylestring);
+					$has_changed = $this->parse_array($this->string_delimiters, $this->color_string, $this->style_string);
 				} // if
 			} // if
 		} // while
@@ -271,26 +271,26 @@ class DelphiHightlighter
 	/***************************/
 	/*Analyse the complete code */
 	/***************************/
-	function analysecode($text)
+	function analyse_code($text)
 	{
 		// Initialize variables
 		$this->newcode		= '';
 		$this->tok			= '';
 		$this->char			= null;
 		$this->code			= $text;
-		$this->codelength	= strlen($this->code);
+		$this->code_length	= strlen($this->code);
 
-		$this->trace('debut analysecode');
-		$this->dump($this->codelength, 'codelength');
+		$this->trace('debut analyse_code');
+		$this->dump($this->code_length, 'code_length');
 		$this->dump($this->code, 'code');
 
-		for ($this->i = 0; $this->i < $this->codelength; $this->i++ )
+		for ($this->i = 0; $this->i < $this->code_length; $this->i++ )
 		{
 			$this->dump($this->i, 'i');
 			$this->char = $this->code[$this->i];
 			$this->dump($this->char,'char');
 			// We're looking for a special case.
-			$this->parsearrays();
+			$this->parse_arrays();
 
 			// We're looking to see if we've reached the end of the chain.
 			if ($this->char == null)
@@ -299,15 +299,15 @@ class DelphiHightlighter
 			}
 
 			// We've finished analyzing the comments, we're checking to see if we have a complete word.
-			if (is_integer(strpos($this->tokdelimiters, $this->char)))
+			if (is_integer(strpos($this->tok_delimiters, $this->char)))
 			{
 				// We come across a delimiter, we cut the word
-				$this->newcode .= $this->analyseword($this->tok);
+				$this->newcode .= $this->analyse_word($this->tok);
 				// We format the delimiter
 
-				if ($this->visiblechar($this->char))
+				if ($this->visible_char($this->char))
 				{
-					$this->newcode .= $this->formatspecialtok($this->char, $this->colorsymbol, $this->stylesymbol);
+					$this->newcode .= $this->format_specialtok($this->char, $this->color_symbol, $this->style_symbol);
 				}
 				else
 				{
@@ -326,7 +326,7 @@ class DelphiHightlighter
 		// We're checking to see if we can get to the end of the code.
 		if (!empty($this->tok))
 		{
-			$this->newcode .= $this->analyseword($this->tok);
+			$this->newcode .= $this->analyse_word($this->tok);
 		}
 
 		return $this->newcode;
