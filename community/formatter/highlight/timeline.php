f2a51616049ef02e14169bf84182fb72abc4637e
vendeeglobe <54716082+vendeeglobe@users.noreply.github.com>
2022-11-15 19:38:13 +0100

    adds timeline formatter

---
+++ community/formatter/highlight/timeline.php
@@ -0,0 +1,76 @@
+<?php
+
+/*
+	converts inline data into a timeline
+
+	%%(timeline
+		[order="asc|desc"]
+		[delim="comma|semicolon"]
+		)
+	content
+	%%
+
+	[0] direction: 'l|r'	(left|right)
+	[1] flag: 'Amsterdam'
+	[2] time: 'July 1 & 2, 2022'
+	[3] description: 'Dutch PHP Conference 2022'
+
+	e.g.
+	l | Moscow | September 12-13, 2022 | PHP Russia 2022
+	r | Amsterdam | July 1 & 2, 2022 | Dutch PHP Conference 2022
+
+	https://wackowiki.org/doc/Dev/PatchesHacks/Timeline
+*/
+
+// set defaults
+$options['delim']	??= 'pipe';
+$options['order']	??= 'asc';
+
+$delim	= match($options['delim']) {
+	'semicolon'	=> ';',
+	default		=> '|',
+};
+
+$lines	= preg_split('/[\n]/u', $text);
+
+if ($options['order'] == 'desc')
+{
+	$lines = array_reverse($lines);
+}
+
+foreach ($lines as $line)
+{
+	// blank
+	if (preg_match('/^#|^\s*$/u', $line))
+	{
+		continue;
+	}
+
+	$tpl->enter('n_');
+
+	$item = explode('|', $line);
+	{
+		// debug
+		#Ut::debug_print_r($item);
+
+		$direction	= match(trim($item[0])) {
+			'r'			=> 'r',
+			default		=> 'l',
+		};
+
+		// further string processing here (links, filter, ...)
+		$flag			= trim($item[1] ?? '');
+		$time			= trim($item[2] ?? '');
+		$description	= trim($item[3] ?? '');
+
+		if ($flag)
+		{
+			$tpl->direction		= $direction;
+			$tpl->flag			= $flag;
+			$tpl->time			= $time;
+			$tpl->description	= $this->format($description, 'wiki', ['post_wacko' => true]);
+		}
+	}
+
+	$tpl->leave(); // n_
+}
