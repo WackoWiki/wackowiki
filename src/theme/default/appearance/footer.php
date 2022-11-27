b2f13474e53a5db660e0149049a2af91a09552b0
vendeeglobe <54716082+vendeeglobe@users.noreply.github.com>
2022-11-27 12:14:39 +0100

    minor fixes

--- src/theme/default/appearance/footer.php
+++ src/theme/default/appearance/footer.php
@@ -62,7 +62,10 @@ if ($this->page && $this->has_access('read'))
 	}
 
 	// permalink
-	$tpl->perma_link = $this->action('hashid');
+	if ($this->method == 'show')
+	{
+		$tpl->perma_link = $this->action('hashid');
+	}
 }
 
 $tpl->enter('credits_');
