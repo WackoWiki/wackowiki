eab1defca554ef9cc9962473d3f2d7f8d36bbf80
vendeeglobe <54716082+vendeeglobe@users.noreply.github.com>
2022-12-25 13:37:53 +0100

    removed redundant \d class element

--- src/formatter/highlight/ini.php
+++ src/formatter/highlight/ini.php
@@ -9,7 +9,7 @@ $text = Ut::html($text);
 
 $text = preg_replace('/([=,\|]+)/um',				'<span class="ini-1">\\1</span>', $text);
 $text = preg_replace('/^([;#].+)$/um',				'<span class="ini-2">\\1</span>', $text);
-$text = preg_replace('/([^\d\w#;:>])([;#].+)$/um',	'<span class="ini-2">\\2</span>', $text);
+$text = preg_replace('/([^\w#;:>])([;#].+)$/um',	'<span class="ini-2">\\2</span>', $text);
 $text = preg_replace('/^(\[.*\])/um',				'<span class="ini-3">\\1</span>', $text);
 
 if ($options['line_numbers'])
