50d819f06cf67f0c2b5822b765e2affa611962c5
vendeeglobe <54716082+vendeeglobe@users.noreply.github.com>
2024-08-02 16:55:36 +0200

    initial language files for Tamil

---
+++ src/setup/lang/inserts.ta.php
@@ -0,0 +1,36 @@
+<?php
+
+$insert = [
+	'lang'				=> 'en',
+
+	'home_page_body'		=> 'file:/wacko_logo.png?right' . "\n" .
+			'**Welcome to your ((WackoWiki:Doc/English WackoWiki)) site!**' . "\n\n" .
+			'Click after you have ((/Login logged in)) on the "Edit this page" link at the bottom to get started.' . "\n\n" .
+			'Documentation can be found at WackoWiki:Doc/English.' . "\n" .
+			'Useful pages: ((WackoWiki:Doc/English/Formatting Formatting)), ((/Search Search)).' . "\n\n",
+	'root_page'			=> 'Home Page',
+
+	'category_page'		=> 'Category',
+	'groups_page'		=> 'Groups',
+	'users_page'		=> 'Users',
+
+	'help_page'			=> 'Help',
+	'terms_page'		=> 'Terms of Use',
+	'privacy_page'		=> 'Privacy policy',
+
+	'registration_page'	=> 'Registration',
+	'password_page'		=> 'Password',
+	'search_page'		=> 'Search',
+	'login_page'		=> 'Login',
+	'account_page'		=> 'Settings',
+
+	'changes_page'		=> 'Recent Changes',
+	'comments_page'		=> 'Recently Commented',
+	'index_page'		=> 'Page Index',
+	'random_page'		=> 'Random Page',
+
+	'changes_page_bm'	=> 'Changes',
+	'comments_page_bm'	=> 'Comments',
+	'index_page_bm'		=> 'Index',
+	'random_page_bm'	=> 'Random',
+];
