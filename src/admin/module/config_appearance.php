54773e3927f7f1f970635e0045e43de8a7dc4529
vendeeglobe <54716082+vendeeglobe@users.noreply.github.com>
2025-01-13 09:31:47 +0100

    removes max values from form for logo

--- src/admin/module/config_appearance.php
+++ src/admin/module/config_appearance.php
@@ -285,7 +285,7 @@ function admin_config_appearance($engine, $module)
 			<?php	// TODO: add option to reset dimensions to default image size
 					// + option to 'readonly / disable' input fields
 			?>
-				<input type="number" min="16" max="500" maxlength="3" style="width: 50px;" id="logo_width" name="logo_width" value="<?php echo (int) $engine->db->logo_width;?>"> × <input type="number" min="16" max="500" maxlength="3" style="width:50px;" id="logo_height" name="logo_height" value="<?php echo (int) $engine->db->logo_height; ?>"> <?php echo $engine->_t('UnitPixel'); ?>
+				<input type="number" min="16" maxlength="3" style="width: 50px;" id="logo_width" name="logo_width" value="<?php echo (int) $engine->db->logo_width;?>"> × <input type="number" min="16" maxlength="3" style="width:50px;" id="logo_height" name="logo_height" value="<?php echo (int) $engine->db->logo_height; ?>"> <?php echo $engine->_t('UnitPixel'); ?>
 			</td>
 		</tr>
 		<?php } ?>
