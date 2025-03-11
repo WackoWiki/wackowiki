d0f6f47b665c36637e8265790facf9845e9334fb
vendeeglobe <54716082+vendeeglobe@users.noreply.github.com>
2025-03-11 08:25:17 +0100

    Indonesian

---
+++ src/setup/lang/inserts.id.php
@@ -0,0 +1,36 @@
+<?php
+
+$insert = [
+	'lang'				=> 'id',
+
+	'home_page_body'		=> 'file:/wacko_logo.png?right' . "\n" .
+			'**Welcome to your ((WackoWiki:Doc/English WackoWiki)) site!**' . "\n\n" .
+			'Click after you have ((/Login logged in)) on the "Edit this page" link at the bottom to get started.' . "\n\n" .
+			'Documentation can be found at WackoWiki:Doc/English.' . "\n" .
+			'Useful pages: ((WackoWiki:Doc/English/Formatting Formatting)), ((/Search Search)).' . "\n\n",
+	'root_page'			=> 'Halaman Beranda',
+
+	'category_page'		=> 'Kategori',
+	'groups_page'		=> 'Kelompok',
+	'users_page'		=> 'Pengguna',
+
+	'help_page'			=> 'Bantuan',
+	'terms_page'		=> 'Ketentuan Penggunaan',
+	'privacy_page'		=> 'Privacy policy',
+
+	'registration_page'	=> 'Registrasi',
+	'password_page'		=> 'Kata sandi',
+	'search_page'		=> 'Cari',
+	'login_page'		=> 'Log masuk',
+	'account_page'		=> 'Settings',
+
+	'changes_page'		=> 'Recent Changes',
+	'comments_page'		=> 'Recently Commented',
+	'index_page'		=> 'Page Index',
+	'random_page'		=> 'Random Page',
+
+	'changes_page_bm'	=> 'Changes',
+	'comments_page_bm'	=> 'Komentar',
+	'index_page_bm'		=> 'Index',
+	'random_page_bm'	=> 'Random',
+];
