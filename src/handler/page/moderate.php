d9fd403777997fd7154a9a946808724f75f89798
vendeeglobe <54716082+vendeeglobe@users.noreply.github.com>
2025-10-15 22:46:27 +0200

    substitute LEFT(text, 500) with SUBSTR(text, 1, 500)
    Ensures compatibility across different SQL databases, including SQLite.

--- src/handler/page/moderate.php
+++ src/handler/page/moderate.php
@@ -1146,7 +1146,7 @@ if (($this->is_moderator() && $this->has_access('read')) || $this->is_admin())
 
 		// make collector query
 		$sql =
-			'SELECT p.page_id, p.owner_id, p.user_id, p.tag, p.title, p.user_id, p.owner_id, ip, LEFT(body, 500) AS body, p.created, p.page_lang, u.user_name, o.user_name as owner_name ' .
+			'SELECT p.page_id, p.owner_id, p.user_id, p.tag, p.title, p.user_id, p.owner_id, ip, SUBSTR(body, 1, 500) AS body, p.created, p.page_lang, u.user_name, o.user_name as owner_name ' .
 			'FROM ' . $prefix . 'page p ' .
 				'LEFT JOIN ' . $prefix . 'user u ON (p.user_id = u.user_id) ' .
 				'LEFT JOIN ' . $prefix . 'user o ON (p.owner_id = o.user_id) ' .
